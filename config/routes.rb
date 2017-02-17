ef26534a4a350b727eb5a6debd21722325790ebb
Tiago Garcia <tiagojsag@gmail.com>
2017-02-17 13:14:05 +0100

    Rename geoid endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :geo_id, only: :index
 
+  get '/get_linked_geoids', to: 'geo_id#index'
+
 end
