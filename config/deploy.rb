2c535d19011722b87b6823598c599af266a89575
Tiago Garcia <tiagojsag@gmail.com>
2017-02-17 12:22:27 +0100

    Update deploy path

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:Vizzuality/trase-api.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/trase-new-api'
+set :deploy_to, '/var/www/trase-api-v2'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
