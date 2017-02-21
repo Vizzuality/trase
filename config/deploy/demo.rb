628a793d6d4a3b1da8416d09d51705076b1a02eb
Tiago Garcia <tiagojsag@gmail.com>
2017-02-21 10:19:20 +0100

    Add demo deploy env; update staging IP address

---
+++ config/deploy/demo.rb
@@ -0,0 +1,4 @@
+server '35.161.156.173', user: 'ubuntu', roles: %w{web app db}, primary: true
+set :ssh_options, {
+    forward_agent: true
+}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server 'staging.trase.earth', user: 'ubuntu', roles: %w{web app db}, primary: true
+server '52.41.12.189', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, {
     forward_agent: true
 }
