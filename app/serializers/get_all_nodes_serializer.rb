ae0487bc143aa1fffa8ab08923f96aac03015e3c
Tiago Garcia <tiagojsag@gmail.com>
2017-02-22 10:23:38 +0100

    Add isAggregated field

--- app/serializers/get_all_nodes_serializer.rb
+++ app/serializers/get_all_nodes_serializer.rb
@@ -1,3 +1,7 @@
 class GetAllNodesSerializer < ActiveModel::Serializer
-  attributes :id, :name, :type, :column_id, :geo_id, :has_flows
+  attributes :id, :name, :type, :column_id, :geo_id, :has_flows, :isAggregated
+
+  def isAggregated
+    object.name.casecmp('other').zero?
+  end
 end
