01cd0e6fb7da392aefc263f2efd044ce1d173b19
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-03-08 10:23:21 +0000

    Created materialized view to query for flow values easily

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217085928) do
+ActiveRecord::Schema.define(version: 20170308111306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,4 +168,72 @@ ActiveRecord::Schema.define(version: 20170217085928) do
   add_foreign_key "node_quants", "nodes", primary_key: "node_id", name: "node_quants_node_id_fkey"
   add_foreign_key "node_quants", "quants", primary_key: "quant_id", name: "node_quants_quant_id_fkey"
   add_foreign_key "nodes", "node_types", primary_key: "node_type_id", name: "nodes_node_type_id_fkey"
+
+  create_view :materialized_flows, materialized: true,  sql_definition: <<-SQL
+      WITH normalized_flows AS (
+           SELECT f_1.flow_id,
+              f_1.year,
+              f_1.node_id,
+              f_1."position",
+              f_1.context_id,
+              cn.node_type_id,
+              cn.node_type
+             FROM (( SELECT flows.flow_id,
+                      flows.year,
+                      a.node_id,
+                      a."position",
+                      flows.context_id
+                     FROM flows,
+                      LATERAL unnest(flows.path) WITH ORDINALITY a(node_id, "position")) f_1
+               JOIN ( SELECT context_nodes.context_id,
+                      context_nodes.column_position,
+                      context_nodes.node_type_id,
+                      node_types.node_type
+                     FROM (context_nodes
+                       JOIN node_types ON ((node_types.node_type_id = context_nodes.node_type_id)))) cn ON (((f_1."position" = (cn.column_position + 1)) AND (f_1.context_id = cn.context_id))))
+          )
+   SELECT f.flow_id,
+      f.context_id,
+      f.year,
+      f.node_id,
+      f.node_type,
+          CASE
+              WHEN (f.node_type = ANY (ARRAY['STATE'::text, 'BIOME'::text])) THEN upper(n.name)
+              ELSE initcap(n.name)
+          END AS node,
+      n.geo_id,
+      f_ex.node_id AS exporter_node_id,
+      n_ex.name AS exporter_node,
+      f_im.node_id AS importer_node_id,
+      n_im.name AS importer_node,
+      f_ctry.node_id AS country_node_id,
+      n_ctry.name AS country_node,
+      q.quant_id,
+      (((q.name || ' ('::text) || q.unit) || ')'::text) AS name,
+      fq.value
+     FROM (((((((((normalized_flows f
+       JOIN nodes n ON ((n.node_id = f.node_id)))
+       JOIN normalized_flows f_ex ON (((f.flow_id = f_ex.flow_id) AND (f_ex.node_type = 'EXPORTER'::text))))
+       JOIN nodes n_ex ON ((f_ex.node_id = n_ex.node_id)))
+       JOIN normalized_flows f_im ON (((f.flow_id = f_im.flow_id) AND (f_im.node_type = 'IMPORTER'::text))))
+       JOIN nodes n_im ON ((n_im.node_id = f_im.node_id)))
+       JOIN normalized_flows f_ctry ON (((f.flow_id = f_ctry.flow_id) AND (f_ctry.node_type = 'COUNTRY'::text))))
+       JOIN nodes n_ctry ON ((n_ctry.node_id = f_ctry.node_id)))
+       JOIN flow_quants fq ON ((f.flow_id = fq.flow_id)))
+       JOIN quants q ON ((fq.quant_id = q.quant_id)))
+    WHERE (f.node_type = ANY (ARRAY['STATE'::text, 'BIOME'::text, 'MUNICIPALITY'::text]))
+    ORDER BY
+          CASE
+              WHEN (f.node_type = 'STATE'::text) THEN 1
+              WHEN (f.node_type = 'BIOME'::text) THEN 2
+              ELSE 3
+          END, n_ex.name, n_im.name, n_ctry.name;
+  SQL
+
+  add_index "materialized_flows", ["country_node_id"], name: "index_materialized_flows_on_country_node_id", using: :btree
+  add_index "materialized_flows", ["exporter_node_id"], name: "index_materialized_flows_on_exporter_node_id", using: :btree
+  add_index "materialized_flows", ["importer_node_id"], name: "index_materialized_flows_on_importer_node_id", using: :btree
+  add_index "materialized_flows", ["node_id"], name: "index_materialized_flows_on_node_id", using: :btree
+  add_index "materialized_flows", ["quant_id"], name: "index_materialized_flows_on_quant_id", using: :btree
+
 end
