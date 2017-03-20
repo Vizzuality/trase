aeefc2573be95dfe7ec7f81a1e519b7c9a6fe2b6
Tiago Garcia <tiagojsag@gmail.com>
2017-03-20 10:16:39 +0100

    Fix typo

--- spec/support/two_flows_context.rb
+++ spec/support/two_flows_context.rb
@@ -20,7 +20,7 @@ shared_context "two flows" do
     FactoryGirl.create(:node_type, node_type:NodeTypeName::MUNICIPALITY)
   }
   let(:exporter_node_type){
-    FactoryGirl.create(:node_type, node_type:NodeTypeName::BIOME)
+    FactoryGirl.create(:node_type, node_type:NodeTypeName::EXPORTER)
   }
   let(:port_node_type){
     FactoryGirl.create(:node_type, node_type:NodeTypeName::PORT)
