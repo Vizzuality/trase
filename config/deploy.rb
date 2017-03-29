3e19c3807289205276ce8b2778f9fb4d4a6d7b8a
Tiago Garcia <tiagojsag@gmail.com>
2017-03-29 12:36:10 +0200

    set ruby version on capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,3 +32,5 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :rvm_ruby_version, '2.4.0'
