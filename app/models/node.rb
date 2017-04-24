d5a13b6be314903f12c9533380830d7ed0412626
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-04-24 10:00:42 +0100

    Started work on top_traders

--- app/models/node.rb
+++ app/models/node.rb
@@ -22,8 +22,74 @@ class Node < ActiveRecord::Base
   has_many :node_quals, :class_name => 'NodeQual', :foreign_key => :node_id
   has_many :node_inds, :class_name => 'NodeInd', :foreign_key => :node_id
 
-  def get_parents(parents=[])
-    return parents.push(self) if self.parent.nil?
-    parent.get_parents(parents.push(self))
+  scope :biomes, -> {
+    includes(:node_type).where('node_types.node_type' => NodeTypeName::BIOME)
+  }
+
+  scope :states, -> {
+    includes(:node_type).where('node_types.node_type' => NodeTypeName::STATE)
+  }
+
+  scope :place_nodes, -> {
+    includes(:node_type).
+    where(
+      'node_types.node_type' => [
+        NodeTypeName::MUNICIPALITY, NodeTypeName::LOGISTICS_HUB, NodeTypeName::BIOME, NodeTypeName::STATE
+      ]
+    )
+  }
+
+  def ancestors(ancestors_memo=[])
+    return ancestors_memo.push(self) if self.parent.nil?
+    parent.ancestors(ancestors_memo.push(self))
   end
+
+  def place_quals
+    data = node_quals.
+      joins(:qual).
+      where('quals.place_factsheet' => true).
+      where('place_factsheet_temporal IS NULL OR place_factsheet_temporal IS FALSE').
+      select([
+        'quals.name',
+        'node_quals.value'#,
+        # 'node_quals.year'
+      ])
+    Hash[data.map do |e|
+      [e['name'], e]
+    end]
+  end
+
+  def place_quants
+    data = node_quants.
+      joins(:quant).
+      where('quants.place_factsheet' => true).
+      where('place_factsheet_temporal IS NULL OR place_factsheet_temporal IS FALSE').
+      select([
+        'quants.name',
+        # 'quants.unit',
+        # 'quants.unit_type',
+        # 'quants.frontend_name',
+        # 'quants.tooltip_text',
+        'node_quants.value'
+      ])
+    Hash[data.map do |e|
+      [e['name'], e]
+    end]
+  end
+
+  def place_inds
+    data = node_inds.
+      joins(:ind).
+      where('inds.place_factsheet' => true).
+      where('place_factsheet_temporal IS NULL OR place_factsheet_temporal IS FALSE').
+      select([
+        'inds.name',
+        'node_inds.value'#,
+        # 'node_inds.year'
+      ])
+    Hash[data.map do |e|
+      [e['name'], e]
+    end]
+  end
+
 end
---
+++ app/models/place_attributes.rb
@@ -0,0 +1,133 @@
+class PlaceAttributes
+
+  def initialize(context, node)
+    @context = context
+    @node = node
+    @node_type = @node.node_type.node_type
+
+    # single_quant_values = NodeQuant
+    #                            .select('quants.name, quants.unit, quants.unit_type, quants.frontend_name, quants.tooltip_text, node_quants.value')
+    #                            .joins(:quant)
+    #                            .joins(:node)
+    #                            .where('place_factsheet IS TRUE AND (place_factsheet_temporal IS NULL OR place_factsheet_temporal IS FALSE) AND nodes.node_id = :node_id', {:node_id => node_id})
+    #                            .as_json
+
+    # single_qual_values = NodeQual
+    #                           .select('quals.name, NULL as unit, NULL as unit_type, quals.frontend_name, quals.tooltip_text, node_quals.value')
+    #                           .joins(:qual)
+    #                           .joins(:node)
+    #                           .where('place_factsheet IS TRUE AND (place_factsheet_temporal IS NULL OR place_factsheet_temporal IS FALSE) AND nodes.node_id = :node_id', {:node_id => node_id})
+    #                           .as_json
+
+    # single_ind_values = NodeInd
+    #                          .select('inds.name, inds.unit, inds.unit_type, inds.frontend_name, inds.tooltip_text, node_inds.value')
+    #                          .joins(:ind)
+    #                          .joins(:node)
+    #                          .where('place_factsheet IS TRUE AND (place_factsheet_temporal IS NULL OR place_factsheet_temporal IS FALSE) AND nodes.node_id = :node_id', {:node_id => node_id})
+    #                          .as_json
+
+    # @temporal_quants = Quant
+    #               .select('quants.name, quants.frontend_name, quants.tooltip_text, quants.quant_id, node_quants.value,  node_quants.year')
+    #               .joins(node_quants: [:node])
+    #               .where('place_factsheet IS TRUE AND place_factsheet_temporal IS TRUE AND nodes.node_id = :node_id', { :node_id => node_id })
+
+    # @single_value_metrics = single_quant_values #+ single_qual_values + single_ind_values
+
+    @data = {
+      column_name: @node_type,
+      country_name: @context.country.name,
+      country_geo_id: @context.country.iso2
+    }
+    ([@node] + @node.ancestors.to_a).each do |node|
+      @data[node.node_type.node_type.downcase + '_name'] = node.name
+      @data[node.node_type.node_type.downcase + '_geo_id'] = node.geo_id
+    end
+
+    if [NodeTypeName::MUNICIPALITY, NodeTypeName::LOGISTICS_HUB].include? @node_type
+      @data = @data.merge(municipality_and_logistics_hub_extra_data)
+    end
+
+    @data = @data.merge(top_traders)
+
+    # @result = {
+    #     data: data,
+    #     type: node.node_type.node_type,
+    #     name: node.name,
+    #     geo_id: node.geo_id,
+    #     single_value_metrics: @single_value_metrics,
+    #     place_quants: node.place_quants
+    #     #temporal_quants: @temporal_quants
+    # }
+  end
+
+  def result
+    {
+      data: @data
+    }
+  end
+
+  def municipality_and_logistics_hub_extra_data
+    data = {}
+    place_quals = @node.place_quals
+    place_quants = @node.place_quants
+    place_inds = @node.place_inds
+
+    biome_name = place_quals[NodeTypeName::BIOME]['value']
+    biome = Node.biomes.find_by_name(biome_name)
+    data['biome_name'] = biome_name
+    data['biome_geo_id'] = biome.try(:geo_id)
+    state_name = place_quals[NodeTypeName::STATE]['value']
+    state = Node.states.find_by_name(state_name)
+    data['state_name'] = state_name
+    data['state_geo_id'] = state.try(:geo_id)
+    if place_quants['AREA_KM2'].present?
+      data['area'] = place_quants['AREA_KM2']['value']
+    end
+    if place_inds['SOY_AREAPERC'].present?
+      data['soy_farmland'] = place_inds['SOY_AREAPERC']['value']
+    end
+    data
+  end
+
+  def top_traders
+    trader_index = ContextNode.joins(:node_type).
+      where(context_id: @context.id).
+      where('node_types.node_type' => NodeTypeName::EXPORTER).
+      pluck(:column_position).first
+
+    select_clause = ActiveRecord::Base.send(
+      :sanitize_sql_array,
+      ["flows.path[?] AS trader_id, sum(CAST(flow_quants.value AS DOUBLE PRECISION)) AS value, nodes.name AS name",
+      trader_index]
+    )
+    nodes_join_clause = ActiveRecord::Base.send(
+      :sanitize_sql_array,
+      ["LEFT JOIN nodes ON nodes.node_id = flows.path[?] AND nodes.name not LIKE 'UNKNOWN%'",
+      trader_index]
+    )
+    group_clause = ActiveRecord::Base.send(
+      :sanitize_sql_array,
+      ["flows.path[?], nodes.name",
+      trader_index]
+    )
+    top_traders = Flow.select(select_clause).
+      joins('LEFT JOIN flow_quants ON flows.flow_id = flow_quants.flow_id').
+      joins("JOIN quants ON quants.quant_id = flow_quants.quant_id AND quants.name = 'Volume'").
+      joins(nodes_join_clause).
+      where('flows.context_id' => @context.id).
+      where('? = ANY(path)', @node.id).
+      where(year: @context.default_year).
+      group(group_clause).
+      order('value DESC').
+      limit(10)
+
+    sum_traders = top_traders.map{ |t| t[:value] }.reduce(0, :+)
+
+    {
+      top_traders: {
+        actors: top_traders.map{ |t| {id: t['trader_id'], name: t['name'], value: t['value']/sum_traders} },
+        matrix: [] #TODO
+      }
+    }
+  end
+end
