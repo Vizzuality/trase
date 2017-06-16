fbef53c6bb4c5596424e2a343c8aafdcc83b2451
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-06-16 15:18:52 +0100

    Moved file preparation logic to specialised class which now also handles zipping

--- spec/controllers/download_controller_spec.rb
+++ spec/controllers/download_controller_spec.rb
@@ -7,29 +7,17 @@ RSpec.describe DownloadController, type: :controller do
       MaterializedFlow.refresh
       FactoryGirl.create(:download_version, symbol: 'v1.0', current: true)
     end
-    it "returns a csv file" do
+    it "returns a zipped csv file" do
       get :index, params: { context_id: context.id }, format: :csv
       expect(assigns(:context)).to eq(context)
-      expect(response.content_type).to eq("text/csv")
-      expect(response.headers["Content-Disposition"]).to eq("attachment; filename=BRAZIL_SOY_v1.0.csv")
+      expect(response.content_type).to eq("application/zip")
+      expect(response.headers["Content-Disposition"]).to eq('attachment; filename="BRAZIL_SOY_v1.0.zip"')
     end
-    it "returns a csv file with pivot" do
-      get :index, params: { context_id: context.id, pivot: 1 }, format: :csv
-      expect(assigns(:context)).to eq(context)
-      expect(response.content_type).to eq("text/csv")
-      expect(response.headers["Content-Disposition"]).to eq("attachment; filename=BRAZIL_SOY_v1.0.csv")
-    end
-    it "returns a semicolon-delimited csv file" do
-      get :index, params: { context_id: context.id, separator: :semicolon}, format: :csv
-      expect(assigns(:context)).to eq(context)
-      expect(response.content_type).to eq("text/csv")
-      expect(response.headers["Content-Disposition"]).to eq("attachment; filename=BRAZIL_SOY_v1.0.csv")
-    end
-    it "returns a json file" do
+    it "returns a zipped json file" do
       get :index, params: { context_id: context.id }, format: :json
       expect(assigns(:context)).to eq(context)
-      expect(response.content_type).to eq("text/json")
-      expect(response.headers["Content-Disposition"]).to eq("attachment; filename=BRAZIL_SOY_v1.0.json")
+      expect(response.content_type).to eq("application/zip")
+      expect(response.headers["Content-Disposition"]).to eq('attachment; filename="BRAZIL_SOY_v1.0.zip"')
     end
   end
 end
