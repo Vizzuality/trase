d5b00fae2687adfa221261e7b7bb123b09f4aa77
Tiago Garcia <tiagojsag@gmail.com>
2017-10-06 19:36:34 +0200

    Add version per context

--- app/models/flow_download.rb
+++ app/models/flow_download.rb
@@ -13,7 +13,7 @@ class FlowDownload
     else
       ','
     end
-    @download_name = "#{@context.country.name}_#{@context.commodity.name}_#{DownloadVersion.current_version_symbol}"
+    @download_name = [@context.country.name, @context.commodity.name, DownloadVersion.current_version_symbol(@context)].reject(&:nil?).join('_')
     query_builder = FlowDownloadQueryBuilder.new(@context, params)
     @query = if @pivot
       query_builder.pivot_query
---
+++ spec/support/paraguay_context.rb
@@ -0,0 +1,9 @@
+shared_context "paraguay context" do
+  let!(:paraguay_context){
+    FactoryGirl.create(
+      :context,
+      country: FactoryGirl.create(:country, name: 'PARAGUAY', iso2: 'PY'),
+      commodity: FactoryGirl.create(:commodity, name: 'SOY')
+    )
+  }
+end
