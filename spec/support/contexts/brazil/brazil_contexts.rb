6a6e2472af8160d9ecfed4a71b57dfb71ce7331f
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-11-07 13:45:09 +0000

    Prevent tests from creating duplicate objects

--- spec/support/contexts/brazil/brazil_contexts.rb
+++ spec/support/contexts/brazil/brazil_contexts.rb
@@ -1,5 +1,6 @@
 shared_context 'brazil contexts' do
   let!(:brazil) do
+    Country.find_by_iso2('BR') ||
     FactoryGirl.create(:country, name: 'BRAZIL', iso2: 'BR', latitude: 10, longitude: 10, zoom: 4)
   end
   let!(:context) do
--- spec/support/contexts/brazil/brazil_resize_by.rb
+++ spec/support/contexts/brazil/brazil_resize_by.rb
@@ -1,11 +1,11 @@
 shared_context 'brazil resize by' do
+  include_context 'brazil contexts'
   include_context 'quants'
-  include_context 'brazil soy indicators'
 
   let!(:resize_by_area) do
-    FactoryGirl.create(:context_resize_by, resize_attribute: area, tooltip_text: 'area tooltip text', context: context)
+    FactoryGirl.create(:context_resize_by, resize_attribute: area, tooltip_text: 'area tooltip text', context: context, position: 1)
   end
   let!(:resize_by_land_conflicts) do
-    FactoryGirl.create(:context_resize_by, resize_attribute: land_conflicts, tooltip_text: 'land conflicts tooltip text', context: context)
+    FactoryGirl.create(:context_resize_by, resize_attribute: land_conflicts, tooltip_text: 'land conflicts tooltip text', context: context, position: 2)
   end
 end
