3a7c931e9e04529220a5659aa668a9d06c4a9df4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-11-07 14:06:01 +0000

    Copy data to revamp before testing

--- spec/version_migration/get_contexts_spec.rb
+++ spec/version_migration/get_contexts_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'Get contexts', type: :request do
 
   describe 'GET /api/v2/get_contexts === GET /api/v3/get_contexts ' do
     it 'has the correct response structure' do
+      SchemaRevamp.new.copy
+
       get '/api/v2/get_contexts'
       v2_response = HashSorter.new(JSON.parse(@response.body)).sort
 
