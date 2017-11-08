f55b5e7a48cc7a1bdf01f30a4f462627bfc87c10
Tiago Garcia <tiagojsag@gmail.com>
2017-11-08 15:58:37 +0100

    Consolidate content database migrations

--- db_content/migrate/20161025184024_add_image_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToPost < ActiveRecord::Migration[5.0]
-  def change
-    add_attachment :posts, :image
-  end
-end
--- db_content/migrate/20161025193352_add_highlighted_field.rb
+++
@@ -1,5 +0,0 @@
-class AddHighlightedField < ActiveRecord::Migration[5.0]
-  def change
-    add_column :posts, :highlighted, :boolean, default: false
-  end
-end
