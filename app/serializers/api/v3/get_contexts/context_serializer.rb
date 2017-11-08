7eecc9ff19e60a96a8c579a362ff48d76ac0cbbd
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-11-08 14:46:15 +0000

    Amendments to serializer to make responses identical

--- app/serializers/api/v3/get_contexts/context_serializer.rb
+++ app/serializers/api/v3/get_contexts/context_serializer.rb
@@ -6,7 +6,6 @@ module Api
 
         has_many :m_recolor_by_attributes, serializer: RecolorByAttributeSerializer, key: :recolor_by
         has_many :m_resize_by_attributes, serializer: ResizeByAttributeSerializer, key: :resize_by
-        # has_many :context_filter_bies, serializer: FilterBySerializer, key: :filter_by
 
         attribute :country_name do
           object.country.name
@@ -25,7 +24,35 @@ module Api
         end
 
         attribute :default_context_layers do
-          object.contextual_layers.pluck(:identifier)
+          identifiers = object.contextual_layers.where(is_default: true).pluck(:identifier)
+          identifiers.any? ? identifiers : nil
+        end
+
+        # TODO: remove once API migration finalised
+        attribute :filter_by do
+          biome_context_node_type = object.context_node_types.
+            joins(:node_type).
+            where('node_types.name' => NodeTypeName::BIOME).
+            first
+          if biome_context_node_type # Brazil - Soy only
+            [
+              {
+                name: NodeTypeName::BIOME,
+                nodes: Api::V3::Node.where(
+                  node_type_id: biome_context_node_type.node_type_id
+                ).
+                where(is_unknown: false).
+                where("name NOT LIKE 'OTHER%'").map do |node|
+                  {
+                    name: node.name,
+                    node_id: node.id
+                  }
+                end
+              }
+            ]
+          else
+            []
+          end
         end
       end
     end
