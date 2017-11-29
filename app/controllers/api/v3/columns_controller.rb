dfebdd6bc2d5754ff3a444af3cc962043e4192b6
Tiago Santos <santos.tiago@gmail.com>
2017-11-29 11:15:45 +0100

    Changed the columns controller to return the profile_type instead of having it hardcoded in the serializer

--- app/controllers/api/v3/columns_controller.rb
+++ app/controllers/api/v3/columns_controller.rb
@@ -3,8 +3,8 @@ module Api
     class ColumnsController < ApiController
       def index
         @node_types = Api::V3::NodeType.
-          joins(context_node_types: :context_node_type_property).
-          select('node_types.id, context_node_types.column_position as position, context_node_type_properties.column_group as group, node_types.name as name, context_node_type_properties.is_default, context_node_type_properties.is_geo_column as is_geo').
+          joins(context_node_types: [:context_node_type_property, :profiles]).
+          select('node_types.id, context_node_types.column_position as position, context_node_type_properties.column_group as group, node_types.name as name, context_node_type_properties.is_default, context_node_type_properties.is_geo_column as is_geo, profiles.name as profile_type').
           where('context_node_types.context_id = :context_id', context_id: @context.id).
           order('context_node_types.column_position ASC')
 
--- app/serializers/api/v3/columns/column_serializer.rb
+++ app/serializers/api/v3/columns/column_serializer.rb
@@ -2,16 +2,7 @@ module Api
   module V3
     module Columns
       class ColumnSerializer < ActiveModel::Serializer
-        attributes :id, :name, :position, :group, :is_default, :is_geo
-
-        attribute :profile_type do
-          case object.name
-          when 'IMPORTER / EXPORTER'
-            'actor'
-          when 'MUNICIPALITY'
-            'place'
-          end
-        end
+        attributes :id, :name, :position, :group, :is_default, :is_geo, :profile_type
       end
     end
   end
