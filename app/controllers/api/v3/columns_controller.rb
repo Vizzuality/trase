e114320e480d90e8bafbd9f713119d37e61324b4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-11-29 21:50:45 +0000

    Needs a left join with profiles

--- app/controllers/api/v3/columns_controller.rb
+++ app/controllers/api/v3/columns_controller.rb
@@ -3,7 +3,8 @@ module Api
     class ColumnsController < ApiController
       def index
         @node_types = Api::V3::NodeType.
-          joins(context_node_types: [:context_node_type_property, :profile]).
+          joins(context_node_types: :context_node_type_property).
+          joins("LEFT JOIN #{Api::V3::Profile.table_name} ON profiles.context_node_type_id = context_node_types.id").
           select('node_types.id, context_node_types.column_position as position, context_node_type_properties.column_group as group, node_types.name as name, context_node_type_properties.is_default, context_node_type_properties.is_geo_column as is_geo, profiles.name as profile_type').
           where('context_node_types.context_id = :context_id', context_id: @context.id).
           order('context_node_types.column_position ASC')
