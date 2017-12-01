8a001ae56d2dfa8436ef841970fa7ca45b6415c2
Tiago Santos <santos.tiago@gmail.com>
2017-12-01 16:58:41 +0100

    First version of the V3 endpoints for Nodes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
         resources :map_groups, only: [:index]
         resources :columns, only: [:index]
         resources :flows, only: [:index]
+        resources :nodes, only: [:index]
       end
       get '/get_all_nodes', to: 'nodes#get_all_nodes'
       resources :newsletter_subscriptions, only: [:create]
