cd880f18aa0fbe46571080d866f70219388c8f61
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-12-11 11:40:08 +0000

    Fixed gold master tests

--- app/controllers/api/v3/nodes_controller.rb
+++ app/controllers/api/v3/nodes_controller.rb
@@ -2,32 +2,27 @@ module Api
   module V3
     class NodesController < ApiController
       def index
-        flow_nodes = Api::V3::Flow.select('distinct(unnest(path)) as node_id')
+        nodes_with_flows = Api::V3::Flow.
+          select('DISTINCT(UNNEST(path)) AS node_id').
+          where(context_id: @context.id)
 
-        node_type_ids = Api::V3::NodeType.
-          joins(:context_node_types).
-          select('node_types.id')
-
-        flow_nodes = flow_nodes.where('flows.context_id = :context_id', context_id: @context.id)
-        node_type_ids = node_type_ids.where('context_node_types.context_id = :context_id', context_id: @context.id)
-
-        matching_nodes = Api::V3::Node.
+        nodes = Api::V3::Node.
           select('
-                 nodes.id as node_id, nodes.main_id, nodes.name, geo_id, nodes.node_type_id as column_id,
-                 node_properties.is_domestic_consumption, nodes.is_unknown,
-                 node_types.name as type, profiles.name as profile_type,
-                 CASE WHEN flow_nodes.node_id IS NOT NULL OR nodes.name = \'OTHER\' THEN true ELSE false END as has_flows
+                 nodes.id, nodes.main_id, nodes.name, geo_id, nodes.node_type_id AS column_id,
+                 NULLIF(node_properties.is_domestic_consumption, FALSE) AS is_domestic_consumption, NULLIF(nodes.is_unknown, FALSE) AS is_unknown,
+                 node_types.name AS type, profiles.name::TEXT AS profile_type,
+                 CASE WHEN nodes_with_flows.node_id IS NOT NULL OR nodes.name = \'OTHER\' THEN true ELSE false END AS has_flows
                  ').
-          joins(:node_type).
           joins(:node_property).
-          joins("LEFT OUTER JOIN (#{flow_nodes.to_sql}) flow_nodes ON flow_nodes.node_id = nodes.id").
-          joins("INNER JOIN context_node_types on context_node_types.node_type_id = node_types.id AND context_node_types.context_id = #{@context.id}").
-          joins('INNER JOIN profiles on profiles.context_node_type_id = context_node_types.id').
-          where('flow_nodes.node_id IS NOT NULL OR substring(geo_id from 1 for 2) = :country_code OR nodes.name = \'OTHER\' ', country_code: @context.country.iso2).
-          where('nodes.node_type_id IN (:node_type_ids)', node_type_ids: node_type_ids).
+          joins(node_type: :context_node_types).
+          joins('LEFT JOIN profiles ON profiles.context_node_type_id = context_node_types.id').
+          joins("LEFT OUTER JOIN (#{nodes_with_flows.to_sql}) nodes_with_flows ON nodes_with_flows.node_id = nodes.id").
+          where('nodes_with_flows.node_id IS NOT NULL OR SUBSTRING(geo_id FROM 1 FOR 2) = ? OR nodes.name = \'OTHER\' ', @context.country.iso2).
+          where('context_node_types.context_id' => @context.id).
           all
 
-        render json: matching_nodes, root: 'data', each_serializer: Api::V3::Nodes::NodeSerializer
+        render json: nodes, root: 'data',
+               each_serializer: Api::V3::Nodes::NodeSerializer
       end
     end
   end
--- app/serializers/api/v3/nodes/node_serializer.rb
+++ app/serializers/api/v3/nodes/node_serializer.rb
@@ -3,12 +3,11 @@ module Api
     module Nodes
       class NodeSerializer < ActiveModel::Serializer
         attribute :main_id, key: :main_node_id
-        attribute :aggregated?, key: :is_aggregated
         attributes :id, :name, :type, :column_id, :geo_id,
-                   :has_flows, :is_domestic_consumption,
-                   :is_unknown, :profile_type
+                   :is_domestic_consumption, :is_unknown, :profile_type,
+                   :has_flows
 
-        def aggregated?
+        attribute :is_aggregated do
           object.name.casecmp('other').zero?
         end
       end
