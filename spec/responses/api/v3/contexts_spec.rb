407226012bb9e4c7c8e9abb87fb47c538d1e4af3
Tiago Garcia <tiagojsag@gmail.com>
2017-12-18 16:04:18 +0100

    Add tests for contexts endpoint in API V3

---
+++ spec/responses/api/v3/contexts_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Get contexts', type: :request do
+  include_context 'brazil soy indicators'
+  include_context 'brazil resize by'
+  include_context 'brazil recolor by'
+
+  describe 'GET /api/v3/contexts' do
+    it 'has the correct response structure' do
+      get '/api/v3/contexts'
+
+      expect(@response.status).to eq 200
+      expect(@response).to match_response_schema('contexts_v3')
+    end
+  end
+end
