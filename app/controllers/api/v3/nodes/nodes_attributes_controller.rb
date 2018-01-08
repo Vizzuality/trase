949436024e5216ff856e04d449e4aed6a9a21d75
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-01-08 14:11:38 +0000

    Shortened code by using an available helper

--- app/controllers/api/v3/nodes/nodes_attributes_controller.rb
+++ app/controllers/api/v3/nodes/nodes_attributes_controller.rb
@@ -3,16 +3,8 @@ module Api
     module Nodes
       class NodesAttributesController < ApiController
         def index
-          # TODO: use ensure_required_param_present when merged
-          unless params[:start_year].present?
-            raise ActionController::ParameterMissing,
-                  'Required param start_year missing'
-          end
-          # TODO: use ensure_required_param_present when merged
-          unless params[:end_year].present?
-            raise ActionController::ParameterMissing,
-                  'Required param end_year missing'
-          end
+          ensure_required_param_present(:start_year)
+          ensure_required_param_present(:end_year)
 
           result = Api::V3::NodeAttributes::Filter.new(
             @context, params[:start_year].to_i, params[:end_year].to_i
