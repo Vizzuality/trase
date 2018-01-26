eb0a1545eb7fc69684db8453987eff271723b871
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-01-26 11:31:49 +0000

    Preventing duplicates in recolor by / resize by attributes test objects

--- spec/support/contexts/api/v3/brazil/brazil_recolor_by.rb
+++ spec/support/contexts/api/v3/brazil/brazil_recolor_by.rb
@@ -3,69 +3,90 @@ shared_context 'api v3 brazil recolor by' do
   include_context 'api v3 quals'
   include_context 'api v3 inds'
 
-  let!(:recolor_by_attributes_forest_500) do
-    FactoryBot.create(
-      :api_v3_recolor_by_attribute,
-      tooltip_text: 'forest 500 tooltip text',
-      context: api_v3_context,
-      position: 1,
-      years: [],
-      group_number: 1,
-      is_disabled: false,
-      is_default: false,
-      legend_type: 'legend_type',
-      legend_color_theme: 'legend_color_theme'
-    )
-  end
-  let!(:recolor_by_inds_forest_500) do
-    FactoryBot.create(
-      :api_v3_recolor_by_ind,
-      recolor_by_attribute: recolor_by_attributes_forest_500,
-      ind: api_v3_forest_500
-    )
-  end
-
-  let!(:recolor_by_attributes_water_scarcity) do
-    FactoryBot.create(
-      :api_v3_recolor_by_attribute,
-      tooltip_text: 'water scarcity tooltip text',
-      context: api_v3_context,
-      position: 2,
-      years: [],
-      group_number: 1,
-      is_disabled: false,
-      is_default: false,
-      legend_type: 'legend_type',
-      legend_color_theme: 'legend_color_theme'
-    )
-  end
-  let!(:recolor_by_inds_water_scarcity) do
-    FactoryBot.create(
-      :api_v3_recolor_by_ind,
-      recolor_by_attribute: recolor_by_attributes_water_scarcity,
-      ind: api_v3_water_scarcity
-    )
+  let!(:api_v3_forest_500_recolor_by_attribute) do
+    recolor_by_attribute = Api::V3::RecolorByInd.
+      includes(:recolor_by_attribute).
+      where(
+        'recolor_by_attributes.context_id' => api_v3_context.id,
+        ind_id: api_v3_forest_500.id
+      ).first&.recolor_by_attribute
+    unless recolor_by_attribute
+      recolor_by_attribute = FactoryBot.create(
+        :api_v3_recolor_by_attribute,
+        tooltip_text: 'forest 500 tooltip text',
+        context: api_v3_context,
+        position: 1,
+        years: [],
+        group_number: 1,
+        is_disabled: false,
+        is_default: false,
+        legend_type: 'stars',
+        legend_color_theme: 'red-blue'
+      )
+      FactoryBot.create(
+        :api_v3_recolor_by_ind,
+        recolor_by_attribute: recolor_by_attribute,
+        ind: api_v3_forest_500
+      )
+    end
+    recolor_by_attribute
   end
 
-  let!(:recolor_by_attributes_biome) do
-    FactoryBot.create(
-      :api_v3_recolor_by_attribute,
-      tooltip_text: 'biome tooltip text',
-      context: api_v3_context,
-      position: 3,
-      years: [],
-      group_number: 1,
-      is_disabled: false,
-      is_default: false,
-      legend_type: 'legend_type',
-      legend_color_theme: 'legend_color_theme'
-    )
+  let!(:api_v3_water_scarcity_recolor_by_attribute) do
+    recolor_by_attribute = Api::V3::RecolorByInd.
+      includes(:recolor_by_attribute).
+      where(
+        'recolor_by_attributes.context_id' => api_v3_context.id,
+        ind_id: api_v3_water_scarcity.id
+      ).first&.recolor_by_attribute
+    unless recolor_by_attribute
+      recolor_by_attribute = FactoryBot.create(
+        :api_v3_recolor_by_attribute,
+        tooltip_text: 'water scarcity tooltip text',
+        context: api_v3_context,
+        position: 2,
+        years: [],
+        group_number: 1,
+        is_disabled: false,
+        is_default: false,
+        legend_type: 'linear',
+        legend_color_theme: 'red-blue'
+      )
+      FactoryBot.create(
+        :api_v3_recolor_by_ind,
+        recolor_by_attribute: recolor_by_attribute,
+        ind: api_v3_water_scarcity
+      )
+    end
+    recolor_by_attribute
   end
-  let!(:recolor_by_quals_biome) do
-    FactoryBot.create(
-      :api_v3_recolor_by_qual,
-      recolor_by_attribute: recolor_by_attributes_biome,
-      qual: api_v3_biome
-    )
+
+  let!(:api_v3_biome_recolor_by_attribute) do
+    recolor_by_attribute = Api::V3::RecolorByQual.
+      includes(:recolor_by_attribute).
+      where(
+        'recolor_by_attributes.context_id' => api_v3_context.id,
+        qual_id: api_v3_biome.id
+      ).first&.recolor_by_attribute
+    unless recolor_by_attribute
+      recolor_by_attribute = FactoryBot.create(
+        :api_v3_recolor_by_attribute,
+        tooltip_text: 'biome tooltip text',
+        context: api_v3_context,
+        position: 3,
+        years: [],
+        group_number: 1,
+        is_disabled: false,
+        is_default: false,
+        legend_type: 'qual',
+        legend_color_theme: 'thematic'
+      )
+      FactoryBot.create(
+        :api_v3_recolor_by_qual,
+        recolor_by_attribute: recolor_by_attribute,
+        qual: api_v3_biome
+      )
+    end
+    recolor_by_attribute
   end
 end
--- spec/support/contexts/api/v3/brazil/brazil_resize_by.rb
+++ spec/support/contexts/api/v3/brazil/brazil_resize_by.rb
@@ -2,43 +2,57 @@ shared_context 'api v3 brazil resize by' do
   include_context 'api v3 brazil contexts'
   include_context 'api v3 quants'
 
-  let!(:resize_by_attributes_area) do
-    FactoryBot.create(
-      :api_v3_resize_by_attribute,
-      tooltip_text: 'area tooltip text',
-      context: api_v3_context,
-      position: 1,
-      years: [],
-      group_number: 1,
-      is_disabled: false,
-      is_default: false
-    )
-  end
-  let!(:resize_by_quants_area) do
-    FactoryBot.create(
-      :api_v3_resize_by_quant,
-      resize_by_attribute: resize_by_attributes_area,
-      quant: api_v3_area
-    )
+  let!(:api_v3_area_resize_by_attribute) do
+    resize_by_attribute = Api::V3::ResizeByQuant.
+      includes(:resize_by_attribute).
+      where(
+        'resize_by_attributes.context_id' => api_v3_context.id,
+        quant_id: api_v3_area.id
+      ).first&.resize_by_attribute
+    unless resize_by_attribute
+      resize_by_attribute = FactoryBot.create(
+        :api_v3_resize_by_attribute,
+        tooltip_text: 'area tooltip text',
+        context: api_v3_context,
+        position: 1,
+        years: [],
+        group_number: 1,
+        is_disabled: false,
+        is_default: false
+      )
+      FactoryBot.create(
+        :api_v3_resize_by_quant,
+        resize_by_attribute: resize_by_attribute,
+        quant: api_v3_area
+      )
+    end
+    resize_by_attribute
   end
 
-  let!(:resize_by_attributes_land_conflict) do
-    FactoryBot.create(
-      :api_v3_resize_by_attribute,
-      tooltip_text: 'land conflict tooltip text',
-      context: api_v3_context,
-      position: 2,
-      years: [],
-      group_number: 1,
-      is_disabled: false,
-      is_default: false
-    )
-  end
-  let!(:resize_by_quants_land_conflict) do
-    FactoryBot.create(
-      :api_v3_resize_by_quant,
-      resize_by_attribute: resize_by_attributes_land_conflict,
-      quant: api_v3_land_conflicts
-    )
+  let!(:api_v3_land_conflict_resize_by_attribute) do
+    resize_by_attribute = Api::V3::ResizeByQuant.
+      includes(:resize_by_attribute).
+      where(
+        'resize_by_attributes.context_id' => api_v3_context.id,
+        quant_id: api_v3_land_conflicts.id
+      ).first&.resize_by_attribute
+    unless resize_by_attribute
+      resize_by_attribute = FactoryBot.create(
+        :api_v3_resize_by_attribute,
+        tooltip_text: 'land conflict tooltip text',
+        context: api_v3_context,
+        position: 2,
+        years: [],
+        group_number: 1,
+        is_disabled: false,
+        is_default: false
+      )
+      FactoryBot.create(
+        :api_v3_resize_by_quant,
+        resize_by_attribute: resize_by_attribute,
+        quant: api_v3_land_conflicts
+      )
+    end
+    resize_by_attribute
   end
 end
