e4cbfc591d571c43c00bd0ac764885a853d12bf1
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-01-29 14:12:57 +0000

    profiles admin interface

---
+++ app/admin/api/v3/profile.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Api::V3::Profile, as: 'Profile' do
+  menu parent: 'Yellow Tables'
+
+  permit_params :context_node_type_id, :name
+
+  form do |f|
+    f.semantic_errors
+    inputs do
+      input :context_node_type, as: :select, required: true,
+        collection: Api::V3::ContextNodeType.select_options
+      input :name, as: :string
+    end
+    f.actions
+  end
+
+  index do
+    column('Country') { |property| property.context_node_type&.context&.country&.name }
+    column('Commodity') { |property| property.context_node_type&.context&.commodity&.name }
+    column('Node Type') { |property| property.context_node_type&.node_type&.name }
+    column :name
+    actions
+  end
+
+  filter :context_node_type, collection: -> { Api::V3::ContextNodeType.
+    select_options }
+end
--- app/models/api/v3/profile.rb
+++ app/models/api/v3/profile.rb
@@ -1,7 +1,16 @@
 module Api
   module V3
     class Profile < YellowTable
+      NAME = [
+        'actor',
+        'place'
+      ].freeze
+
       belongs_to :context_node_type
+
+      validates :context_node_type, presence: true
+      validates :name, uniqueness: {scope: :context_node_type},
+        inclusion: NAME
 
       def self.blue_foreign_keys
         [
