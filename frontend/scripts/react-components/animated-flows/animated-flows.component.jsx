6257663732e04eb0122899ae446190f764ee0f6a
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-01-29 15:20:37 +0100

    fix: eslint compliance

--- frontend/scripts/react-components/animated-flows/animated-flows.component.jsx
+++ frontend/scripts/react-components/animated-flows/animated-flows.component.jsx
@@ -1,11 +1,9 @@
-/* eslint-disable */
+/* eslint-disable camelcase,no-var,vars-on-top,no-shadow */
 import React from 'react';
 import debounce from 'lodash/debounce';
 import { select as d3_select, interval as d3_interval, easeLinear } from 'd3';
 import d3_sankey from './sankey';
 import energy from './energy.json';
-import 'd3-transition';
-
 
 class AnimatedFlows extends React.PureComponent {
   constructor(props) {
@@ -38,52 +36,49 @@ class AnimatedFlows extends React.PureComponent {
     this.setState({
       width: parseInt(width, 10),
       height: parseInt(height, 10)
-    })
+    });
   }
 
   build() {
     const { width, height } = this.state;
     this.container.innerHTML = '';
 
-    var svg = d3_select(this.container)
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g");
+    const svg = d3_select(this.container)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g');
 
-    var sankey = d3_sankey(width)
+    const sankey = d3_sankey(width)
       .nodeWidth(2)
       .nodePadding(10)
       .size([width, height]);
 
-    var path = sankey.link();
+    const path = sankey.link();
 
     sankey
       .nodes(energy.nodes)
       .links(energy.links)
       .layout(32);
 
-    function setDash(d) {
-      var d3this = d3_select(this);
-      var totalLength = d3this.node().getTotalLength();
+    function setDash() {
+      const d3this = d3_select(this);
+      const totalLength = d3this.node().getTotalLength();
       d3this
-        .attr('stroke-dasharray', totalLength + ' ' + totalLength)
-        .attr('stroke-dashoffset', totalLength)
+        .attr('stroke-dasharray', `${totalLength} ${totalLength}`)
+        .attr('stroke-dashoffset', totalLength);
     }
 
-    var gradientLink = svg.append("g").selectAll(".gradient-link")
+    var gradientLink = svg.append('g').selectAll('.gradient-link')
       .data(energy.links)
-      .enter().append("path")
-      .attr("class", "gradient-link")
-      .attr("d", path)
-      .style("stroke-width", function(d) {
-        return Math.max(1, d.dy);
-      })
-      .sort(function(a, b) {
-        return b.dy - a.dy;
-      })
+      .enter()
+      .append('path')
+      .attr('class', 'gradient-link')
+      .attr('d', path)
+      .style('stroke-width', d => (Math.max(1, d.dy)))
+      .sort((a, b) => (b.dy - a.dy))
       .each(setDash)
-      .style('stroke', function(d) {
+      .style('stroke', () => {
         var id = 'c-000-to-000';
         if (!svg.select(id).node()) {
           var gradient = svg.append('defs')
@@ -97,71 +92,61 @@ class AnimatedFlows extends React.PureComponent {
 
           gradient.append('stop')
             .attr('offset', '0%')
-            .attr('stop-color', "#000")
-            .attr('stop-opacity', .1);
+            .attr('stop-color', '#000')
+            .attr('stop-opacity', 0.1);
 
           gradient.append('stop')
             .attr('offset', '100%')
-            .attr('stop-color', "#000")
-            .attr('stop-opacity', .3);
+            .attr('stop-color', '#000')
+            .attr('stop-opacity', 0.3);
         }
-        return "url(#" + id + ")";
+        return `url(#${id})`;
       });
 
-    var link = svg.append("g").selectAll(".link")
+    svg.append('g')
+      .selectAll('.link')
       .data(energy.links)
-      .enter().append("path")
-      .attr("class", "link")
-      .attr("d", path)
-      .style("stroke-width", function(d) {
-        return Math.max(1, d.dy);
-      })
-      .sort(function(a, b) {
-        return b.dy - a.dy;
-      });
+      .enter()
+      .append('path')
+      .attr('class', 'link')
+      .attr('d', path)
+      .style('stroke-width', d => (Math.max(1, d.dy)))
+      .sort((a, b) => (b.dy - a.dy));
 
     function branchAnimate(nodeData) {
-      var links = svg.selectAll(".gradient-link")
-        .filter(function(gradientD) {
-          return nodeData.sourceLinks.indexOf(gradientD) > -1
-        });
+      var links = svg.selectAll('.gradient-link')
+        .filter(gradientD => (nodeData.sourceLinks.indexOf(gradientD) > -1));
       var nextLayerNodeData = [];
-      links.each(function(d) {
-        nextLayerNodeData.push(d.target);
-      });
+      links.each(d => nextLayerNodeData.push(d.target));
 
       links
-        .style("opacity", null)
+        .style('opacity', null)
         .transition()
         .duration(1500)
         .ease(easeLinear)
         .attr('stroke-dashoffset', 0)
-        .on('end', function() {
-          nextLayerNodeData.forEach(function(d) {
-            branchAnimate(d);
-          });
-        });
+        .on('end', () => nextLayerNodeData.forEach(d => branchAnimate(d)));
     }
-    var node = svg.append("g").selectAll(".node")
+    var node = svg.append('g')
+      .selectAll('.node')
       .data(energy.nodes)
-      .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
-      })
+      .enter()
+      .append('g')
+      .attr('class', 'node')
+      .attr('transform', d => (`translate(${d.x}, ${d.y})`));
 
     function cleanBranches() {
-      //cancel all transitions by making a new one
+      // cancel all transitions by making a new one
       gradientLink.transition();
       gradientLink
-        .style("opacity", 0)
-        .each(function(d) {
+        .style('opacity', 0)
+        .each(function gradientSetDash(d) {
           setDash.call(this, d);
         });
     }
 
     function animateBranch() {
-      var r = Math.floor(Math.random() * (energy.nodes.length + 1) + 0);
+      const r = Math.floor(Math.random() * (energy.nodes.length + 1));
       var node = energy.nodes[r];
       if (node && node.sourceLinks.length > 0 && node.x === 0) {
         branchAnimate(node);
@@ -170,28 +155,27 @@ class AnimatedFlows extends React.PureComponent {
       }
     }
 
-    node.append("rect")
-      .attr("height", function(d) {
-        return d.dy;
-      })
-      .attr("width", sankey.nodeWidth())
-      .style("opacity", 0);
+    node.append('rect')
+      .attr('height', d => (d.dy))
+      .attr('width', sankey.nodeWidth())
+      .style('opacity', 0);
 
     animateBranch();
-    d3_interval(function(){
+    d3_interval(
+      () => {
         cleanBranches();
-        animateBranch()
-      }
-      , 10000);
-    }
+        animateBranch();
+      },
+      10000
+    );
+  }
 
   render() {
     return (
       <div
-        ref={ref => { this.container = ref; }}
+        ref={(ref) => { this.container = ref; }}
         className="c-animated-flows"
-      >
-      </div>
+      />
     );
   }
 }
