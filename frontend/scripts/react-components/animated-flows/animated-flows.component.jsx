e4faa6b83dd907b702d7c7f3cd1e48297a09dc7f
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-01-29 14:10:56 +0100

    fix: animated flows transition without react-faux-dom

--- frontend/scripts/react-components/animated-flows/animated-flows.component.jsx
+++ frontend/scripts/react-components/animated-flows/animated-flows.component.jsx
@@ -1,51 +1,92 @@
 /* eslint-disable */
 import React from 'react';
-import { withFauxDOM } from 'react-faux-dom';
-import * as d3 from 'd3';
-import d3Sankey from './sankey';
+import debounce from 'lodash/debounce';
+import { select as d3_select, interval as d3_interval, easeLinear } from 'd3';
+import d3_sankey from './sankey';
 import energy from './energy.json';
+import 'd3-transition';
 
-d3.sankey = d3Sankey;
 
 class AnimatedFlows extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: null,
+      height: null
+    };
+
+    this.build = this.build.bind(this);
+    this.update = debounce(this.update.bind(this), 350);
+  }
+
   componentDidMount() {
-    const width = parseInt(window.innerWidth);
-    const height = parseInt(600);
+    window.addEventListener('resize', this.update);
+    this.update();
+  }
 
-    const component = this;
+  componentDidUpdate() {
+    this.build();
+  }
 
-    const sankey = d3.sankey(width)
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.build);
+  }
+
+  update() {
+    const parent = this.container.parentNode;
+    const { height, width } = window.getComputedStyle(parent);
+    this.setState({
+      width: parseInt(width, 10),
+      height: parseInt(height, 10)
+    })
+  }
+
+  build() {
+    const { width, height } = this.state;
+    this.container.innerHTML = '';
+
+    var svg = d3_select(this.container)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g");
+
+    var sankey = d3_sankey(width)
       .nodeWidth(2)
       .nodePadding(10)
       .size([width, height]);
 
-    const path = sankey.link();
+    var path = sankey.link();
 
     sankey
       .nodes(energy.nodes)
       .links(energy.links)
       .layout(32);
 
-    const chart = this.props.connectFauxDOM('div', 'chart');
-    chart.setAttribute('class', 'flowsContainer');
-    const svg = d3.select(chart).append("svg")
-       .attr("width", width)
-       .attr("height", height)
-       .append("g");
+    function setDash(d) {
+      var d3this = d3_select(this);
+      var totalLength = d3this.node().getTotalLength();
+      d3this
+        .attr('stroke-dasharray', totalLength + ' ' + totalLength)
+        .attr('stroke-dashoffset', totalLength)
+    }
 
-    const gradientLink = svg.append("g")
-      .selectAll(".gradient-link")
+    var gradientLink = svg.append("g").selectAll(".gradient-link")
       .data(energy.links)
       .enter().append("path")
       .attr("class", "gradient-link")
       .attr("d", path)
-      .style("stroke-width", d => Math.max(1, d.dy))
-      .sort((a, b) => (b.dy - a.dy))
+      .style("stroke-width", function(d) {
+        return Math.max(1, d.dy);
+      })
+      .sort(function(a, b) {
+        return b.dy - a.dy;
+      })
       .each(setDash)
-      .style('stroke', (d) => {
-        const id = 'c-000-to-000';
-        if (!svg.select(id)._groups[0][0]) {
-          const gradient = svg.append('defs')
+      .style('stroke', function(d) {
+        var id = 'c-000-to-000';
+        if (!svg.select(id).node()) {
+          var gradient = svg.append('defs')
             .append('linearGradient')
             .attr('id', id)
             .attr('x1', '0%')
@@ -67,95 +108,92 @@ class AnimatedFlows extends React.PureComponent {
         return "url(#" + id + ")";
       });
 
-    const link = svg.append("g").selectAll(".link")
+    var link = svg.append("g").selectAll(".link")
       .data(energy.links)
       .enter().append("path")
       .attr("class", "link")
       .attr("d", path)
-      .style("stroke-width", d => Math.max(1, d.dy))
-      .sort((a, b) => (b.dy - a.dy));
-
-    const node = svg.append("g")
-      .selectAll(".node")
-      .data(energy.nodes)
-      .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", d => ("translate(" + d.x + "," + d.y + ")"));
-
-    node.append("rect")
-      .attr("height", d => (d.dy))
-      .attr("width", sankey.nodeWidth())
-      .style("opacity", 0);
-
-    function setDash(d) {
-      const d3this = d3.select(this);
-      const totalLength = d3this.node().style
-        .getProperty('totalLength');
-      d3this
-        .attr('stroke-dasharray', totalLength + ' ' + totalLength)
-        .attr('stroke-dashoffset', totalLength)
-    }
+      .style("stroke-width", function(d) {
+        return Math.max(1, d.dy);
+      })
+      .sort(function(a, b) {
+        return b.dy - a.dy;
+      });
 
     function branchAnimate(nodeData) {
-
-      const links = svg.selectAll(".gradient-link")
-        .filter((gradientD) => (nodeData.sourceLinks.indexOf(gradientD) > -1));
-      const nextLayerNodeData = [];
-      links.each(d => nextLayerNodeData.push(d.target));
+      var links = svg.selectAll(".gradient-link")
+        .filter(function(gradientD) {
+          return nodeData.sourceLinks.indexOf(gradientD) > -1
+        });
+      var nextLayerNodeData = [];
+      links.each(function(d) {
+        nextLayerNodeData.push(d.target);
+      });
 
       links
         .style("opacity", null)
         .transition()
         .duration(1500)
-        .ease(d3.easeLinear)
+        .ease(easeLinear)
         .attr('stroke-dashoffset', 0)
-        .on('end', () => {
-          nextLayerNodeData.forEach(branchAnimate);
+        .on('end', function() {
+          nextLayerNodeData.forEach(function(d) {
+            branchAnimate(d);
+          });
         });
     }
+    var node = svg.append("g").selectAll(".node")
+      .data(energy.nodes)
+      .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      })
 
     function cleanBranches() {
       //cancel all transitions by making a new one
-      if (component.props.isAnimatingFauxDOM()) {
-        component.props.stopAnimatingFauxDOM();
-      }
       gradientLink.transition();
       gradientLink
         .style("opacity", 0)
-        .each(d => setDash.call(this, d));
+        .each(function(d) {
+          setDash.call(this, d);
+        });
     }
 
     function animateBranch() {
-      if (!component.props.isAnimatingFauxDOM()) {
-        component.props.animateFauxDOM(10100);
-      }
-      const r = Math.floor(Math.random() * (energy.nodes.length + 1));
-      const node = energy.nodes[r];
-      if (!!node.sourceLinks.length && node.x === 0) {
+      var r = Math.floor(Math.random() * (energy.nodes.length + 1) + 0);
+      var node = energy.nodes[r];
+      if (node && node.sourceLinks.length > 0 && node.x === 0) {
         branchAnimate(node);
       } else {
         animateBranch();
       }
     }
 
-    animateBranch();
+    node.append("rect")
+      .attr("height", function(d) {
+        return d.dy;
+      })
+      .attr("width", sankey.nodeWidth())
+      .style("opacity", 0);
 
-    setInterval(
-      () => {
+    animateBranch();
+    d3_interval(function(){
         cleanBranches();
-        animateBranch();
-      },
-      10000
-    );
-  }
+        animateBranch()
+      }
+      , 10000);
+    }
 
   render() {
     return (
-      <div className="c-animated-flows">
-        {this.props.chart}
+      <div
+        ref={ref => { this.container = ref; }}
+        className="c-animated-flows"
+      >
       </div>
     );
   }
 }
 
-export default withFauxDOM(AnimatedFlows);
+export default AnimatedFlows;
