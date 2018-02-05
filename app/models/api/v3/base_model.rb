fb7e9a7205ce8a936c0769d945936e81a9153b57
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-02-05 11:01:15 +0000

    Fixed ineffective private modifier

--- app/models/api/v3/base_model.rb
+++ app/models/api/v3/base_model.rb
@@ -6,33 +6,39 @@ module Api
 
       establish_connection DB_REVAMP
 
-      private
+      class << self
+        def column_comment(column_name)
+          tmp = column_comments.find do |cc|
+            cc['name'] == column_name
+          end
+          tmp && tmp['comment']
+        end
 
-      def self.all_column_comments
-        unless defined?(@@all_comments)
-          data = YAML.safe_load(
+        private
+
+        def all_column_comments
+          unless defined?(@@all_comments)
+            data = load_from_file
+            @@all_comments = data && data['tables']
+          end
+          @@all_comments
+        end
+
+        def column_comments
+          unless @comments
+            tmp = all_column_comments.find do |cc|
+              cc['name'] == table_name
+            end
+            @comments = tmp && tmp['columns']
+          end
+          @comments
+        end
+
+        def load_from_file
+          YAML.safe_load(
             File.open("#{Rails.root}/db/schema_comments.yml")
           )
-          @@all_comments = data && data['tables']
         end
-        @@all_comments
-      end
-
-      def self.column_comments
-        unless @comments
-          tmp = all_column_comments.find do |cc|
-            cc['name'] == table_name
-          end
-          @comments = tmp && tmp['columns']
-        end
-        @comments
-      end
-
-      def self.column_comment(column_name)
-        tmp = column_comments.find do |cc|
-          cc['name'] == column_name
-        end
-        tmp && tmp['comment']
       end
     end
   end
