d8ad42bf00023fdd90d14bbc892637465d3890dd
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-02-09 16:18:05 +0100

    import toolInitialState instead of repeating

--- frontend/scripts/__tests__/utils/stateURL.test.js
+++ frontend/scripts/__tests__/utils/stateURL.test.js
@@ -1,58 +1,5 @@
 import { filterStateToURL } from '../../utils/stateURL';
-
-const initialState = {
-  areNodesExpanded: false,
-  choropleth: {},
-  choroplethLegend: null,
-  columns: [],
-  contexts: [],
-  currentHighlightedChoroplethBucket: null,
-  currentQuant: null,
-  detailedView: false,
-  expandedMapSidebarGroupsIds: [],
-  expandedNodesIds: [],
-  forcedOverview: false,
-  geoIdsDict: [],
-  highlightedGeoIds: [],
-  highlightedNodeCoordinates: [],
-  highlightedNodeData: [],
-  highlightedNodesIds: [],
-  initialDataLoading: false,
-  isMapVisible: false,
-  linkedGeoIds: [],
-  links: [],
-  linksLoading: false,
-  mapContextualLayers: [],
-  mapDimensions: [],
-  mapDimensionsGroups: [],
-  mapVectorData: null,
-  mapView: null,
-  nodes: [],
-  nodesColoredAtColumn: null,
-  nodesColoredBySelection: null,
-  nodesDict: null,
-  nodesDictWithMeta: [],
-  recolorByNodeIds: [],
-  recolorGroups: [],
-  selectedBiomeFilter: { value: 'none', name: 'none' },
-  selectedColumnsIds: [],
-  selectedContext: null,
-  selectedContextId: null,
-  selectedMapBasemap: null,
-  selectedMapContextualLayers: null,
-  selectedMapDimensions: [null, null],
-  selectedMapDimensionsWarnings: null,
-  selectedNodesColumnsPos: [],
-  selectedNodesData: [],
-  selectedNodesGeoIds: [],
-  selectedNodesIds: [],
-  selectedRecolorBy: { type: 'none', name: 'none' },
-  selectedResizeBy: { type: 'none', name: 'none' },
-  selectedYears: [],
-  unmergedLinks: [],
-  visibleNodes: [],
-  visibleNodesByColumn: []
-};
+import { toolInitialState } from '../../reducers/tool.reducer';
 
 test('filterStateToURL should return only URL_PARAMS_PROPS', () => {
   const filteredState = {
@@ -74,15 +21,14 @@ test('filterStateToURL should return only URL_PARAMS_PROPS', () => {
     selectedBiomeFilterName: 'none' // remove after merge
   };
 
-  const completeState = { ...initialState };
-  expect(filterStateToURL(completeState)).toEqual(filteredState);
+  expect(filterStateToURL(toolInitialState)).toEqual(filteredState);
 
-  const incompleteState = {
-    ...initialState,
-    selectedResizeBy: undefined,
-    selectedRecolorBy: undefined,
-    selectedBiomeFilter: undefined
-  };
+  const {
+    selectedResizeBy,
+    selectedRecolorBy,
+    selectedBiomeFilter,
+    ...incompleteState // object rest assignment do not remove unused!
+  } = toolInitialState;
   expect(filterStateToURL(incompleteState)).toEqual({
     ...filteredState,
     selectedResizeByName: null,
