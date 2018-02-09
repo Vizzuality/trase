4b9ec00c34928995edc34ecab8c2d19b6782e184
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-02-09 17:46:41 +0100

    fix: in-app navigation broke stuff

--- frontend/scripts/react-components/team/team.component.jsx
+++ frontend/scripts/react-components/team/team.component.jsx
@@ -6,37 +6,40 @@ import cx from 'classnames';
 
 const Team = props => {
   const { groups, members } = props;
+
   return (
-    <div className="c-team">
-      {groups.map(group => (
-        <section className="team-group" key={group.name}>
-          <h3 className="subtitle">{group.name}</h3>
-          <div className="team-list">
-            <div className="row -equal-height">
-              {group.staffMembers.map(slug => (
-                <div
-                  key={members[slug].position + members[slug].name}
-                  className="column small-12 medium-4"
-                >
-                  <div className="team-list-item">
-                    <Link to={{ type: 'teamMember', payload: { member: kebabCase(slug) } }}>
-                      <div
-                        className={cx('c-team-profile-picture', {
-                          '-placeholder': !members[slug].smallImageUrl
-                        })}
-                        style={{ backgroundImage: `url(${members[slug].smallImageUrl})` }}
-                      />
-                      <h3 className="team-list-item-title title -medium">{members[slug].name}</h3>
-                      <span className="team-list-item-subtitle subtitle -gray">See More</span>
-                    </Link>
+    groups && (
+      <div className="c-team">
+        {groups.map(group => (
+          <section className="team-group" key={group.name}>
+            <h3 className="subtitle">{group.name}</h3>
+            <div className="team-list">
+              <div className="row -equal-height">
+                {group.staffMembers.map(slug => (
+                  <div
+                    key={members[slug].position + members[slug].name}
+                    className="column small-12 medium-4"
+                  >
+                    <div className="team-list-item">
+                      <Link to={{ type: 'teamMember', payload: { member: kebabCase(slug) } }}>
+                        <div
+                          className={cx('c-team-profile-picture', {
+                            '-placeholder': !members[slug].smallImageUrl
+                          })}
+                          style={{ backgroundImage: `url(${members[slug].smallImageUrl})` }}
+                        />
+                        <h3 className="team-list-item-title title -medium">{members[slug].name}</h3>
+                        <span className="team-list-item-subtitle subtitle -gray">See More</span>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
-        </section>
-      ))}
-    </div>
+          </section>
+        ))}
+      </div>
+    )
   );
 };
 
@@ -56,11 +59,6 @@ Team.propTypes = {
       bio: PropTypes.string.isRequired
     })
   )
-};
-
-Team.defaultProps = {
-  groups: [],
-  members: {}
 };
 
 export default Team;
