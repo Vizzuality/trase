309723ddffb04ced484867e93954ba37e0739fb2
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-02-14 09:56:24 +0100

    Added unique locks for database jobs with expiration matching the typical running time

--- app/workers/database_update_worker.rb
+++ app/workers/database_update_worker.rb
@@ -1,6 +1,11 @@
 class DatabaseUpdateWorker
   include Sidekiq::Worker
-  sidekiq_options retry: 0, backtrace: true, unique: :until_and_while_executing
+  sidekiq_options queue: :database,
+                  retry: 0,
+                  backtrace: true,
+                  unique: :until_executed,
+                  run_lock_expiration: 60 * 60 * 2, # 2 hrs
+                  log_duplicate_payload: true
 
   def perform(database_update_id)
     database_update = Api::V3::DatabaseUpdate.find(database_update_id)
--- app/workers/download_flows_refresh_worker.rb
+++ app/workers/download_flows_refresh_worker.rb
@@ -1,6 +1,11 @@
 class DownloadFlowsRefreshWorker
   include Sidekiq::Worker
-  sidekiq_options retry: 0, backtrace: true, unique: :until_and_while_executing
+  sidekiq_options queue: :database,
+                  retry: 0,
+                  backtrace: true,
+                  unique: :until_and_while_executing,
+                  run_lock_expiration: 150, # 2.5 mins
+                  log_duplicate_payload: true
 
   def perform(options)
     Api::V3::Readonly::DownloadFlow.refresh(options)
