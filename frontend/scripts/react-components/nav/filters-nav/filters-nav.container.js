8ef8d03327c8142c883e5c358a959000c5bec925
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-02-21 17:26:35 +0100

    feat: load initial data in explore page

--- frontend/scripts/react-components/nav/filters-nav/filters-nav.container.js
+++ frontend/scripts/react-components/nav/filters-nav/filters-nav.container.js
@@ -2,9 +2,10 @@ import { connect } from 'react-redux';
 import routerLinks from 'router/nav-links';
 import FiltersNav from './filters-nav.component';
 
-function mapStateToProps() {
+function mapStateToProps(state) {
   return {
-    links: routerLinks.nav
+    links: routerLinks.nav,
+    selectedContext: state.tool.selectedContext
   };
 }
 
