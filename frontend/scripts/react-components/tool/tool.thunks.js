f8a7f670c50737d6acfba800c648230d9dca5606
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-02-22 19:03:01 +0100

    fix: somewhat optimize thunks

--- frontend/scripts/react-components/tool/tool.thunks.js
+++ frontend/scripts/react-components/tool/tool.thunks.js
@@ -1,16 +1,24 @@
 import { loadInitialData } from 'actions/tool.actions';
 import { displayStoryModal, loadDisclaimer, resize } from 'actions/app.actions';
 
-export const loadDisclaimerTool = dispatch => dispatch(loadDisclaimer());
-export const loadInitialDataTool = dispatch =>
-  // const { app, tool } = getState();
+const shouldDispatch = getState => {
+  const { type, prev } = getState().location;
+  return type === 'tool' && prev.type !== 'tool';
+};
+
+export const loadDisclaimerTool = (dispatch, getState) =>
+  shouldDispatch(getState) && dispatch(loadDisclaimer());
+
+export const loadInitialDataTool = (dispatch, getState) =>
   // TODO: custom container prevents from optimizing this
   // currently we need to load initial data everytime we enter the page
   // the implementation of the vanilla redux container doesn't pass existing props to the sankey
   // passes callbacks that after execution assign values to local variables
-  // const hasInitialData = app.tooltips && tool.contexts.length > 0;
-  dispatch(loadInitialData());
-export const resizeSankeyTool = dispatch => dispatch(resize());
+  shouldDispatch(getState) && dispatch(loadInitialData());
+
+export const resizeSankeyTool = (dispatch, getState) =>
+  shouldDispatch(getState) && dispatch(resize());
+
 export const loadStoryModalTool = (dispatch, getState) => {
   const { query } = getState().location;
   return query.story && dispatch(displayStoryModal(query.story));
