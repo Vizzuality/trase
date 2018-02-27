6234dea5ac3303bc0971fe1016ffbeada2028509
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-02-27 13:51:44 +0000

    Fixed Qual dictionary

--- lib/dictionary/qual.rb
+++ lib/dictionary/qual.rb
@@ -2,7 +2,7 @@ module Dictionary
   class Qual < Attribute
     def find_by_name(name)
       Api::V3::Qual.
-        select(:id, :name, 'qual_properties.display_name', 'qual_properties.tooltip_text', :unit).
+        select(:id, :name, 'qual_properties.display_name', 'qual_properties.tooltip_text').
         joins(:qual_property).
         where(name: name).
         first
