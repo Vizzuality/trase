86dd54ed5c490e9210e6676adc1ad504d23cea73
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-03-02 19:33:24 +0100

    feat: includes loading placeholder

--- frontend/scripts/react-components/profiles/top.component.jsx
+++ frontend/scripts/react-components/profiles/top.component.jsx
@@ -7,37 +7,41 @@ import 'styles/components/shared/top.scss';
 class Top extends Component {
   renderList() {
     const { data, targetLink, year } = this.props;
-    return data.map((item, index) => {
-      const itemValue = Array.isArray(item.values)
-        ? formatValue(item.values[0] * 100, 'percentage')
-        : formatValue(item.value * 100, 'percentage');
+    return data.length > 0 ? (
+      data.map((item, index) => {
+        const itemValue = Array.isArray(item.values)
+          ? formatValue(item.values[0] * 100, 'percentage')
+          : formatValue(item.value * 100, 'percentage');
 
-      return (
-        <li key={index} className="top-item">
-          <span className="item-name">
-            <span className="node-name">{item.name}</span>
-            {this.props.targetLink &&
-              !item.is_domestic_consumption && (
-                <Link
-                  className="outside-link"
-                  to={{ type: targetLink, payload: { query: { nodeId: item.id, year } } }}
-                >
-                  <svg className="icon icon-outside-link">
-                    <use xlinkHref="#icon-outside-link" />
-                  </svg>
-                </Link>
-              )}
-          </span>
-          {this.props.unit ? (
-            <span className="item-value" data-unit={this.props.unit}>
-              {itemValue}
+        return (
+          <li key={index} className="top-item">
+            <span className="item-name">
+              <span className="node-name">{item.name}</span>
+              {this.props.targetLink &&
+                !item.is_domestic_consumption && (
+                  <Link
+                    className="outside-link"
+                    to={{ type: targetLink, payload: { query: { nodeId: item.id, year } } }}
+                  >
+                    <svg className="icon icon-outside-link">
+                      <use xlinkHref="#icon-outside-link" />
+                    </svg>
+                  </Link>
+                )}
             </span>
-          ) : (
-            <span className="item-value">{itemValue}</span>
-          )}
-        </li>
-      );
-    });
+            {this.props.unit ? (
+              <span className="item-value" data-unit={this.props.unit}>
+                {itemValue}
+              </span>
+            ) : (
+              <span className="item-value">{itemValue}</span>
+            )}
+          </li>
+        );
+      })
+    ) : (
+      <TopPlaceholder width={450} />
+    );
   }
 
   render() {
@@ -49,6 +53,35 @@ class Top extends Component {
     );
   }
 }
+
+const TopPlaceholder = props => {
+  const sizes = ['small', 'medium', 'big', 'small-2', 'medium-2', 'big-2'];
+  const getIndex = i => parseInt(Math.random() * 100 / (i || 1), 10) % 3;
+  return Array(10)
+    .fill(0)
+    .map((row, i) => (
+      <li
+        key={`list-item-${i}`}
+        className="placeholder-list-item"
+        style={{ minWidth: props.width }}
+      >
+        <div className="placeholder-start">
+          {Array(3)
+            .fill(0)
+            .map((elemRow, index) => (
+              <span
+                key={`item-${i}-element-${index}`}
+                className={`placeholder-element -${sizes[getIndex(index)]}`}
+              />
+            ))}
+          <svg className="icon icon-outside-link">
+            <use xlinkHref="#icon-outside-link" />
+          </svg>
+        </div>
+        <div className={`placeholder-end -${sizes[getIndex(i)]}`} />
+      </li>
+    ));
+};
 
 Top.propTypes = {
   data: PropTypes.array.isRequired,
