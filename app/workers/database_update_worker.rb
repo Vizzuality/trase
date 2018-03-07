da923f2daf3643718036dbeea1345c8743ba519e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-03-07 18:01:51 +0000

    Moved transaction logic inside the Importer class and fixed rollback

--- app/workers/database_update_worker.rb
+++ app/workers/database_update_worker.rb
@@ -10,15 +10,6 @@ class DatabaseUpdateWorker
   def perform(database_update_id)
     database_update = Api::V3::DatabaseUpdate.find(database_update_id)
     database_update.update_attributes(jid: self.jid) # make sure validations run here
-    ActiveRecord::Base.transaction do
-      Api::V3::Import::Importer.new.call(database_update)
-    end
-    database_update.update_attribute(:status, Api::V3::DatabaseUpdate::FINISHED)
-  rescue => e
-    database_update.update_attribute(:status, Api::V3::DatabaseUpdate::FAILED)
-    database_update.update_attribute(:error, e.message)
-    Rails.logger.error e.message
-    Appsignal.send_error(e)
-    return
+    Api::V3::Import::Importer.new.call(database_update)
   end
 end
