991b273150644fbb953c3ede5ac18e1ddb1d06e9
Tiago Garcia <tiagojsag@gmail.com>
2018-03-08 18:11:21 +0100

    Remove robots.txt from prod config

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, '/var/www/trase'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, '.env', 'frontend/.env', 'frontend/dist/robots.txt'
+append :linked_files, '.env', 'frontend/.env'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,6 @@
 server 'staging.trase.earth', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, forward_agent: true
 
+append :linked_files, 'frontend/dist/robots.txt'
+
 set :branch, 'develop'
