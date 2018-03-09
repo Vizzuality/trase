c6178daaf4506ca428d1c0665d0d7a98d0922f11
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-03-09 14:27:17 +0100

    feat: includes mobile version of entrypoints

--- frontend/scripts/react-components/home/entrypoints.component.jsx
+++ frontend/scripts/react-components/home/entrypoints.component.jsx
@@ -1,16 +1,58 @@
 import React from 'react';
 import Siema from 'react-siema';
 import Link from 'redux-first-router-link';
+import debounce from 'lodash/debounce';
 
 class Entrypoints extends React.PureComponent {
+  static getIsMobile() {
+    return window.innerWidth <= 700;
+  }
   constructor(props) {
     super(props);
     this.state = {
-      currentSlide: 0
+      currentSlide: 0,
+      isMobile: Entrypoints.getIsMobile()
     };
+    this.entrypoints = [
+      {
+        link: { type: 'profileRoot' },
+        subtitle: 'Profile',
+        text:
+          'View the trade and sustainability profile of a particular' +
+          ' company or production region.',
+        className: '-start'
+      },
+      {
+        link: { type: 'tool' },
+        subtitle: 'Supply Chain',
+        text:
+          'Follow trade flows to identify sourcing regions, profile supply chain risks and' +
+          ' assess opportunities for sustainable production.',
+        className: '-mid'
+      },
+      {
+        link: { type: 'tool', payload: { state: { isMapVisible: true } } },
+        subtitle: 'Map',
+        text:
+          'Explore the sustainability of different production regions and identify risks and' +
+          ' opportunities facing downstream buyers.',
+        className: '-end'
+      }
+    ];
     this.getSliderRef = this.getSliderRef.bind(this);
     this.onClickPrev = this.onClickPrev.bind(this);
     this.onClickNext = this.onClickNext.bind(this);
+    this.onResize = debounce(this.onResize.bind(this), 300);
+    this.renderEntrypoints = this.renderEntrypoints.bind(this);
+    this.renderSlider = this.renderSlider.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
   }
 
   onClickPrev() {
@@ -23,63 +65,53 @@ class Entrypoints extends React.PureComponent {
     this.setState({ currentSlide: this.slider.currentSlide });
   }
 
+  onResize() {
+    this.setState({ isMobile: Entrypoints.getIsMobile() });
+  }
+
   getSliderRef(el) {
     this.slider = el;
   }
 
-  render() {
+  renderEntrypoints(grid) {
+    return this.entrypoints.map(slide => (
+      <div className={grid}>
+        <div key={slide.subtitle} className={`entrypoint-slide ${slide.className}`}>
+          <Link to={slide.link}>
+            <div className="entrypoint-slide-content">
+              <h3 className="subtitle">{slide.subtitle}</h3>
+              <p className="entrypoint-text">{slide.text}</p>
+            </div>
+          </Link>
+        </div>
+      </div>
+    ));
+  }
+
+  renderSlider() {
     const { currentSlide } = this.state;
+    return (
+      <React.Fragment>
+        <Siema loop={false} perPage={2.15} ref={this.getSliderRef}>
+          {this.renderEntrypoints('column small-6')}
+        </Siema>
+        {currentSlide > 0 && (
+          <button className="entrypoint-button -prev" onClick={this.onClickPrev} />
+        )}
+
+        {currentSlide === 0 && (
+          <button className="entrypoint-button -next" onClick={this.onClickNext} />
+        )}
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    const { isMobile } = this.state;
     return (
       <div className="c-entrypoints">
         <div className="row">
-          <Siema loop={false} perPage={2.15} ref={this.getSliderRef}>
-            <div className="column small-6">
-              <div className="entrypoint-slide -start">
-                <Link to={{ type: 'profileRoot' }}>
-                  <div className="entrypoint-slide-content">
-                    <h3 className="subtitle">Profile</h3>
-                    <p className="entrypoint-text">
-                      View the trade and sustainability profile of a particular company or
-                      production region.
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="column small-6">
-              <div className="entrypoint-slide -mid">
-                <Link to={{ type: 'profileRoot' }}>
-                  <div className="entrypoint-slide-content">
-                    <h3 className="subtitle">Supply Chain</h3>
-                    <p className="entrypoint-text">
-                      Follow trade flows to identify sourcing regions, profile supply chain risks
-                      and assess opportunities for sustainable production.
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            </div>
-            <div className="column small-6">
-              <div className="entrypoint-slide -end">
-                <Link to={{ type: 'profileRoot' }}>
-                  <div className="entrypoint-slide-content">
-                    <h3 className="subtitle">Profile</h3>
-                    <p className="entrypoint-text">
-                      Explore the sustainability of different production regions and identify risks
-                      and opportunities facing downstream buyers.
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </Siema>
-          {currentSlide > 0 && (
-            <button className="entrypoint-button -prev" onClick={this.onClickPrev} />
-          )}
-
-          {currentSlide === 0 && (
-            <button className="entrypoint-button -next" onClick={this.onClickNext} />
-          )}
+          {isMobile ? this.renderEntrypoints() : this.renderSlider('column small-12')}
         </div>
       </div>
     );
