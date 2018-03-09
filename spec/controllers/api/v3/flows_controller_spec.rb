a92a78cdc09c9120939f3a4677fb222dae23cb1c
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-03-09 14:27:02 +0000

    Check for presence of required flows paremeters and fail accordingly

---
+++ spec/controllers/api/v3/flows_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Api::V3::FlowsController, type: :controller do
+  include_context 'api v3 brazil flows quants'
+
+  describe 'GET index' do
+    let(:node_types) {
+      [
+        api_v3_municipality_node_type,
+        api_v3_exporter_node_type,
+        api_v3_importer_node_type,
+        api_v3_country_node_type
+      ]
+    }
+    let(:filter_params) {
+      {
+        year_start: 2015,
+        year_end: 2015,
+        include_columns: node_types.map(&:id),
+        flow_quant: api_v3_volume.name,
+        limit: 1
+      }
+    }
+    context 'when context without node types' do
+      let(:context) { FactoryBot.create(:api_v3_context) }
+
+      it 'returns 400' do
+        get :index, params: {
+          context_id: context.id
+        }.merge(filter_params)
+        expect(response).to have_http_status(400)
+      end
+    end
+  end
+end
