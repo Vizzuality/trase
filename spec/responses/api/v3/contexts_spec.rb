36206677fa75f5f14baf453fcdff6c7f87405d85
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-03-12 13:52:07 +0000

    Remove dependency on SchemaRevamp.refresh from specs

--- spec/responses/api/v3/contexts_spec.rb
+++ spec/responses/api/v3/contexts_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Get contexts', type: :request do
+  before do
+    Api::V3::DownloadAttribute.skip_callback(:commit, :after, :refresh_dependencies)
+    Api::V3::ResizeByAttribute.skip_callback(:commit, :after, :refresh_dependencies)
+    Api::V3::RecolorByAttribute.skip_callback(:commit, :after, :refresh_dependencies)
+  end
+  after do
+    Api::V3::DownloadAttribute.set_callback(:commit, :after, :refresh_dependencies)
+    Api::V3::ResizeByAttribute.set_callback(:commit, :after, :refresh_dependencies)
+    Api::V3::RecolorByAttribute.set_callback(:commit, :after, :refresh_dependencies)
+  end
   include_context 'api v3 brazil soy nodes'
   include_context 'api v3 brazil download attributes'
   include_context 'api v3 brazil resize by attributes'
@@ -8,7 +18,11 @@ RSpec.describe 'Get contexts', type: :request do
 
   describe 'GET /api/v3/contexts' do
     before(:each) do
-      SchemaRevamp.new.refresh
+      Api::V3::Readonly::Attribute.refresh
+      Api::V3::Readonly::DownloadAttribute.refresh
+      Api::V3::Readonly::ResizeByAttribute.refresh
+      Api::V3::Readonly::RecolorByAttribute.refresh
+      ActiveRecord::Base.connection.execute('COMMIT') # TODO: make default conn
     end
 
     it 'has the correct response structure' do
--- spec/responses/api/v3/map_layers_spec.rb
+++ spec/responses/api/v3/map_layers_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Map layers', type: :request do
+  before do
+    Api::V3::MapAttributeGroup.skip_callback(:commit, :after, :refresh_dependencies)
+    Api::V3::MapAttribute.skip_callback(:commit, :after, :refresh_dependencies)
+  end
+  after do
+    Api::V3::MapAttributeGroup.set_callback(:commit, :after, :refresh_dependencies)
+    Api::V3::MapAttribute.set_callback(:commit, :after, :refresh_dependencies)
+  end
   include_context 'api v3 brazil contextual layers'
   include_context 'api v3 brazil map attributes'
 
   describe 'GET /api/v3/contexts/:context_id/map_layers' do
     before(:each) do
-      SchemaRevamp.new.refresh
+      Api::V3::Readonly::Attribute.refresh
+      Api::V3::Readonly::MapAttribute.refresh
+      ActiveRecord::Base.connection.execute('COMMIT') # TODO: make default conn
     end
 
     it 'has the correct response structure' do
