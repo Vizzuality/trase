452eb6e572cc88b70e6764b2c0c7557ce848bef0
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-03-16 14:03:28 +0000

    Removed obsolete delete statments

--- spec/controllers/content/pages_controller_spec.rb
+++ spec/controllers/content/pages_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Content::PagesController, type: :controller do
-  describe 'GET index' do
+  describe 'GET show' do
     it 'assigns requested page' do
       index = FactoryBot.create(:page, name: 'index')
       get :show, params: {name: 'index'}
