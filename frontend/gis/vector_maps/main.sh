26828d9515fe106f2f4c98cbcb3c386696d24ec6
Tiago Garcia <tiagojsag@gmail.com>
2018-03-20 15:56:11 +0100

    Update geojson calculation logic to improve file loading time

--- frontend/gis/vector_maps/main.sh
+++ frontend/gis/vector_maps/main.sh
@@ -5,19 +5,20 @@ BIN='./node_modules/.bin'
 mkdir -p tmp
 
 ${BIN}/cartodb -u p2cs-sei -f geojson 'SELECT the_geom, geoid FROM BRAZIL_biomes' > tmp/BRAZIL_BIOME.json
-${BIN}/geo2topo -p --simplify 0.0000001 -o public/vector_layers/BRAZIL_BIOME.topo.json -- tmp/BRAZIL_BIOME.json
+${BIN}/mapshaper tmp/BRAZIL_BIOME.json -simplify rdp 30% planar keep-shapes -o public/vector_layers/BRAZIL_BIOME.topo.json format=topojson
 
 ${BIN}/cartodb -u p2cs-sei -f geojson 'SELECT the_geom, geoid FROM BRAZIL_states' > tmp/BRAZIL_STATE.json
-${BIN}/geo2topo -p --simplify 0.0000001 -o public/vector_layers/BRAZIL_STATE.topo.json -- tmp/BRAZIL_STATE.json
+${BIN}/mapshaper tmp/BRAZIL_STATE.json -simplify rdp 30% planar keep-shapes -o public/vector_layers/BRAZIL_STATE.topo.json format=topojson
 
 ${BIN}/cartodb -u p2cs-sei -f geojson 'SELECT the_geom, geoid, nome, biome_geoid FROM BRAZIL_municipalities_1' > tmp/BRAZIL_MUNICIPALITY.json
-${BIN}/geo2topo -p --simplify 0.0000001 -o public/vector_layers/BRAZIL_MUNICIPALITY.topo.json -- tmp/BRAZIL_MUNICIPALITY.json
+${BIN}/mapshaper tmp/BRAZIL_MUNICIPALITY.json -simplify rdp 30% planar keep-shapes -o public/vector_layers/BRAZIL_MUNICIPALITY.topo.json format=topojson
 
 ${BIN}/cartodb -u p2cs-sei -f geojson 'SELECT the_geom, geoid FROM col_departments' > tmp/COLOMBIA_DEPARTMENT.json
-${BIN}/geo2topo -p --simplify 0.0000001 -o public/vector_layers/COLOMBIA_DEPARTMENT.topo.json -- tmp/COLOMBIA_DEPARTMENT.json
+${BIN}/mapshaper tmp/COLOMBIA_DEPARTMENT.json -simplify rdp 30% planar keep-shapes -o public/vector_layers/COLOMBIA_DEPARTMENT.topo.json format=topojson
 
 rm tmp/PARAGUAY_DEPARTMENT.json
 rm tmp/PARAGUAY_DEPARTMENT_renamed.json
 ogr2ogr -f GeoJSON -t_srs crs:84 tmp/PARAGUAY_DEPARTMENT.json gis/shapefiles/paraguay-department/PARAGUAY_ADM1.shp
 ogr2ogr -f GeoJSON tmp/PARAGUAY_DEPARTMENT_renamed.json tmp/PARAGUAY_DEPARTMENT.json -sql "SELECT ID_GOVT AS geoid from PARAGUAY_ADM1"
-${BIN}/geo2topo -p --simplify 0.0000000001 -o public/vector_layers/PARAGUAY_DEPARTMENT.topo.json -- tmp/PARAGUAY_DEPARTMENT_renamed.json
+${BIN}/mapshaper tmp/PARAGUAY_DEPARTMENT_renamed.json -simplify rdp 30% planar keep-shapes -o public/vector_layers/PARAGUAY_DEPARTMENT.topo.json format=topojson
+
