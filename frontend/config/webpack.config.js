8ef485eb64cfd913ac28723122f44ac8a095014d
Tiago Garcia <tiagojsag@gmail.com>
2018-03-22 14:58:43 +0100

    Add prefetching to all webpack chunks

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
 /**
  * BundleAnalyzerPlugin allows profiling the webpack generated js, to help identify improvement points
  * If you want to enable it, uncomment the line bellow and ´new BundleAnalyzerPlugin()´ further down.
@@ -34,6 +35,10 @@ module.exports = {
       DATA_DOWNLOAD_ENABLED: process.env.DATA_DOWNLOAD_ENABLED === 'true',
       icons: templates.icons,
       head: templates.head
+    }),
+    new PreloadWebpackPlugin({
+      rel: 'prefetch',
+      include: 'allChunks'
     }),
     new webpack.DefinePlugin({
       NODE_ENV_DEV: process.env.NODE_ENV === 'development',
