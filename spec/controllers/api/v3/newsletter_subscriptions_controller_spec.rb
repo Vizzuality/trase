4d6017df64c22f490e53b737e118a4962ece6995
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-03-22 11:22:32 +0000

    Rescue Mailchimp::ListAlreadySubscribedError

---
+++ spec/controllers/api/v3/newsletter_subscriptions_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Api::V3::NewsletterSubscriptionsController, type: :controller do
+  describe 'POST create' do
+    it 'rescues from Mailchimp::ListAlreadySubscribedError' do
+      allow_any_instance_of(Mailchimp::Lists).to receive(:subscribe).and_raise(Mailchimp::ListAlreadySubscribedError)
+      post :create, params: {email: 'john.doe@example.com'}
+      expect(response).to be_success
+    end
+  end
+end
