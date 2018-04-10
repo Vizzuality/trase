7ee52618f1bf0e082bc2808797909d6c7cb89edc
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-04-10 17:37:55 +0200

    feat: limit years based on resize by and recolor by

--- frontend/scripts/react-components/nav/filters-nav/years-selector/years-selector.container.js
+++ frontend/scripts/react-components/nav/filters-nav/years-selector/years-selector.container.js
@@ -2,12 +2,20 @@ import { connect } from 'react-redux';
 import { toggleDropdown } from 'actions/app.actions';
 import { selectYears } from 'actions/tool.actions';
 import YearsSelector from 'react-components/nav/filters-nav/years-selector/years-selector.component';
+import intersection from 'lodash/intersection';
 
-const mapStateToProps = state => ({
-  currentDropdown: state.app.currentDropdown,
-  selectedYears: state.tool.selectedYears,
-  years: state.tool.selectedContext.years
-});
+const mapStateToProps = state => {
+  const { selectedResizeBy, selectedRecolorBy, selectedContext, selectedYears } = state.tool;
+  const availableYearsResize = intersection(selectedResizeBy.years, selectedContext.years);
+  const availableYearsRecolor = intersection(selectedRecolorBy.years, selectedContext.years);
+  const years = intersection(availableYearsRecolor, availableYearsResize);
+
+  return {
+    years,
+    selectedYears,
+    currentDropdown: state.app.currentDropdown
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   onToggle: id => {
