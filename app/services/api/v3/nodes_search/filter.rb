96237bb38ea22c26071e8f2eca681c7b5d45f42d
Tiago Garcia <tiagojsag@gmail.com>
2018-04-13 22:29:50 +0200

    Remove duplicate results from api search endpoint

--- app/services/api/v3/nodes_search/filter.rb
+++ app/services/api/v3/nodes_search/filter.rb
@@ -7,7 +7,7 @@ module Api
         end
 
         def call(query, context_id = nil, profile_only = nil)
-          @rel = @rel.search_by_name(query).limit(100)
+          @rel = @rel.search_by_name(query).group(:id, :name, :node_type, :context_id, :profile, :rank).limit(100)
           @rel = @rel.where('context_id' => context_id) if context_id
           @rel = @rel.where('profile IS NOT NULL') if profile_only
 
