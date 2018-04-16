78ab7ac32335e14e254498ecfe15fa63b00e60a6
Tomasz Subik <tsubik@gmail.com>
2018-04-16 17:34:20 +0200

    global search: group by node type

--- frontend/scripts/react-components/nav/top-nav/global-search/global-search.container.js
+++ frontend/scripts/react-components/nav/top-nav/global-search/global-search.container.js
@@ -2,20 +2,31 @@ import { loadSearchResults } from 'actions/app.actions';
 import GlobalSearch from 'react-components/nav/top-nav/global-search/global-search.component';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import groupBy from 'lodash/groupBy';
 
 const mapStateToProps = state => {
   const { search } = state.app;
+  const { contexts } = state.tool;
+
+  const searchResults = Object.values(
+    groupBy(search.results, ({ mainId, contextId }) => `${mainId}_${contextId}`)
+  ).map(nodes => {
+    const node = nodes[0];
+    const ctx = contexts.find(c => c.id === node.contextId);
+    const nodeType = nodes.map(n => n.nodeType).join(' & ');
+
+    return {
+      name: node.name,
+      nodeTypeText: `${nodeType} for ${ctx.countryName} ${ctx.commodityName}`,
+      contextId: node.contextId,
+      nodes
+    };
+  });
 
   return {
     isLoading: search.isLoading,
     searchTerm: search.term,
-    searchResults: search.results.map(n => ({
-      id: n.id,
-      name: n.name,
-      contextId: n.contextId,
-      profileType: n.profile,
-      type: n.nodeType
-    }))
+    searchResults
   };
 };
 
