e050d2e280a96786e558539cbf450ac5229d4684
Tomasz Subik <tsubik@gmail.com>
2018-05-14 17:29:50 +0200

    new years selector component

--- frontend/scripts/react-components/nav/filters-nav/years-selector/years-selector.component.jsx
+++ frontend/scripts/react-components/nav/filters-nav/years-selector/years-selector.component.jsx
@@ -1,167 +1,135 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/mouse-events-have-key-events, jsx-a11y/no-static-element-interactions, react/no-unused-prop-types */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import FiltersDropdown from 'react-components/nav/filters-nav/filters-dropdown.component';
-import YearsThumb from 'react-components/nav/filters-nav/years-selector/years-thumb.component';
 import cx from 'classnames';
-import 'styles/components/tool/years-slider.scss';
+import range from 'lodash/range';
 
-const YEAR_WIDTH = 40;
+import FiltersDropdown from 'react-components/nav/filters-nav/filters-dropdown.component';
+
+import 'styles/components/tool/years-selector.scss';
+
 const id = 'years';
 
 class YearsSelector extends Component {
   constructor(props) {
     super(props);
 
-    this.onSelectorDown = this.onSelectorDown.bind(this);
-    this.onSelectorMoved = this.onSelectorMoved.bind(this);
-    this.onSelectorUp = this.onSelectorUp.bind(this);
-    this.onDropdownUp = this.onDropdownUp.bind(this);
+    this.state = {
+      ...this.getStateFromProps(this.props),
+      hovered: null
+    };
 
-    this.state = this.getState(props);
+    this.setActive = this.setActive.bind(this);
+    this.setHovered = this.setHovered.bind(this);
+    this.getClassName = this.getClassName.bind(this);
   }
 
   componentWillReceiveProps(props) {
-    this.setState(this.getState(props));
+    this.setState(this.getStateFromProps(props));
   }
 
-  onDropdownUp() {
-    if (this.dragging === true) {
-      return;
-    }
-    this.props.onToggle(id);
-  }
+  getStateFromProps({ selectedYears }) {
+    const [start, end] = selectedYears;
 
-  onSelectorDown(event) {
-    this.dragging = true;
-    this.currentSelectorThumb = event.target.getAttribute('data-thumb');
-    document.addEventListener('mousemove', this.onSelectorMoved);
-    document.addEventListener('touchmove', this.onSelectorMoved);
-    document.addEventListener('mouseup', this.onSelectorUp);
-    document.addEventListener('touchend', this.onSelectorUp);
-  }
-
-  onSelectorMoved(event) {
-    const sliderOffset = this.slider.getBoundingClientRect().left;
-    const pointerX = event.touches ? event.touches[0].clientX : event.clientX;
-    const x = pointerX - sliderOffset;
-    this.moveSelector(x);
-  }
-
-  onSelectorUp(event) {
-    this.dragging = false;
-    event.stopPropagation();
-    document.removeEventListener('mousemove', this.onSelectorMoved);
-    document.removeEventListener('mouseup', this.onSelectorUp);
-    document.removeEventListener('touchmove', this.onSelectorMoved);
-    document.removeEventListener('touchend', this.onSelectorUp);
-    this.releaseSelector();
-  }
-
-  getState({ selectedYears, years }) {
-    const left = YEAR_WIDTH * years.indexOf(selectedYears[1] - 1);
-    const right = YEAR_WIDTH * (years.indexOf(selectedYears[1]) + 1);
     return {
-      left,
-      right
+      start,
+      end
     };
   }
 
-  moveSelector(x) {
-    const deltaWidth = this.state.right - this.state.left;
+  setActive(year) {
+    const { start, end } = this.state;
+    const { onSelected } = this.props;
 
-    let left;
-    let right;
-
-    if (this.currentSelectorThumb === null) {
-      left = x - deltaWidth / 2;
-      right = x + deltaWidth / 2;
-      if (left < 0) {
-        left = 0;
-        right = deltaWidth;
-      } else if (right > this.totalWidth) {
-        right = this.totalWidth;
-        left = right - deltaWidth;
+    if (start !== null) {
+      if (end !== null) {
+        this.setState({ start: null, end: null });
+      } else {
+        const newStart = Math.min(start, year);
+        const newEnd = Math.max(start, year);
+        this.setState({ start: newStart, end: newEnd });
+        onSelected([newStart, newEnd]);
       }
-    } else if (this.currentSelectorThumb === 'left') {
-      // not after right thumb, not below 0
-      left = Math.min(this.state.right, Math.max(0, x));
-      right = this.state.right;
-    } else if (this.currentSelectorThumb === 'right') {
-      // not before left thumb, not above total width
-      right = Math.max(this.state.left, Math.min(this.totalWidth, x));
-      left = this.state.left;
+    } else {
+      this.setState({ start: year });
     }
-    this.setState({
-      left,
-      right
-    });
   }
 
-  releaseSelector() {
-    let leftIndex = Math.round(this.state.left / YEAR_WIDTH);
-    if (leftIndex === this.props.years.length) {
-      leftIndex = this.props.years.length - 1;
+  setHovered(year) {
+    this.setState({ hovered: year });
+  }
+
+  getClassName(year) {
+    const { start, end, hovered } = this.state;
+    const { years } = this.props;
+
+    const [startYear, endYear] = [start || hovered, end || hovered].sort();
+
+    const classes = [];
+
+    if (range(startYear, (endYear || startYear) + 1).includes(year)) {
+      classes.push('active');
+    }
+    if (year === startYear && year < Math.max(...years)) {
+      classes.push('start');
+    }
+    if ((year === endYear && start) || (startYear === year && year === Math.max(...years))) {
+      classes.push('end');
     }
 
-    let rightIndex = Math.max(0, Math.round(this.state.right / YEAR_WIDTH) - 1);
-    if (rightIndex <= leftIndex) {
-      rightIndex = leftIndex;
-    }
-    this.props.onSelected([this.props.years[leftIndex], this.props.years[rightIndex]]);
+    return classes.join(' ');
   }
 
   render() {
-    const { className, dropdownClassName, currentDropdown, selectedYears, years } = this.props;
-    this.totalWidth = YEAR_WIDTH * years.length;
+    const {
+      className,
+      dropdownClassName,
+      currentDropdown,
+      years,
+      selectedYears,
+      onToggle
+    } = this.props;
+    const [selectedStart, selectedEnd] = selectedYears;
+    const { start, end } = this.state;
     const title =
-      selectedYears[0] === selectedYears[1] ? (
-        <span>{selectedYears[0]}</span>
+      selectedStart === selectedEnd ? (
+        <span>{selectedStart}</span>
       ) : (
         <span>
-          {selectedYears[0]}&thinsp;-&thinsp;{selectedYears[1]}
+          {selectedStart}&thinsp;-&thinsp;{selectedEnd}
         </span>
       );
-    const totalWidthStyle = { width: `${this.totalWidth}px` };
-    const deltaWidth = this.state.right - this.state.left;
-    const selectorWidthStyle = {
-      width: `${deltaWidth}px`,
-      left: `${this.state.left}px`
-    };
+
     return (
-      <div className={cx('js-dropdown', className)} onMouseUp={this.onDropdownUp}>
+      <div className={cx('js-dropdown', className)} onClick={() => onToggle(id)}>
         <div className={cx('c-dropdown', dropdownClassName)}>
           <span className="dropdown-label">
-            year{selectedYears[0] !== selectedYears[1] && <span>s</span>}
+            year{selectedStart !== selectedEnd && <span>s</span>}
           </span>
           <span className="dropdown-title">{title}</span>
-          <FiltersDropdown
-            id={id}
-            currentDropdown={currentDropdown}
-            onClickOutside={this.props.onToggle}
-          >
-            <div className="dropdown-list">
-              <div
-                className="c-years-slider"
-                ref={elem => {
-                  this.slider = elem;
-                }}
-              >
-                <ul className="background" style={totalWidthStyle}>
-                  {years.map((year, index) => <li key={index} />)}
-                </ul>
-                <div
-                  className="selector"
-                  style={selectorWidthStyle}
-                  onMouseDown={this.onSelectorDown}
-                  onTouchStart={this.onSelectorDown}
-                >
-                  <YearsThumb id="left" />
-                  <YearsThumb id="right" x={deltaWidth} />
+          <FiltersDropdown id={id} currentDropdown={currentDropdown} onClickOutside={onToggle}>
+            <div className="dropdown-list" onClick={e => e.stopPropagation()}>
+              <div className={cx('c-years-selector', start && end ? 'selected' : 'selecting')}>
+                <div className="years-selector-content">
+                  {years.map(year => (
+                    <div
+                      key={year}
+                      onClick={() => this.setActive(year)}
+                      onMouseOut={this.clearHovered}
+                      onMouseOver={() => this.setHovered(year)}
+                      className={cx('button', this.getClassName(year))}
+                    >
+                      <div className="unrotate">
+                        <div className={cx('fill', this.getClassName(year))}>
+                          <span>{year}</span>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-                <ul className="years" style={totalWidthStyle}>
-                  {years.map((year, index) => <li key={index}>{year}</li>)}
-                </ul>
+                <div className="years-selector-footer">
+                  <p>Select year</p>
+                </div>
               </div>
             </div>
           </FiltersDropdown>
