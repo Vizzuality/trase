6915abb1275aee3d09abac68c3bd4e729c328ae4
Tiago Garcia <tiagojsag@gmail.com>
2018-05-25 09:11:00 +0200

    Fix issue with missing prop in context selector

--- frontend/scripts/react-components/shared/context-selector/context-selector.container.js
+++ frontend/scripts/react-components/shared/context-selector/context-selector.container.js
@@ -3,12 +3,13 @@ import { toggleDropdown, selectContextById } from 'actions/app.actions';
 import ContextSelector from 'react-components/shared/context-selector/context-selector.component';
 
 const mapStateToProps = state => {
-  const { contexts, tooltips, currentDropdown, selectedContext } = state.app;
+  const { contexts, tooltips, currentDropdown, selectedContext, contextIsUserSelected } = state.app;
 
   return {
     contexts: contexts || [],
     tooltipText: tooltips && tooltips.sankey.nav.context.main,
     currentDropdown,
+    contextIsUserSelected,
     selectedContextCountry: selectedContext && selectedContext.countryName,
     selectedContextCommodity: selectedContext && selectedContext.commodityName
   };
