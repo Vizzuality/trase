5e0aca36691bba3767f955fd14581878496fe170
Tomasz Subik <tsubik@gmail.com>
2018-05-31 13:21:38 +0200

    profile: mobile line colors, no need for generated colors as there should be only max 5 lines

--- frontend/scripts/react-components/profiles/line.component.jsx
+++ frontend/scripts/react-components/profiles/line.component.jsx
@@ -10,9 +10,8 @@ import { extent as d3_extent } from 'd3-array';
 import { area as d3_area, line as d3_line } from 'd3-shape';
 import { format as d3_format } from 'd3-format';
 import { timeFormat as d3_timeFormat } from 'd3-time-format';
-import chroma from 'chroma-js';
 
-import { LINE_LABEL_HEIGHT, PROFILE_CHOROPLETH_COLORS } from 'constants';
+import { LINE_LABEL_HEIGHT } from 'constants';
 import abbreviateNumber from 'utils/abbreviateNumber';
 import i18n from 'utils/transifex';
 import { Responsive } from 'react-components/shared/responsive.hoc';
@@ -109,7 +108,6 @@ class Line extends Component {
       return a.values[last] - b.values[last];
     });
     const numLines = lines.length;
-    const colorScaleForMobile = chroma.scale(PROFILE_CHOROPLETH_COLORS).colors(numLines);
 
     lines.forEach((lineData, i) => {
       const lineValuesWithFormat = this.prepareData(xValues, lineData);
@@ -160,20 +158,14 @@ class Line extends Component {
           pathContainers = d3Container
             .datum(lineValuesWithFormat[0])
             .append('g')
-            .attr('id', lineData.geo_id);
+            .attr('id', lineData.geo_id)
+            .attr('class', d => {
+              const lineIndex = isSmallChart ? i : d[0].value9;
 
-          if (isSmallChart) {
-            pathContainers.attr('class', `${style} generated-color`);
-            pathContainers.attr('stroke', colorScaleForMobile[lineNumber - 1]);
-          } else {
-            pathContainers.attr(
-              'class',
-              d =>
-                isFunction(settings.lineClassNameCallback)
-                  ? settings.lineClassNameCallback(d, style)
-                  : style
-            );
-          }
+              return isFunction(settings.lineClassNameCallback)
+                ? settings.lineClassNameCallback(lineIndex, style)
+                : style;
+            });
 
           pathContainers
             .selectAll('path')
@@ -315,21 +307,15 @@ class Line extends Component {
     const lineOnMouseLeave = lineData => {
       this.chart.querySelector(`#${lineData.geo_id}`).classList.remove('selected');
     };
-    const colorScaleForMobile = chroma.scale(PROFILE_CHOROPLETH_COLORS).colors(lines.length);
 
     return (
       <ul className="line-bottom-legend">
         {lines.map((lineData, index) => {
           const style = typeof data.style !== 'undefined' ? data.style.style : lineData.style;
-          const strokeColor = isSmallChart ? colorScaleForMobile[index] : null;
-          let lineClassName = style;
-
-          if (isSmallChart) {
-            lineClassName = `${style} generated-color`;
-          } else if (isFunction(settings.lineClassNameCallback)) {
-            lineClassName = settings.lineClassNameCallback([lineData], style);
-          }
-
+          const lineIndex = isSmallChart ? lines.length - index - 1 : lineData.value9;
+          const lineClassName = isFunction(settings.lineClassNameCallback)
+            ? settings.lineClassNameCallback(lineIndex, style)
+            : style;
           const isLink =
             typeof onLinkClick !== 'undefined' && lineData.node_id && lineData.profile_type;
           const linkOnClick = () => {
@@ -346,7 +332,7 @@ class Line extends Component {
               onMouseLeave={() => lineOnMouseLeave(lineData)}
             >
               <svg height="6" width="20" className="line-color">
-                <g className={lineClassName} stroke={strokeColor}>
+                <g className={lineClassName}>
                   <path d="M0 3 20 3" />
                 </g>
               </svg>
