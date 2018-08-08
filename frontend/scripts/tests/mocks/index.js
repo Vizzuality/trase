7cbe941f2a94a096c794264e1e5dcc3522793974
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-08-08 12:27:02 +0200

    fix: mocks

--- frontend/scripts/tests/mocks/index.js
+++ frontend/scripts/tests/mocks/index.js
@@ -1,3 +1,5 @@
+import entries from 'lodash/entries';
+
 import contexts from './contexts';
 import actorSummary from './actor-summary';
 import rootSearchActor from './root-search-actor';
@@ -5,11 +7,17 @@ import topCountries from './top-countries-actor';
 
 require('dotenv').config({ silent: true });
 
-export default {
-  [`${process.env.API_V3_URL}/api/v3/contexts`]: contexts,
-  [`${
-    process.env.API_V3_URL
-  }/api/v3/nodes/search?query=bunge&context_id=1&profile_only=true`]: rootSearchActor,
-  [`${process.env.API_V3_URL}/api/v3/contexts/1/actors/441/basic_attributes`]: actorSummary,
-  [`${process.env.API_V3_URL}/api/v3/contexts/1/actors/441/top_countries?year=2015`]: topCountries
+const endpointsMap = {
+  '/api/v3/contexts': contexts,
+  '/api/v3/nodes/search?query=bunge&context_id=1&profile_only=true': rootSearchActor,
+  '/api/v3/contexts/1/actors/441/basic_attributes?year=2015': actorSummary,
+  '/api/v3/contexts/1/actors/441/top_countries?year=2015': topCountries
 };
+
+const addBaseUrl = data =>
+  entries(data).reduce(
+    (acc, [endpoint, mock]) => ({ ...acc, [process.env.API_V3_URL + endpoint]: mock }),
+    {}
+  );
+
+export default addBaseUrl(endpointsMap);
