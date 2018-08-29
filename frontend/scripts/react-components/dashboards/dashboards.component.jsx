8df388446aef0f654158e9945dff5043f993b339
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-08-29 16:35:26 +0200

    dashboards landing page initial mock

--- frontend/scripts/react-components/dashboards/dashboards.component.jsx
+++ frontend/scripts/react-components/dashboards/dashboards.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import StoryTile from 'react-components/home/story-tile.component';
+import Card from 'react-components/shared/card.component';
 // import PropTypes from 'prop-types';
 // import cx from 'classnames';
 
@@ -8,15 +8,34 @@ function Dashboards(props) {
   return (
     <div className="l-dashboards">
       <div className="c-dashboards">
-        <div className="row">
-          {posts.slice(0, 5).map(post => (
-            <div className="column medium-4">
-              <div className="slide">
-                <StoryTile slide={post} />
+        <h1 className="title">See how commodity trade impacts the world</h1>
+        <section className="dashboards-grid">
+          <div className="row">
+            {posts.slice(0, 5).map(post => (
+              <div className="column small-12 medium-6 large-4">
+                <div className="post">
+                  <Card
+                    title={post.title}
+                    subtitle={post.category}
+                    imageUrl={post.imageUrl}
+                    className="dashboard-item"
+                    actionName="Go To Dashboard"
+                    linkUrl={post.completePostUrl}
+                  />
+                </div>
               </div>
+            ))}
+            <div className="column small-12 medium-6 large-4">
+              <Card
+                className="-dashed"
+                title="Create your own Dashboard with custom options."
+                subtitle="Custom dashboard"
+                actionName="Create dashboard"
+                imageUrl="/images/logos/trase_logo_white.svg"
+              />
             </div>
-          ))}
-        </div>
+          </div>
+        </section>
       </div>
     </div>
   );
