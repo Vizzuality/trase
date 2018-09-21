b84893918f45de5a5a8a6265ee3fc0dd2ef8c24d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-09-21 11:23:22 +0100

    Helper to check if materialised view is populated

---
+++ spec/services/api/v3/is_mview_populated_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Api::V3::IsMviewPopulated do
+  describe :call do
+    let(:subject) { Api::V3::IsMviewPopulated.new(:nodes_mv) }
+    it 'should return false when not populated' do
+      expect(subject.call).to be false
+    end
+
+    it 'should return true when populated' do
+      FactoryBot.create(:api_v3_node)
+      Api::V3::Readonly::Node.refresh
+      expect(subject.call).to be true
+    end
+  end
+end
