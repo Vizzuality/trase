beecf001817e6d69e484a5e45ead06548a851d4b
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-10-22 21:12:17 +0200

    adds fallback to xAxis and tooltip unit

--- frontend/scripts/react-components/dashboard-element/dashboard-widget/dashboard-widget-tooltip.jsx
+++ frontend/scripts/react-components/dashboard-element/dashboard-widget/dashboard-widget-tooltip.jsx
@@ -10,12 +10,14 @@ class DashboardWidgetTooltip extends React.PureComponent {
         text = `${tooltip.prefix} `;
       }
       // TODO use tooltip formatter
-      text += payload[key] && payload[key].toLocaleString();
-      if (tooltip.suffix) {
-        text += ` ${tooltip.suffix}`;
+      text += payload[key] && payload[key].toLocaleString(undefined, { maximumFractionDigits: 0 });
+      if (tooltip.suffix || (meta.yAxis && meta.yAxis.suffix)) {
+        // TODO temporary fallback, suffix should be taken from tooltip always
+        const suffix = tooltip.suffix || meta.yAxis.suffix;
+        text += ` ${suffix}`;
       }
     } else if (meta && !meta[key]) {
-      text = payload[key] && payload[key].toLocaleString();
+      text = payload[key] && payload[key].toLocaleString(undefined, { maximumFractionDigits: 0 });
     }
     return text;
   }
@@ -38,7 +40,7 @@ class DashboardWidgetTooltip extends React.PureComponent {
             <span style={{ backgroundColor: item.color || 'white' }} />
             <p>
               <span>{DashboardWidgetTooltip.getTooltipLabel(meta, item.dataKey)}</span>
-              <span>
+              <span className="key-item-value">
                 {DashboardWidgetTooltip.getTooltipValue(meta, item.dataKey, item.payload)}
               </span>
             </p>
