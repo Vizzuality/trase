a12ea7556d36242b9eba45faebb9a982c8203900
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-10-24 13:29:17 +0100

    Added node_type_id to search endpoint response

--- app/serializers/api/v3/dashboards/node_serializer.rb
+++ app/serializers/api/v3/dashboards/node_serializer.rb
@@ -2,7 +2,7 @@ module Api
   module V3
     module Dashboards
       class NodeSerializer < ActiveModel::Serializer
-        attributes :id, :name
+        attributes :id, :name, :node_type_id
         attribute :node_type do
           object['node_type']
         end
