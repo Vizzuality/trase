599a8bfd6edb63b653b549e60cd8a10c21b6281f
Javi Abia <weberjavi@gmail.com>
2018-10-24 12:27:01 +0200

    Add suffix condition

--- frontend/scripts/react-components/chart/axis-label/axis-label-component.jsx
+++ frontend/scripts/react-components/chart/axis-label/axis-label-component.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const percentageSuffix = (text, suffix) => `${suffix} ${text}`;
+const regularSuffix = (text, suffix) => `${text} [${suffix}]`;
+
 const AxisLabel = props => {
   const { text, suffix } = props;
-  return <p className="widget-yAxis-label">{`${text} [${suffix}]`}</p>;
+  const isSuffixPercentage = suffix === '%';
+  return (
+    <p className="widget-yAxis-label">
+      {isSuffixPercentage ? percentageSuffix(text, suffix) : regularSuffix(text, suffix)}
+    </p>
+  );
 };
 
 AxisLabel.propTypes = {
