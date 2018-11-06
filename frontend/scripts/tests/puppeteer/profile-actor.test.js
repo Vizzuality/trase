19d45e1c24af69775d4710e6ec8231766c25551e
Rodrigo Solis <solis.ro@gmail.com>
2018-11-06 19:43:01 +0100

    fix puppeteer tests

--- frontend/scripts/tests/puppeteer/profile-actor.test.js
+++ frontend/scripts/tests/puppeteer/profile-actor.test.js
@@ -5,7 +5,7 @@ import { getRequestMockFn } from './utils';
 import { testProfileSpinners, testProfileSummary, testProfileMultiTable } from './shared';
 
 const BASE_URL = 'http://0.0.0.0:8081';
-const TIMEOUT = process.env.PUPETEER_TIMEOUT || 6000;
+const TIMEOUT = process.env.PUPETEER_TIMEOUT || 30000;
 
 const { page } = global;
 
