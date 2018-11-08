2bb4ee33f970579b57ee125c5c1767b9e21ab850
Rodrigo Solis <solis.ro@gmail.com>
2018-11-08 17:56:21 +0100

    add coverage support

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,12 @@
 // jest.config.js
 module.exports = {
+  preset: 'jest-puppeteer',
   rootDir: 'scripts',
   moduleFileExtensions: ['js', 'jsx', 'json'],
   moduleDirectories: ['node_modules'],
   globals: {
     NODE_ENV_DEV: false
   },
-  preset: 'jest-puppeteer'
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.js']
 };
