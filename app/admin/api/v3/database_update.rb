1a2c0b2560d846350b3dfa4b58da6b44ac8db919
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2018-11-28 23:12:40 +0000

    Changes to CMS page to support mirror database update

--- app/admin/api/v3/database_update.rb
+++ app/admin/api/v3/database_update.rb
@@ -8,16 +8,37 @@ ActiveAdmin.register_page 'Database Update' do
   content do
     current_update = Api::V3::DatabaseUpdate.started.first
     database_updates = Api::V3::DatabaseUpdate.order(created_at: :desc).limit(25)
-    render partial: 'admin/database_update/form', locals: {
+    main_schema_versions = Api::V3::S3ObjectList.instance.call(include: ['MAIN'])
+    unless current_update
+      render partial: 'admin/database_update/mirror_form', locals: {
+        current_update: current_update,
+        main_schema_versions: main_schema_versions
+      }
+      render partial: 'admin/database_update/remote_form', locals: {
+        current_update: current_update
+      }
+    end
+    render partial: 'admin/database_update/list', locals: {
       current_update: current_update,
       database_updates: database_updates
     }
   end
 
-  page_action :start, method: :post do
+  page_action :start_remote, method: :post do
     database_update = Api::V3::DatabaseUpdate.new(status: Api::V3::DatabaseUpdate::STARTED)
     if database_update.save
-      DatabaseUpdateWorker.perform_async(database_update.id)
+      RemoteDatabaseUpdateWorker.perform_async(database_update.id)
+      redirect_to admin_database_update_path, notice: 'Database update scheduled. Please refresh for updates.'
+    else
+      redirect_to admin_database_update_path, notice: 'Database update already in progress.'
+    end
+  end
+
+  page_action :start_mirror, method: :post do
+    puts params.inspect
+    database_update = Api::V3::DatabaseUpdate.new(status: Api::V3::DatabaseUpdate::STARTED)
+    if database_update.save
+      MirrorDatabaseUpdateWorker.perform_async(database_update.id, params[:main_schema_version])
       redirect_to admin_database_update_path, notice: 'Database update scheduled. Please refresh for updates.'
     else
       redirect_to admin_database_update_path, notice: 'Database update already in progress.'
