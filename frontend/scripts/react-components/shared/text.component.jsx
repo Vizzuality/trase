5009d18600c91523ed7639eaaf3ba4338e78fc42
sorodrigo <rodrigo.solis@vizzuality.com>
2018-12-03 16:38:11 +0100

    updates text styles

--- frontend/scripts/react-components/shared/text.component.jsx
+++ frontend/scripts/react-components/shared/text.component.jsx
@@ -5,22 +5,34 @@ import cx from 'classnames';
 import 'styles/components/shared/text.scss';
 
 function Text(props) {
-  const { as, children, variant, ...rest } = props;
+  const { as, variant, color, size, align, weight, children, ...rest } = props;
   const textProps = {
     ...rest,
-    className: cx('c-text', variant)
+    className: cx('c-text', variant, {
+      [`color-${color}`]: color,
+      [`size-${size}`]: size,
+      [`weight-${weight}`]: weight,
+      [`align-${align}`]: align
+    })
   };
-  return React.createComponent(as, textProps, children);
+  return React.createElement(as, textProps, children);
 }
 
 Text.defaultProps = {
   as: 'p',
+  size: 'rg',
+  color: 'grey',
+  weight: 'light',
   variant: 'serif'
 };
 
 Text.propTypes = {
+  size: PropTypes.string,
+  align: PropTypes.string,
+  color: PropTypes.string,
+  weight: PropTypes.string,
   children: PropTypes.node,
-  className: PropTypes.string,
+  variant: PropTypes.string,
   as: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
 };
 
