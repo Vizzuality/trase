ec654b6a0e8055f59aaaa9fc981d452074e9af18
sorodrigo <rodrigo.solis@vizzuality.com>
2018-12-11 13:58:36 +0100

    updates named-maps for production environment

--- frontend/scripts/actions/map/context_layers_carto.js
+++ frontend/scripts/actions/map/context_layers_carto.js
@@ -2,36 +2,28 @@
 module.exports = {
   production: {
     brazil_states: {
-      uid: 'brazil_states1538040623401',
-      layergroupid: 'p2cs-sei@5f503d7c@ad9947aed69ba64bb275829c43505e45:1525368076102'
+      uid: 'brazil_states__production',
+      layergroupid: 'p2cs-sei@4f456911@94bb9db1cc2bbb6f823b52891ea85a4b:1536754740910'
     },
     brazil_biomes: {
-      uid: 'brazil_biomes1538040624445',
-      layergroupid: 'p2cs-sei@76cc05f0@80e0824f230b00c7b8d58beaaeb520d8:1536754747512'
+      uid: 'brazil_biomes__production',
+      layergroupid: 'p2cs-sei@ee8b4bf2@53eff313f97bb4d6e956f5078a2ce178:1536754747512'
     },
-    water_scarcity: {
-      uid: 'water_scarcity1538040625290',
-      layergroupid: 'p2cs-sei@b74e67c2@309c4fc88126169c70ae0a2d80abb2d0:1536754755124'
+    brazil_water_scarcity: {
+      uid: 'brazil_water_scarcity__production',
+      layergroupid: 'p2cs-sei@ce96a651@bd75df185965352f558793d64c6c76af:1536754755124'
     },
-    indigenous_areas: {
-      uid: 'indigenous_areas1538040626144',
-      layergroupid: 'p2cs-sei@5ffb143b@d0af2032fe70f7cf11e53c24017d2a1b:1538034854446'
+    brazil_indigenous_areas: {
+      uid: 'brazil_indigenous_areas__production',
+      layergroupid: 'p2cs-sei@bef64c15@b9f9b57ffb78da8860a82269aa5673aa:1538034854446'
     },
     brazil_protected: {
-      uid: 'brazil_protected1538040627004',
-      layergroupid: 'p2cs-sei@5d4f9d60@510b43b8a25a0053e13e90135955fee0:1537979981544'
+      uid: 'brazil_protected__production',
+      layergroupid: 'p2cs-sei@851f6bd9@124c2f3d625b7192f32bb55a83a8b2fd:1537979981544'
     },
     brazil_defor_alerts: {
-      uid: 'brazil_defor_alerts1538040627875',
-      layergroupid: 'p2cs-sei@89f013b7@f2c183cc1c4ff03128c333156e32e236:1525368094112'
-    },
-    col_protected_areas: {
-      uid: 'col_protected_areas1538040628734',
-      layergroupid: 'p2cs-sei@fa1d57ed@c1cfe7a14d3a25fe34899d40a737a29b:1537979981544'
-    },
-    col_adm1_1: {
-      uid: 'col_adm1_11538040629580',
-      layergroupid: 'p2cs-sei@bbb977bf@fc3af4c09050cd0a896103344de12ac0:1525368093739'
+      uid: 'brazil_defor_alerts__production',
+      layergroupid: 'p2cs-sei@c4807a94@9495c323e40ff63e0c59e0fc6ffbebcd:1538401614852'
     }
   },
   development: {
