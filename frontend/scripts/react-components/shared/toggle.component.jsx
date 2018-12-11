d82c216b2c11b3aa72587a96faf70a85bb0d90fd
sorodrigo <rodrigo.solis@vizzuality.com>
2018-12-11 19:25:46 +0100

    adds logistics map legend

---
+++ frontend/scripts/react-components/shared/toggle.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import 'styles/components/shared/toggle.scss';
+
+function Toggle(props) {
+  return (
+    <input
+      id={props.id}
+      className="c-toggle"
+      type="checkbox"
+      checked={props.checked}
+      onChange={props.onChange}
+    />
+  );
+}
+
+Toggle.propTypes = {
+  id: PropTypes.string,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func
+};
+
+export default Toggle;
