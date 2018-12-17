fbb92702c9459561a5fa66634969b48ad8c5cf64
sorodrigo <rodrigo.solis@vizzuality.com>
2018-12-17 13:05:21 +0100

    add logistics maps feature flag

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -40,6 +40,7 @@ module.exports = {
       SHOW_WORLD_MAP_IN_EXPLORE: process.env.SHOW_WORLD_MAP_IN_EXPLORE === 'true',
       ALWAYS_DISPLAY_DASHBOARD_INFO: process.env.ALWAYS_DISPLAY_DASHBOARD_INFO === 'true',
       ENABLE_DASHBOARDS: process.env.ENABLE_DASHBOARDS === 'true',
+      ENABLE_LOGISTICS_MAP: process.env.ENABLE_LOGISTICS_MAP === 'true',
       API_V3_URL: JSON.stringify(process.env.API_V3_URL),
       API_V2_URL: JSON.stringify(process.env.API_V2_URL),
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
