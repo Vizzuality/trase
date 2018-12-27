39926a780030198eb719255ac7821a9c222691fc
sorodrigo <rodrigo.solis@vizzuality.com>
2018-12-27 16:04:55 +0100

    refactors view mode and admin level selectors

--- frontend/scripts/react-components/nav/filters-nav/filters-nav.selectors.js
+++ frontend/scripts/react-components/nav/filters-nav/filters-nav.selectors.js
@@ -15,6 +15,10 @@ const getSelectedContext = state => state.app.selectedContext;
 const getToolSelectedYears = state => state.tool.selectedYears;
 const getToolResizeBy = state => state.tool.selectedResizeBy;
 const getToolRecolorBy = state => state.tool.selectedRecolorBy;
+const getToolSelectedBiome = state => state.tool.selectedBiomeFilter;
+const getContextFilterBy = state => state.app.selectedContext && state.app.selectedContext.filterBy;
+const getAppTooltips = state => state.app.tooltips;
+const getToolDetailedView = state => state.tool.detailedView;
 
 export const getToolYearsProps = createSelector(
   [getToolSelectedYears, getToolResizeBy, getToolRecolorBy, getSelectedContext],
@@ -39,12 +43,52 @@ export const getToolYearsProps = createSelector(
   }
 );
 
+export const getToolAdminLevelProps = createSelector(
+  [getToolSelectedBiome, getContextFilterBy],
+  (selectedFilter, filterBy) => {
+    const [adminLevel] = filterBy || [];
+    if (!adminLevel) return null;
+    return {
+      label: adminLevel.name,
+      id: 'toolAdminLevel',
+      listClassName: '-medium',
+      items: [
+        { name: 'All', id: 'none' },
+        ...adminLevel.nodes
+          .filter(node => node.name !== selectedFilter.name)
+          .map(node => ({ ...node, id: node.name }))
+      ],
+      selectedItem:
+        typeof selectedFilter !== 'undefined' && selectedFilter.value !== 'none'
+          ? selectedFilter
+          : { name: 'All' }
+    };
+  }
+);
+
+export const getToolViewModeProps = createSelector(
+  [getAppTooltips, getToolDetailedView],
+  (tooltips, isDetailedView) => {
+    const items = [{ name: 'Summary', id: false }, { name: 'All Flows', id: true }];
+    return {
+      items: items.filter(i => i.id !== isDetailedView),
+      label: 'Change view',
+      id: 'toolViewMode',
+      tooltip: tooltips.sankey.nav.view.main,
+      titleClassName: '-small',
+      dropdownClassName: '-capitalize -small',
+      selectedItem: items.find(item => item.id === isDetailedView)
+    };
+  }
+);
+
 const getLogisticsMapYearsProps = createSelector(
   [getActiveParams],
   activeParams => ({
     label: 'Year',
     id: 'logisticsMapYear',
     items: LOGISTICS_MAP_YEARS,
+    listClassName: '-medium',
     selectedItem: LOGISTICS_MAP_YEARS.find(year => year.id === activeParams.year)
   })
 );
@@ -55,13 +99,28 @@ const getLogisticsMapContextProps = createSelector(
     label: 'Country – Commodity',
     id: 'logisticsMapContext',
     items: LOGISTICS_MAP_CONTEXTS,
+    listClassName: '-medium',
     selectedItem: LOGISTICS_MAP_CONTEXTS.find(ctx => ctx.id === activeParams.context)
   })
 );
 
 export const getNavFilters = createSelector(
-  [getCurrentPage, getSelectedContext, getLogisticsMapYearsProps, getLogisticsMapContextProps],
-  (page, selectedContext, logisticsMapsYears, logisticsMapsContexts) => {
+  [
+    getCurrentPage,
+    getSelectedContext,
+    getToolAdminLevelProps,
+    getToolViewModeProps,
+    getLogisticsMapYearsProps,
+    getLogisticsMapContextProps
+  ],
+  (
+    page,
+    selectedContext,
+    toolAdminLevel,
+    toolViewMode,
+    logisticsMapsYears,
+    logisticsMapsContexts
+  ) => {
     switch (page) {
       case 'tool':
         return {
@@ -69,7 +128,7 @@ export const getNavFilters = createSelector(
           showToolLinks: true,
           left: [
             { type: FILTERS.contextSelector, props: { selectedContext, id: 'contextSelector' } },
-            // { type: FILTERS.dropdownSelector },
+            ...(toolAdminLevel ? [{ type: FILTERS.dropdownSelector, props: toolAdminLevel }] : []),
             {
               type: FILTERS.yearSelector,
               props: { key: 'yearsSelector' }
@@ -77,8 +136,8 @@ export const getNavFilters = createSelector(
           ],
           right: [
             // { type: FILTERS.dropdownSelector },
-            { type: FILTERS.recolorBySelector, props: { id: 'toolRecolorBy' } }
-            // { type: FILTERS.dropdownSelector, props: { id: 'viewSelector' }  },
+            { type: FILTERS.recolorBySelector, props: { id: 'toolRecolorBy' } },
+            { type: FILTERS.dropdownSelector, props: toolViewMode }
           ]
         };
       case 'explore':
