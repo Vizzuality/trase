4b2c398ef9d50bf0b9c05141fababe8c52b1bf18
sorodrigo <rodrigo.solis@vizzuality.com>
2018-12-28 13:31:44 +0100

    show profiles spinners until iframe has loaded

--- frontend/scripts/react-components/profile-node/profile-node-widgets/gfw-widget.component.jsx
+++ frontend/scripts/react-components/profile-node/profile-node-widgets/gfw-widget.component.jsx
@@ -3,79 +3,102 @@ import PropTypes from 'prop-types';
 import Widget from 'react-components/widgets/widget.component';
 import { GET_NODE_SUMMARY_URL } from 'utils/getURLFromParams';
 import camelCase from 'lodash/camelCase';
+import cx from 'classnames';
 import ShrinkingSpinner from 'react-components/shared/shrinking-spinner/shrinking-spinner.component';
 
-function GfwWidget(props) {
-  const { year, nodeId, contextId, profileType, renderIframes } = props;
-  const params = { node_id: nodeId, context_id: contextId, profile_type: profileType, year };
-  const GADM_DICTIONARY_URL = '/BRAZIL_GADM_GEOID.json';
-  return (
-    <Widget
-      params={[params]}
-      query={[GET_NODE_SUMMARY_URL, GADM_DICTIONARY_URL]}
-      raw={[false, true]}
-    >
-      {({ data, error, loading }) => {
-        if (error) {
-          // TODO: display a proper error message to the user
-          console.error('Error loading summary data for profile page', error);
-          return null;
-        }
+class GfwWidget extends React.PureComponent {
+  state = {
+    gladAlertsLoaded: false,
+    treeCoverLossLoaded: false
+  };
 
-        if (loading || !renderIframes) {
+  loadGladAlerts = () => this.setState({ gladAlertsLoaded: true });
+
+  loadTreeCoverLoss = () => this.setState({ treeCoverLossLoaded: true });
+
+  renderSpinner() {
+    return (
+      <div className="spinner-section">
+        <ShrinkingSpinner className="-large" />
+      </div>
+    );
+  }
+
+  render() {
+    const { year, nodeId, contextId, profileType, renderIframes } = this.props;
+    const { gladAlertsLoaded, treeCoverLossLoaded } = this.state;
+    const params = { node_id: nodeId, context_id: contextId, profile_type: profileType, year };
+    const GADM_DICTIONARY_URL = '/BRAZIL_GADM_GEOID.json';
+    return (
+      <Widget
+        params={[params]}
+        query={[GET_NODE_SUMMARY_URL, GADM_DICTIONARY_URL]}
+        raw={[false, true]}
+      >
+        {({ data, error, loading }) => {
+          if (error) {
+            // TODO: display a proper error message to the user
+            console.error('Error loading summary data for profile page', error);
+            return null;
+          }
+
+          if (loading || !renderIframes) {
+            return (
+              <React.Fragment>
+                {this.renderSpinner()}
+                {this.renderSpinner()}
+              </React.Fragment>
+            );
+          }
+
+          const { jurisdictionGeoId } = data[GET_NODE_SUMMARY_URL];
+          const gadm = data[GADM_DICTIONARY_URL];
+          const { path, match } = gadm[camelCase(jurisdictionGeoId)];
+
+          if (match < 0.9) {
+            return null;
+          }
+
           return (
             <React.Fragment>
-              <div className="spinner-section">
-                <ShrinkingSpinner className="-large" />
-              </div>
-              <div className="spinner-section">
-                <ShrinkingSpinner className="-large" />
-              </div>
+              {!treeCoverLossLoaded && this.renderSpinner()}
+              <section
+                className={cx('gfw-widget-container', { 'is-hidden': !treeCoverLossLoaded })}
+              >
+                <div className="row align-center">
+                  <div className="column small-10">
+                    <iframe
+                      width="100%"
+                      height="510"
+                      frameBorder="0"
+                      title="tree cover loss"
+                      onLoad={this.loadTreeCoverLoss}
+                      src={`//${GFW_WIDGETS_BASE_URL}/embed/dashboards/country/${path}?widget=treeLoss&trase=true`}
+                    />
+                  </div>
+                </div>
+              </section>
+              {!gladAlertsLoaded && this.renderSpinner()}
+              <section className={cx('gfw-widget-container', { 'is-hidden': !gladAlertsLoaded })}>
+                <div className="row align-center">
+                  <div className="column small-10">
+                    <iframe
+                      width="100%"
+                      height="510"
+                      frameBorder="0"
+                      title="glad alerts"
+                      onLoad={this.loadGladAlerts}
+                      src={`//${GFW_WIDGETS_BASE_URL}/embed/dashboards/country/${path}?widget=gladAlerts&trase=true`}
+                    />
+                  </div>
+                </div>
+              </section>
             </React.Fragment>
           );
-        }
-
-        const { jurisdictionGeoId } = data[GET_NODE_SUMMARY_URL];
-        const gadm = data[GADM_DICTIONARY_URL];
-        const { path, match } = gadm[camelCase(jurisdictionGeoId)];
-
-        if (match < 0.9) {
-          return null;
-        }
-
-        return (
-          <React.Fragment>
-            <section className="gfw-widget-container">
-              <div className="row align-center">
-                <div className="column small-10">
-                  <iframe
-                    title="tree cover loss"
-                    width="100%"
-                    height="510"
-                    frameBorder="0"
-                    src={`//${GFW_WIDGETS_BASE_URL}/embed/dashboards/country/${path}?widget=treeLoss&trase=true`}
-                  />
-                </div>
-              </div>
-            </section>
-            <section className="gfw-widget-container">
-              <div className="row align-center">
-                <div className="column small-10">
-                  <iframe
-                    title="gfw integration"
-                    width="100%"
-                    height="510"
-                    frameBorder="0"
-                    src={`//${GFW_WIDGETS_BASE_URL}/embed/dashboards/country/${path}?widget=gladAlerts&trase=true`}
-                  />
-                </div>
-              </div>
-            </section>
-          </React.Fragment>
-        );
-      }}
-    </Widget>
-  );
+        }}
+      </Widget>
+    );
+  }
 }
 
 GfwWidget.propTypes = {
