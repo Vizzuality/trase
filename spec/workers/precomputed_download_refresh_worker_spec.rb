029d8424e8e3ee14fb485ce3f13470c13fc3867a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-01-15 11:12:56 +0000

    Symbolize keys when passing options from sidekiq worker

---
+++ spec/workers/precomputed_download_refresh_worker_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe PrecomputedDownloadRefreshWorker, type: :worker do
+  Sidekiq::Testing.inline!
+
+  describe :call do
+    before(:each) do
+      Api::V3::Readonly::DownloadFlow.refresh(sync: true)
+    end
+
+    let(:context) { FactoryBot.create(:api_v3_context) }
+    it 'symbolizes options keys' do
+      expect(Api::V3::Download::FlowDownload).to receive(:new).
+        with(context, pivot: true).and_call_original
+      PrecomputedDownloadRefreshWorker.perform_async(context.id, pivot: true)
+    end
+  end
+end
