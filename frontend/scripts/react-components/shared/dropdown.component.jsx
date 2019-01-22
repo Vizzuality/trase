0051d8eff114637e718e22574422240b890ae135
sorodrigo <rodrigo.solis@vizzuality.com>
2019-01-22 10:33:25 +0100

    fix mobile dropdown events

--- frontend/scripts/react-components/shared/dropdown.component.jsx
+++ frontend/scripts/react-components/shared/dropdown.component.jsx
@@ -5,50 +5,43 @@ import cx from 'classnames';
 import PropTypes from 'prop-types';
 
 export default class Dropdown extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: false
-    };
-    this.onDropdownValueClicked = this.onDropdownValueClicked.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-    this.handleKeyUpOutside = this.handleKeyUpOutside.bind(this);
-  }
+  state = {
+    isOpen: false
+  };
 
   componentDidMount() {
     window.addEventListener('keyup', this.handleKeyUpOutside);
     window.addEventListener('mouseup', this.handleClickOutside);
-    window.addEventListener('touchstart', this.handleClickOutside);
+    window.addEventListener('touchend', this.handleClickOutside);
   }
 
   componentWillUnmount() {
     document.removeEventListener('keyup', this.handleKeyUpOutside);
     document.removeEventListener('mouseup', this.handleClickOutside);
-    document.removeEventListener('touchstart', this.handleClickOutside);
+    document.removeEventListener('touchend', this.handleClickOutside);
   }
 
-  onDropdownValueClicked(e, value) {
+  onDropdownValueClicked = (e, value) => {
     e.preventDefault();
     this.setState({ isOpen: false });
     this.props.onValueSelected(value);
-  }
+  };
 
-  onTitleClick() {
+  onTitleClick = () => {
     const { isOpen } = this.state;
     this.setState({ isOpen: !isOpen });
-  }
+  };
 
-  handleClickOutside(event) {
+  handleClickOutside = event => {
     if (!this.ref || this.ref.contains(event.target)) return;
     this.close();
-  }
+  };
 
-  handleKeyUpOutside(event) {
+  handleKeyUpOutside = event => {
     if (event.key === 'Escape' && this.state.isOpen) {
       this.close();
     }
-  }
+  };
 
   close() {
     this.setState({ isOpen: false });
@@ -75,12 +68,7 @@ export default class Dropdown extends Component {
         })}
       >
         <span className="dropdown-label">{label}</span>
-        <span
-          className="dropdown-title"
-          onClick={() => {
-            this.onTitleClick();
-          }}
-        >
+        <span className="dropdown-title" onClick={this.onTitleClick}>
           {valueRenderer ? valueRenderer(value) : value}
         </span>
         <ul
@@ -93,7 +81,7 @@ export default class Dropdown extends Component {
               className={cx('dropdown-item', getItemClassName(elem))}
               key={index}
               onClick={e => this.onDropdownValueClicked(e, elem)}
-              onTouchStart={e => this.onDropdownValueClicked(e, elem)}
+              onTouchEnd={e => this.onDropdownValueClicked(e, elem)}
             >
               {valueRenderer ? valueRenderer(elem) : elem}
             </li>
