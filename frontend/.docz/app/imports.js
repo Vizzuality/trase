f8d8009b5e53bbf20773a994cae717be1f27d947
sorodrigo <rodrigo.solis@vizzuality.com>
2019-01-31 19:15:07 +0100

    adds search input to component-kit

--- frontend/.docz/app/imports.js
+++ frontend/.docz/app/imports.js
@@ -3,8 +3,10 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "docs-buttons" */ 'docs/buttons.mdx'),
   'docs/cards.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "docs-cards" */ 'docs/cards.mdx'),
+  'docs/inputs.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-inputs" */ 'docs/inputs.mdx'),
   'docs/lists.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "docs-lists" */ 'docs/lists.mdx'),
   'docs/texts.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-texts" */ 'docs/texts.mdx'),
-}
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-texts" */ 'docs/texts.mdx')
+};
