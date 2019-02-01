88b70763323abd71d40af80392cdaa08e9cbecb2
sorodrigo <rodrigo.solis@vizzuality.com>
2019-02-01 13:07:53 +0100

    ignore docz folder

--- frontend/doczrc.js
+++ frontend/doczrc.js
@@ -1,17 +1,20 @@
+// eslint-disable-next-line
 import merge from 'webpack-merge';
 import webpackConfig from './config/webpack.config';
 
 export default {
+  dest: './docs/dist',
   title: 'Trase',
   themeConfig: {
     colors: {
       primary: '#34444C',
       sidebarBg: '#fff0c2'
-    },
+    }
   },
-  modifyBundlerConfig: config => merge(config, {
-    resolve: webpackConfig.resolve,
-    module: webpackConfig.module
-  }),
+  modifyBundlerConfig: config =>
+    merge(config, {
+      resolve: webpackConfig.resolve,
+      module: webpackConfig.module
+    }),
   plugins: []
-}
+};
