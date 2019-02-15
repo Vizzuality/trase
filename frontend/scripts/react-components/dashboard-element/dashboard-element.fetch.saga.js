422263aa524ae9bc47aa4bbd9669a717a788b33c
Alvaro Leal <pursuance@gmail.com>
2019-02-15 17:45:09 +0100

    Move fetchs to sagas, split file, fix bug

---
+++ frontend/scripts/react-components/dashboard-element/dashboard-element.fetch.saga.js
@@ -0,0 +1,158 @@
+import { put, call, cancelled } from 'redux-saga/effects';
+import {
+  DASHBOARD_ELEMENT__SET_PANEL_TABS,
+  DASHBOARD_ELEMENT__SET_PANEL_DATA,
+  DASHBOARD_ELEMENT__SET_MORE_PANEL_DATA,
+  getDashboardPanelParams,
+  setDashboardPanelLoadingItems,
+  DASHBOARD_ELEMENT__SET_SEARCH_RESULTS
+} from 'react-components/dashboard-element/dashboard-element.actions';
+import {
+  getURLFromParams,
+  GET_DASHBOARD_OPTIONS_URL,
+  GET_DASHBOARD_OPTIONS_TABS_URL,
+  GET_DASHBOARD_SEARCH_RESULTS_URL
+} from 'utils/getURLFromParams';
+import { fetchWithCancel } from './fetch-with-cancel';
+
+export function* getDashboardPanelData(dashboardElement, optionsType, options) {
+  const { page, activeTab } = dashboardElement[`${dashboardElement.activePanelId}Panel`];
+  const tab = activeTab && activeTab.id;
+  const params = getDashboardPanelParams(dashboardElement, optionsType, {
+    page,
+    ...options
+  });
+  const url = getURLFromParams(GET_DASHBOARD_OPTIONS_URL, params);
+  const key = optionsType !== 'attributes' ? optionsType : 'indicators'; // FIXME
+  yield put({
+    type: DASHBOARD_ELEMENT__SET_PANEL_DATA,
+    payload: {
+      key,
+      tab,
+      data: null,
+      meta: null,
+      loading: true
+    }
+  });
+  const { source, fetchPromise } = fetchWithCancel(url);
+  try {
+    const { data } = yield call(fetchPromise);
+    yield put({
+      type: DASHBOARD_ELEMENT__SET_PANEL_DATA,
+      payload: {
+        key,
+        tab,
+        data: data.data,
+        meta: data.meta,
+        loading: false
+      }
+    });
+  } catch (e) {
+    console.error('Error', e);
+  } finally {
+    if (yield cancelled()) {
+      console.error('Cancelled', tab);
+      if (source) {
+        source.cancel();
+      }
+    }
+  }
+}
+
+export function* getDashboardPanelSectionTabs(dashboardElement, optionsType) {
+  const params = getDashboardPanelParams(dashboardElement, optionsType);
+  const url = getURLFromParams(GET_DASHBOARD_OPTIONS_TABS_URL, params);
+  const { source, fetchPromise } = fetchWithCancel(url);
+  try {
+    const { data } = yield call(fetchPromise);
+    yield put({
+      type: DASHBOARD_ELEMENT__SET_PANEL_TABS,
+      payload: {
+        data: data.data
+      }
+    });
+  } catch (e) {
+    console.error('Error', e);
+  } finally {
+    if (yield cancelled()) {
+      console.error('Cancelled', url);
+      if (source) {
+        source.cancel();
+      }
+    }
+  }
+}
+
+export function* getMoreDashboardPanelData(dashboardElement, optionsType, activeTab, direction) {
+  const { page } = dashboardElement[`${dashboardElement.activePanelId}Panel`];
+  const params = getDashboardPanelParams(dashboardElement, optionsType, {
+    page
+  });
+  const url = getURLFromParams(GET_DASHBOARD_OPTIONS_URL, params);
+  const key = optionsType !== 'attributes' ? optionsType : 'indicators'; // FIXME
+  const { source, fetchPromise } = fetchWithCancel(url);
+  try {
+    const { data } = yield call(fetchPromise);
+    yield put({
+      type: DASHBOARD_ELEMENT__SET_MORE_PANEL_DATA,
+      payload: {
+        key,
+        tab: activeTab && activeTab.id,
+        direction,
+        data: data.data
+      }
+    });
+  } catch (e) {
+    console.error('Error', e);
+    yield put(setDashboardPanelLoadingItems(false));
+  } finally {
+    if (yield cancelled()) {
+      console.error('Cancelled', url);
+      if (source) {
+        source.cancel();
+      }
+    }
+  }
+}
+
+export function* fetchDashboardPanelSearchResults(dashboardElement, query) {
+  if (!query) return;
+  let optionsType = dashboardElement.activePanelId;
+  if (
+    optionsType === 'sources' &&
+    dashboardElement.sourcesPanel.activeItem === null &&
+    dashboardElement.sourcesPanel.activeTab === null
+  ) {
+    optionsType = 'countries';
+  }
+  const filters = {
+    ...getDashboardPanelParams(dashboardElement, optionsType),
+    node_types_ids: undefined
+  };
+  const params = {
+    ...filters,
+    q: query
+  };
+  const url = getURLFromParams(GET_DASHBOARD_SEARCH_RESULTS_URL, params);
+
+  const { source, fetchPromise } = fetchWithCancel(url);
+  try {
+    const { data } = yield call(fetchPromise);
+    console.log('json', data, url);
+    yield put({
+      type: DASHBOARD_ELEMENT__SET_SEARCH_RESULTS,
+      payload: {
+        data: data.data
+      }
+    });
+  } catch (e) {
+    console.error('Error', e);
+  } finally {
+    if (yield cancelled()) {
+      console.error('Cancelled', url);
+      if (source) {
+        source.cancel();
+      }
+    }
+  }
+}
