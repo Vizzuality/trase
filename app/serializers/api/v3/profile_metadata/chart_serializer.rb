116d99823d10ef2cc6797a8837f75fbcb1f09f22
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-02-15 11:59:00 +0000

    Added chart type to API response

--- app/serializers/api/v3/profile_metadata/chart_serializer.rb
+++ app/serializers/api/v3/profile_metadata/chart_serializer.rb
@@ -2,7 +2,7 @@ module Api
   module V3
     module ProfileMetadata
       class ChartSerializer < ActiveModel::Serializer
-        attributes :id, :identifier, :title, :position
+        attributes :id, :chart_type, :identifier, :title, :position
 
         has_many :children, serializer: self
       end
