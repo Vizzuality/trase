5c1b5381d846420a9eea2a946eca6a499c955728
Alvaro Leal <pursuance@gmail.com>
2019-02-20 17:35:07 +0100

    Remove unneded tests

--- frontend/scripts/tests/dashboard-element/fetch.saga.test.js
+++ frontend/scripts/tests/dashboard-element/fetch.saga.test.js
@@ -1,20 +1,11 @@
-import { put, cancelled, call } from 'redux-saga/effects';
+import { cancelled } from 'redux-saga/effects';
 import { fetchWithCancel } from 'react-components/dashboard-element/fetch-with-cancel';
 
 import {
   getDashboardPanelData,
   getDashboardPanelSectionTabs,
-  fetchDashboardPanelSearchResults,
-  getMoreDashboardPanelData,
-  removeLoadingSpinner
+  fetchDashboardPanelSearchResults
 } from 'react-components/dashboard-element/dashboard-element.fetch.saga';
-import {
-  DASHBOARD_ELEMENT__SET_PANEL_DATA,
-  DASHBOARD_ELEMENT__SET_PANEL_TABS,
-  DASHBOARD_ELEMENT__SET_SEARCH_RESULTS,
-  DASHBOARD_ELEMENT__SET_MORE_PANEL_DATA,
-  DASHBOARD_ELEMENT__SET_LOADING_ITEMS
-} from 'react-components/dashboard-element/dashboard-element.actions';
 import { getURLFromParams } from 'utils/getURLFromParams';
 
 jest.mock('react-components/dashboard-element/fetch-with-cancel', () => ({
@@ -62,60 +53,16 @@ const sourceMock = { cancel: jest.fn() };
 getURLFromParams.mockImplementation(() => someUrl);
 fetchWithCancel.mockImplementation(() => ({ source: sourceMock, fetchPromise: () => {} }));
 
+const optionsType = 'companies';
+const query = 'Bra';
 describe('getDashboardPanelData', () => {
-  const data = {
-    data: { hello: 1 },
-    meta: {}
-  };
-  const optionsType = 'companies';
-  const options = undefined; // Do we use this page param?
   jest.mock('utils/getURLFromParams', () => ({
     getURLFromParams: jest.fn()
   }));
 
-  it('Calls DASHBOARD_ELEMENT__SET_PANEL_DATA action to clear data', () => {
-    const generator = getDashboardPanelData(dashboardElement, optionsType, options);
-    expect(generator.next().value).toEqual(
-      put({
-        type: DASHBOARD_ELEMENT__SET_PANEL_DATA,
-        payload: {
-          key: optionsType,
-          tab: 1,
-          data: null,
-          meta: null,
-          loading: true
-        }
-      })
-    );
+  it('Cancels if the fetch is cancelled', () => {
+    const generator = getDashboardPanelData(dashboardElement, optionsType);
     generator.next();
-    expect(generator.next({ data }).value).toEqual(
-      put({
-        type: DASHBOARD_ELEMENT__SET_PANEL_DATA,
-        payload: {
-          key: optionsType,
-          tab: 1,
-          data: data.data,
-          meta: data.meta,
-          loading: false
-        }
-      })
-    );
-  });
-
-  it('Calls DASHBOARD_ELEMENT__SET_PANEL_DATA action to clear data and Cancels if the fetch is cancelled', () => {
-    const generator = getDashboardPanelData(dashboardElement, optionsType, options);
-    expect(generator.next().value).toEqual(
-      put({
-        type: DASHBOARD_ELEMENT__SET_PANEL_DATA,
-        payload: {
-          key: optionsType,
-          tab: 1,
-          data: null,
-          meta: null,
-          loading: true
-        }
-      })
-    );
     generator.next();
     expect(generator.return().value).toEqual(cancelled());
     generator.next(true);
@@ -124,25 +71,6 @@ describe('getDashboardPanelData', () => {
 });
 
 describe('getDashboardPanelSectionTabs', () => {
-  const data = {
-    data: { hello: 1 },
-    meta: {}
-  };
-  const optionsType = 'companies';
-
-  it('Calls DASHBOARD_ELEMENT__SET_PANEL_TABS with the fetch data', () => {
-    const generator = getDashboardPanelSectionTabs(dashboardElement, optionsType);
-    generator.next();
-    expect(generator.next({ data }).value).toEqual(
-      put({
-        type: DASHBOARD_ELEMENT__SET_PANEL_TABS,
-        payload: {
-          data: data.data
-        }
-      })
-    );
-  });
-
   it('Cancels if the fetch is cancelled', () => {
     const generator = getDashboardPanelSectionTabs(dashboardElement, optionsType);
     generator.next();
@@ -153,43 +81,7 @@ describe('getDashboardPanelSectionTabs', () => {
 });
 
 describe('getMoreDashboardPanelData', () => {
-  const data = {
-    data: { hello: 1 },
-    meta: {}
-  };
-
-  it('Calls DASHBOARD_ELEMENT__SET_MORE_PANEL_DATA action', () => {
-    const optionsType = 'companies';
-    const activeTab = { id: 4, name: 'BIOME' };
-    const direction = 'FORWARD';
-    const loadingAction = loading => ({
-      payload: { loadingItems: loading },
-      type: DASHBOARD_ELEMENT__SET_LOADING_ITEMS
-    });
-    const generator = getMoreDashboardPanelData(
-      dashboardElement,
-      optionsType,
-      activeTab,
-      direction
-    );
-    expect(generator.next().value).toEqual(put(loadingAction(true)));
-    generator.next();
-    expect(generator.next({ data }).value).toEqual(
-      put({
-        type: DASHBOARD_ELEMENT__SET_MORE_PANEL_DATA,
-        payload: {
-          key: optionsType,
-          tab: 4,
-          data: data.data,
-          direction
-        }
-      })
-    );
-    expect(generator.next().value).toEqual(call(removeLoadingSpinner));
-  });
-
-  it('Calls DASHBOARD_ELEMENT__SET_MORE_PANEL_DATA action to clear data', () => {
-    const query = 'Bra';
+  it('Cancels if the fetch is cancelled', () => {
     const generator = fetchDashboardPanelSearchResults(dashboardElement, query);
     generator.next();
     expect(generator.return().value).toEqual(cancelled());
@@ -199,34 +91,7 @@ describe('getMoreDashboardPanelData', () => {
 });
 
 describe('fetchDashboardPanelSearchResults', () => {
-  const data = {
-    data: { hello: 1 },
-    meta: {}
-  };
-
-  it('returns if there is no query ', () => {
-    const query = null;
-    const generator = fetchDashboardPanelSearchResults(dashboardElement, query);
-    generator.next();
-    expect(generator.next().value).toEqual(undefined);
-  });
-
-  it('Calls DASHBOARD_ELEMENT__SET_SEARCH_RESULTS action to clear data', () => {
-    const query = 'Bra';
-    const generator = fetchDashboardPanelSearchResults(dashboardElement, query);
-    generator.next();
-    expect(generator.next({ data }).value).toEqual(
-      put({
-        type: DASHBOARD_ELEMENT__SET_SEARCH_RESULTS,
-        payload: {
-          data: data.data
-        }
-      })
-    );
-  });
-
-  it('Calls DASHBOARD_ELEMENT__SET_SEARCH_RESULTS action to clear data', () => {
-    const query = 'Bra';
+  it('Cancels if the fetch is cancelled', () => {
     const generator = fetchDashboardPanelSearchResults(dashboardElement, query);
     generator.next();
     expect(generator.return().value).toEqual(cancelled());
