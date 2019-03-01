7c8226271772affa74898ba54d6085a097188457
Alvaro Leal <pursuance@gmail.com>
2019-03-01 12:37:19 +0100

    Fix tags group container class functions

--- frontend/scripts/react-components/shared/tags-group/tags-group.jsx
+++ frontend/scripts/react-components/shared/tags-group/tags-group.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Component from 'react-components/shared/tags-group/tags-group.component';
+import TagsGroupComponent from 'react-components/shared/tags-group/tags-group.component';
 
 class TagsGroupContainer extends React.Component {
-  removeOption(optionToClear, part) {
+  static propTypes = {
+    clearPanel: PropTypes.func,
+    removeSentenceItem: PropTypes.func,
+    readOnly: PropTypes.bool
+  };
+
+  removeOption = (optionToClear, part) => {
     const { removeSentenceItem, clearPanel } = this.props;
 
     if (optionToClear.value === 'clear-all') {
@@ -12,39 +18,35 @@ class TagsGroupContainer extends React.Component {
     } else {
       removeSentenceItem(part.value.find(v => v.id === optionToClear.value), part.panel);
     }
-  }
+  };
 
-  getOptions(values) {
+  getOptions = values => {
     const { readOnly } = this.props;
     const iconProp = readOnly ? {} : { icon: 'close' };
     const options = values.map(p => ({ value: p.id, label: p.name, ...iconProp }));
     return readOnly
       ? options
       : options.concat({ label: 'CLEAR ALL', value: 'clear-all', ...iconProp });
-  }
+  };
 
-  clearSingleItem(part) {
+  clearSingleItem = part => {
     const { removeSentenceItem, clearPanel } = this.props;
     if (part.panel === 'companies') {
       removeSentenceItem(part.value[0], part.panel);
     } else clearPanel(part.panel);
-  }
+  };
 
   render() {
-    return React.createElement(Component, {
-      ...this.props,
-      removeOption: this.removeOption.bind(this),
-      clearSingleItem: this.clearSingleItem.bind(this),
-      getOptions: this.getOptions.bind(this)
-    });
+    return (
+      <TagsGroupComponent
+        {...this.props}
+        removeOption={this.removeOption}
+        clearSingleItem={this.clearSingleItem}
+        getOptions={this.getOptions}
+      />
+    );
   }
 }
-
-TagsGroupContainer.propTypes = {
-  clearPanel: PropTypes.func,
-  removeSentenceItem: PropTypes.func,
-  readOnly: PropTypes.bool
-};
 
 TagsGroupContainer.defaultProps = {
   readOnly: false
