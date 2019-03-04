543b99d4b60baffdf99bc4abe893f245e6dba9e5
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-03-04 17:59:43 +0000

    Add specs for tooltip logic

---
+++ spec/factories/api/v3/api_v3_quant_country_properties.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :api_v3_quant_country_property, class: 'Api::V3::QuantCountryProperty' do
+    association :quant, factory: :api_v3_quant
+    association :country, factory: :api_v3_country
+    tooltip_text { 'Country specific tooltip text for QUANT' }
+  end
+end
