3a02c53818b8aff106fb753a945f66dc6d0a6f81
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-03-12 12:25:54 +0000

    Get correct tooltip in map_layers endpoint

--- lib/api/v3/profiles/get_tooltip_per_attribute.rb
+++ lib/api/v3/profiles/get_tooltip_per_attribute.rb
@@ -42,28 +42,28 @@ module Api
           context_specific_tooltip ||
             country_specific_tooltip ||
             commodity_specific_tooltip ||
-            ro_chart_attribute.tooltip_text
+            ro_chart_attribute[:tooltip_text]
         end
 
         def attribute
-          "#{ro_chart_attribute.original_type.downcase}_id".to_sym
+          "#{ro_chart_attribute[:original_type].downcase}_id".to_sym
         end
 
         def context_specific_tooltip
           context_specific_property.
-            find_by(attribute => ro_chart_attribute.original_id,
+            find_by(attribute => ro_chart_attribute[:original_id],
                     context_id: context.id)&.tooltip_text
         end
 
         def country_specific_tooltip
           country_specific_property.
-            find_by(attribute => ro_chart_attribute.original_id,
+            find_by(attribute => ro_chart_attribute[:original_id],
                     country_id: context.country_id)&.tooltip_text
         end
 
         def commodity_specific_tooltip
           commodity_specific_property.
-            find_by(attribute => ro_chart_attribute.original_id,
+            find_by(attribute => ro_chart_attribute[:original_id],
                     commodity_id: context.commodity_id)&.tooltip_text
         end
       end
