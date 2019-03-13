91ddf5e363eb7cf912318206e111b5e2b34aaada
Alvaro Leal <pursuance@gmail.com>
2019-03-13 17:33:48 +0100

    Fix text component

--- frontend/scripts/react-components/shared/text/text.component.jsx
+++ frontend/scripts/react-components/shared/text/text.component.jsx
@@ -5,17 +5,32 @@ import cx from 'classnames';
 import './text.scss';
 
 function Text(props) {
-  const { as, variant, color, size, align, weight, transform, children, className ...rest } = props;
+  const {
+    as,
+    variant,
+    color,
+    size,
+    align,
+    weight,
+    transform,
+    children,
+    className,
+    ...rest
+  } = props;
   const textProps = {
     ...rest,
-    className: cx('c-text', variant, {
-      [`color-${color}`]: color,
-      [`size-${size}`]: size,
-      [`weight-${weight}`]: weight,
-      [`align-${align}`]: align,
-      [`transform-${transform}`]: transform
-    }),
-    className
+    className: cx(
+      'c-text',
+      variant,
+      {
+        [`color-${color}`]: color,
+        [`size-${size}`]: size,
+        [`weight-${weight}`]: weight,
+        [`align-${align}`]: align,
+        [`transform-${transform}`]: transform
+      },
+      className
+    )
   };
   return React.createElement(as, textProps, children);
 }
