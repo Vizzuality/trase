78ea1b9e717c8fcacda8a00f41afa06463f82f5a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-03-15 09:37:15 +0000

    Single year, ncont overview

---
+++ app/services/api/v3/dashboards/charts/single_year_ncont_overview.rb
@@ -0,0 +1,77 @@
+module Api
+  module V3
+    module Dashboards
+      module Charts
+        class SingleYearNcontOverview
+          # @param chart_parameters [Api::V3::Dashboards::ChartParameters]
+          def initialize(chart_parameters)
+            @cont_attribute = chart_parameters.cont_attribute
+            @context = chart_parameters.context
+            @year = chart_parameters.start_year
+            @ncont_attribute = chart_parameters.ncont_attribute
+            @nodes_ids_by_position = chart_parameters.nodes_ids_by_position
+            initialize_query
+          end
+
+          def call
+            @data = @query.map do |r|
+              r.attributes.slice('x', 'y0').symbolize_keys
+            end
+            @meta = {
+              xAxis: {
+                label: @ncont_attribute.display_name,
+                prefix: '',
+                format: '',
+                suffix: @ncont_attribute.unit
+              },
+              yAxis: {
+                label: @cont_attribute.display_name,
+                prefix: '',
+                format: '',
+                suffix: @cont_attribute.unit
+              },
+              x: {
+                type: 'category', # category || date || number
+                label: @ncont_attribute.display_name,
+                tooltip: {prefix: '', format: '', suffix: ''}
+              },
+              y0: {
+                type: 'number',
+                label: @cont_attribute.display_name,
+                tooltip: {prefix: '', format: '', suffix: ''}
+              }
+            }
+            {data: @data, meta: @meta}
+          end
+
+          private
+
+          def initialize_query
+            cont_attr_table = @cont_attribute.flow_values_class.table_name
+            ncont_attr_table = @ncont_attribute.flow_values_class.table_name
+            @query = Api::V3::Flow.
+              select([
+                "#{ncont_attr_table}.value AS x",
+                "SUM(#{cont_attr_table}.value) AS y0"
+              ]).
+              joins(cont_attr_table.to_sym).
+              joins("LEFT JOIN #{ncont_attr_table} ON #{ncont_attr_table}.flow_id = flows.id").
+              where(
+                context_id: @context.id,
+                year: @year,
+                "#{cont_attr_table}.#{@cont_attribute.attribute_id_name}" =>
+                  @cont_attribute.original_id,
+                "#{ncont_attr_table}.#{@ncont_attribute.attribute_id_name}" =>
+                  @ncont_attribute.original_id
+              ).
+              group(1)
+
+            @nodes_ids_by_position.each do |position, nodes_ids|
+              @query = @query.where('ARRAY[flows.path[?]] && ARRAY[?]', position + 1, nodes_ids)
+            end
+          end
+        end
+      end
+    end
+  end
+end
