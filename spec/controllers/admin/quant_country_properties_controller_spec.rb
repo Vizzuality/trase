82cac09bc6f4e8ae0f42260b2d1efedbfb383175
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-03-15 11:23:01 +0000

    Increase test coverage in quant country properties ctrl spec

--- spec/controllers/admin/quant_country_properties_controller_spec.rb
+++ spec/controllers/admin/quant_country_properties_controller_spec.rb
@@ -18,9 +18,24 @@ RSpec.describe Admin::QuantCountryPropertiesController, type: :controller do
       )
     }
 
+    let(:invalid_attributes) {
+      FactoryBot.attributes_for(
+        :api_v3_quant_country_property,
+        quant_id: nil,
+        country_id: country.id,
+        tooltip_text: tooltip_text
+      )
+    }
+
     it 'clears cache' do
       expect(controller).to receive(:clear_cache_for_regexp)
       post :create, params: {api_v3_quant_country_property: valid_attributes}
+      expect(response).not_to render_template(:new)
+    end
+
+    it 'fails if validation is not met' do
+      post :create, params: {api_v3_quant_country_property: invalid_attributes}
+      expect(response).to render_template(:new)
     end
   end
 end
