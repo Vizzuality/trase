da6c18e689a513ed89345d0ab930f7606b96f245
sorodrigo <rodrigo.solis@vizzuality.com>
2019-03-21 19:28:49 +0100

    adds nav variant to dropdown

---
+++ frontend/scripts/react-components/nav/filters-nav/nav-dropdown-selector/nav-dropdown-selector.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Dropdown from 'react-components/shared/dropdown';
+
+function NavDropdownSelector(props) {
+  return <Dropdown variant="nav" placement="bottom-start" {...props} />;
+}
+
+export default NavDropdownSelector;
