4443eead604cd04fe7e8c81404d1c2551863fd40
sorodrigo <rodrigo.solis@vizzuality.com>
2019-03-22 11:59:47 +0100

    use new dropdown on logistics map

--- frontend/scripts/react-components/nav/filters-nav/nav-dropdown-selector/nav-dropdown-selector.component.jsx
+++ frontend/scripts/react-components/nav/filters-nav/nav-dropdown-selector/nav-dropdown-selector.component.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Dropdown from 'react-components/shared/dropdown';
 
 function NavDropdownSelector(props) {
-  return <Dropdown variant="nav" placement="bottom-start" {...props} />;
+  return (
+    <Dropdown
+      {...props}
+      variant="nav"
+      placement="bottom-start"
+      onChange={item => props.onSelected(item.value)}
+    />
+  );
 }
+
+NavDropdownSelector.propTypes = {
+  onSelected: PropTypes.func.isRequired
+};
 
 export default NavDropdownSelector;
