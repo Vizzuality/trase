b368c4d9edf67380269d7e33ae5f3020b2a12bb7
Alvaro Leal <pursuance@gmail.com>
2019-03-25 12:57:30 +0100

    Use snapshots for selector test

--- frontend/scripts/tests/dashboard-widget/selectors.test.js
+++ frontend/scripts/tests/dashboard-widget/selectors.test.js
@@ -1,17 +1,48 @@
-import { getConfig } from 'react-components/dashboard-element/dashboard-widget/dashboard-widget.selectors';
+import { makeGetConfig } from 'react-components/dashboard-element/dashboard-widget/dashboard-widget.selectors';
 import CHART_CONFIG from 'react-components/dashboard-element/dashboard-widget/dashboard-widget-config';
-import { meta, yKeys, colors, parsedConfig } from 'scripts/tests/dashboard-widget/selectors.mocks';
 
+const meta = {
+  xAxis: {
+    label: 'Year',
+    prefix: '',
+    format: '',
+    suffix: ''
+  },
+  yAxis: {
+    label: 'Water scarcity',
+    prefix: '',
+    format: '',
+    suffix: '/7'
+  },
+  x: {
+    type: 'category',
+    label: 'Year',
+    tooltip: {
+      prefix: '',
+      format: '',
+      suffix: ''
+    }
+  },
+  y1: {
+    type: 'number',
+    label: '',
+    tooltip: {
+      prefix: '',
+      format: '',
+      suffix: ''
+    }
+  }
+};
+
+// makeGetConfig is a function that returns the selector just to be able to memoize the selector result
 describe('Widget config parse selectors', () => {
-  describe('getConfig', () => {
+  describe('makeGetConfig', () => {
     it('returns the defaultConfig if there is no meta', () => {
-      expect(getConfig.resultFunc(null, yKeys, colors, CHART_CONFIG.line)).toEqual(
-        CHART_CONFIG.line
-      );
+      expect(makeGetConfig()(null, { meta: null })).toEqual(CHART_CONFIG.line);
     });
 
     it('Parses the config', () => {
-      expect(getConfig.resultFunc(meta, yKeys, colors, CHART_CONFIG.line)).toEqual(parsedConfig);
+      expect(makeGetConfig()(null, { meta, chartType: 'line' })).toMatchSnapshot();
     });
   });
 });
