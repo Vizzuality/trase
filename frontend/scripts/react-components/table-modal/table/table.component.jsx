35927da5e7e4842ddeaaedafab12a28c4c9ef7e6
Alvaro Leal <pursuance@gmail.com>
2019-04-02 16:12:49 +0200

    Table WIP

--- frontend/scripts/react-components/table-modal/table/table.component.jsx
+++ frontend/scripts/react-components/table-modal/table/table.component.jsx
@@ -1,41 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import 'react-components/table-modal/table-modal.scss';
-import { Table as VirtualizedTable, Column } from 'react-virtualized/dist/commonjs/Table';
-import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+import 'react-components/table-modal/table/table.scss';
+import ShrinkingSpinner from 'react-components/shared/shrinking-spinner/shrinking-spinner.component';
+import { FixedSizeGrid } from 'react-window';
+import cx from 'classnames';
+import Text from 'react-components/shared/text';
 
-function Table({ data }) {
-  console.log('data', data);
+function Table(props) {
+  const { data, headings, width, height, rowHeight, loading, className } = props;
+
   if (!data) return null;
+  const columnWidth = width / headings.length;
+
   return (
-    <div className="c-table">
-      <AutoSizer disableHeight>
-        {({ width }) => (
-          <VirtualizedTable
-            rowCount={data.length}
-            width={width}
-            height={500}
-            rowGetter={({ index }) => data[index]}
-            rowHeight={30}
-          >
-            {data.map(column => (
-              <Column
-                className="table-column"
-                key={column}
-                dataKey={column}
-                flexGrow={0}
-                width={100}
-              />
-            ))}
-          </VirtualizedTable>
-        )}
-      </AutoSizer>
-    </div>
+    <table className="c-table">
+      <tbody>
+        <tr className="table-heading" style={{ width }}>
+          {headings.map((h, index) => (
+            <th className="header-item" key={`${h}${index}`}>
+              <Text color="white" weight="bold" size="md" variant="mono">
+                {h}
+              </Text>
+            </th>
+          ))}
+        </tr>
+        <FixedSizeGrid
+          className={cx('c-grid-list', className)}
+          height={height}
+          width={width}
+          columnWidth={columnWidth}
+          rowHeight={rowHeight}
+          rowCount={data.length}
+          columnCount={headings.length}
+        >
+          {({ rowIndex, columnIndex, style }) => {
+            const item = data[rowIndex][columnIndex];
+            if (typeof item === 'undefined') return null;
+            return (
+              <td
+                style={style}
+                className={cx('header-item', rowIndex % 2 ? 'list-item-odd' : 'list-item-even')}
+              >
+                <Text size="md" as="span" variant="mono">
+                  {item}
+                </Text>
+              </td>
+            );
+          }}
+        </FixedSizeGrid>
+      </tbody>
+      {loading && (
+        <div className="grid-list-loading-container">
+          <ShrinkingSpinner className="-small -dark grid-list-spinner" />
+        </div>
+      )}
+    </table>
   );
 }
 
 Table.propTypes = {
-  data: PropTypes.array
+  loading: PropTypes.bool,
+  className: PropTypes.string,
+  data: PropTypes.array.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  rowHeight: PropTypes.number.isRequired,
+  headings: PropTypes.array.isRequired
 };
 
 export default Table;
