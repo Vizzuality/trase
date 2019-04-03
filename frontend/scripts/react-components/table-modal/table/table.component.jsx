77ea85d66b81c6f7c8689b24517c1ad116e6d340
Alvaro Leal <pursuance@gmail.com>
2019-04-03 15:28:59 +0200

    Fix getMaxLength and warnings

--- frontend/scripts/react-components/table-modal/table/table.component.jsx
+++ frontend/scripts/react-components/table-modal/table/table.component.jsx
@@ -7,7 +7,7 @@ import cx from 'classnames';
 import Text from 'react-components/shared/text';
 import maxBy from 'lodash/maxBy';
 
-const getMaxLength = row => String(maxBy(row, cell => String(cell.length))).length;
+const getMaxLength = row => String(maxBy(row, cell => String(cell).length)).length;
 
 function Table(props) {
   const { data, headings, width, height, loading, rowHeight, className } = props;
@@ -17,7 +17,7 @@ function Table(props) {
 
   return (
     <table className={cx('c-table', className)}>
-      <tbody>
+      <thead>
         <tr className="table-heading" style={{ width }}>
           {headings.map((h, index) => (
             <th className="header-item" key={`${h}${index}`}>
@@ -27,31 +27,33 @@ function Table(props) {
             </th>
           ))}
         </tr>
-        <VariableSizeGrid
-          className={cx('c-grid-list')}
-          height={height}
-          width={width}
-          columnWidth={() => columnWidth}
-          rowHeight={index => rowHeight || getMaxLength(data[index]) + minRowHeight}
-          rowCount={data.length}
-          columnCount={headings.length}
-        >
-          {({ rowIndex, columnIndex, style }) => {
-            const item = data[rowIndex][columnIndex];
-            if (typeof item === 'undefined') return null;
-            return (
-              <td
-                style={style}
-                className={cx('header-item', rowIndex % 2 ? 'list-item-odd' : 'list-item-even')}
-              >
-                <Text size="md" as="span" variant="mono">
-                  {item}
-                </Text>
-              </td>
-            );
-          }}
-        </VariableSizeGrid>
-      </tbody>
+      </thead>
+      <VariableSizeGrid
+        className={cx('c-grid-list')}
+        height={height}
+        width={width}
+        outerElementType="tbody"
+        innerElementType="tr"
+        columnWidth={() => columnWidth}
+        rowHeight={index => rowHeight || getMaxLength(data[index]) + minRowHeight}
+        rowCount={data.length}
+        columnCount={headings.length}
+      >
+        {({ rowIndex, columnIndex, style }) => {
+          const item = data[rowIndex][columnIndex];
+          if (typeof item === 'undefined') return null;
+          return (
+            <td
+              style={style}
+              className={cx('header-item', rowIndex % 2 ? 'list-item-odd' : 'list-item-even')}
+            >
+              <Text size="md" as="span" variant="mono">
+                {item}
+              </Text>
+            </td>
+          );
+        }}
+      </VariableSizeGrid>
       {loading && (
         <div className="grid-list-loading-container">
           <ShrinkingSpinner className="-small -dark grid-list-spinner" />
