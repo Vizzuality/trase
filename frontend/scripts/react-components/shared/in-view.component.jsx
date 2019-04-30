bd5ecd040742d916ebd8bcdceb9a8e706a6d3322
sorodrigo <rodrigo.solis@vizzuality.com>
2019-04-30 11:23:46 +0200

    fix e2e tests for intersection observer

--- frontend/scripts/react-components/shared/in-view.component.jsx
+++ frontend/scripts/react-components/shared/in-view.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { InView } from 'react-intersection-observer';
 
 function InViewFallback(props) {
-  if ('IntersectionObserver' in window) {
+  if ('IntersectionObserver' in window && process.env.NODE_ENV !== 'test') {
     return <InView {...props} />;
   }
 
