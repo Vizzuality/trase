44b03b0567a1572def351542d67ae2ec0832408f
Alvaro Leal <pursuance@gmail.com>
2019-05-06 21:28:48 +0200

    Bridge map dimensions component

---
+++ frontend/scripts/react-components/tool/map-dimensions/map-dimensions.container.js
@@ -0,0 +1,42 @@
+import { connect } from 'react-redux';
+import MapDimensions from 'react-components/tool/map-dimensions/map-dimensions.component';
+import { toggleMapSidebarGroup, toggleMapDimension } from 'actions/tool.actions';
+import { loadTooltip } from 'actions/app.actions';
+import { mapToVanilla } from 'react-components/shared/vanilla-react-bridge.component';
+
+const isCloroplethEnabled = state => {
+  const firstColumnId = state.tool.selectedColumnsIds[0];
+  const column = state.tool.columns.find(c => c.id === firstColumnId);
+  return column ? !column.isChoroplethDisabled : true;
+};
+const mapStateToProps = state => ({
+  mapDimensionsGroups: state.tool.mapDimensionsGroups,
+  expandedMapSidebarGroupsIds: state.tool.expandedMapSidebarGroupsIds,
+  selectMapDimensions: state.tool.selectedMapDimensions,
+  toggleSidebarGroups: state.tool.expandedMapSidebarGroupsIds,
+  isCloroplethEnabled: isCloroplethEnabled(state)
+});
+
+const methodProps = [
+  {
+    name: 'loadMapDimensions',
+    compared: ['mapDimensionsGroups'],
+    returned: ['mapDimensionsGroups', 'expandedMapSidebarGroupsIds']
+  },
+  {
+    name: 'setVisibility',
+    compared: ['selectedColumnsIds'],
+    returned: ['isCloroplethEnabled']
+  }
+];
+
+const mapDispatchToProps = {
+  onMapDimensionsLoaded: () => loadTooltip(),
+  onToggleGroup: id => toggleMapSidebarGroup(id),
+  onDimensionClick: uid => toggleMapDimension(uid)
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(mapToVanilla(MapDimensions, methodProps, Object.keys(mapDispatchToProps)));
