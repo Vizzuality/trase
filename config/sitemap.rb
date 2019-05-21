5e7a10d4a0e1ebab2f2cbaa16917cf31417b5c4f
Alvaro Leal <pursuance@gmail.com>
2019-05-21 18:34:05 +0200

    ERD snapshot in docs and profiles detail pages WIP

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,10 +1,22 @@
-# Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://trase.earth"
+
+# TODO: Language query needed
 
 SitemapGenerator::Sitemap.create do
   add'/explore'
   add'/flows'
   add'/profiles'
+
+  place_ids = Api::V3::NodeType.where(name: ['MUNICIPALITY', 'LOGISTICS HUB'])).map {|z| z.id }
+  Api::V3::Node.where(node_type_id: place_ids, is_unknown: false).map(&:id).each do |nodeId|
+    add "/profile-place?nodeId=#{nodeId}&contextId=#{contextId}&year=#{year}"
+  end
+
+  actor_ids = Api::V3::NodeType.where(name: ['IMPORTER', 'EXPORTER'])).map {|z| z.id }
+    Api::V3::Node.where(node_type_id: actor_ids, is_unknown: false).map(&:id).each do |nodeId|
+    add "/profile-actor?nodeId=#{nodeId}&contextId=#{contextId}&year=#{year}"
+  end
+
   add'/data'
   add'/about'
   Content::Page.all.map(&:name).each do |page|
