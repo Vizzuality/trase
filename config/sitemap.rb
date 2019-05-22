9584b1777642bf24acf673e88135230726640923
Alvaro Leal <pursuance@gmail.com>
2019-05-22 18:05:47 +0200

    Add changefreq

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,25 @@
 SitemapGenerator::Sitemap.default_host = "http://trase.earth"
-
-# TODO: Language query needed
-
 SitemapGenerator::Sitemap.create do
-  add'/explore'
-  add'/flows'
-  add'/profiles'
+  add'/explore', :changefreq => 'monthly'
+  add'/flows', :changefreq => 'monthly'
+  add'/profiles', :changefreq => 'monthly'
 
   contexts = Api::V3::Context.all;
   contexts.each do |context|
     context.years.each do |year|
       Api::V3::Readonly::Node.where(profile: 'place').map(&:id).each do |nodeId|
-        add "/profile-place?nodeId=#{nodeId}&year=#{year}&contextId=#{context.id}"
+        add "/profile-place?nodeId=#{nodeId}&year=#{year}&contextId=#{context.id}", :changefreq => 'monthly'
       end
       Api::V3::Readonly::Node.where(profile: 'actor').map(&:id).each do |nodeId|
-        add "/profile-actor?nodeId=#{nodeId}&year=#{year}&contextId=#{context.id}"
+        add "/profile-actor?nodeId=#{nodeId}&year=#{year}&contextId=#{context.id}", :changefreq => 'monthly'
       end
     end
   end
 
-  add'/data'
-  add'/about'
+  add'/data', :changefreq => 'monthly'
+  add'/about', :changefreq => 'yearly'
+
   Content::Page.all.map(&:name).each do |page|
-    add "/about/#{page}"
+    add "/about/#{page}", :changefreq => 'yearly'
   end
 end
