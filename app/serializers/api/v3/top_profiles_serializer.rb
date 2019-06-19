fd3f9aca0e1a9fd67a07cdf4b659e2c6d2f87d15
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-06-19 17:50:35 +0100

    Add year & summary to top profiles endpoint

--- app/serializers/api/v3/top_profiles_serializer.rb
+++ app/serializers/api/v3/top_profiles_serializer.rb
@@ -4,9 +4,23 @@ module Api
       attributes :context_id, :node_id
       attribute :profile_type
       attribute :photo_url
+      attribute :year
+      attribute :summary
 
       def profile_type
         object.node.node_type.context_node_types.find_by(context_id: object.context_id).profile.name
+      end
+
+      def year
+        object.context.years.max
+      end
+
+      def summary
+        service =
+          "Api::V3::#{profile_type.pluralize.capitalize}::BasicAttributes".constantize
+        service.new(
+          object.context, object.node, year
+        ).call[:summary]
       end
 
       def photo_url
