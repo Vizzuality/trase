f209358716a38241c5ce554e74b710ce9f0a8617
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-06-20 10:53:22 +0100

    Changed top profile to yellow table and included it in data update process

--- app/models/api/v3/top_profile.rb
+++ app/models/api/v3/top_profile.rb
@@ -19,9 +19,16 @@
 
 module Api
   module V3
-    class TopProfile < BlueTable
+    class TopProfile < YellowTable
       belongs_to :context
       belongs_to :node
+
+      def self.blue_foreign_keys
+        [
+          {name: :context_id, table_class: Api::V3::Context},
+          {name: :node_id, table_class: Api::V3::Node}
+        ]
+      end
     end
   end
 end
