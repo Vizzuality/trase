9bc0360e4a65e0a4c28a1cd1bebe827bdd8b19cc
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-06-20 10:51:03 +0100

    Updated schema annotation

--- app/models/api/v3/top_profile.rb
+++ app/models/api/v3/top_profile.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: top_profiles
+#
+#  id         :bigint(8)        not null, primary key
+#  context_id :bigint(8)        not null
+#  node_id    :bigint(8)        not null
+#
+# Indexes
+#
+#  index_top_profiles_on_context_id  (context_id)
+#  index_top_profiles_on_node_id     (node_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (context_id => contexts.id)
+#  fk_rails_...  (node_id => nodes.id)
+#
+
 module Api
   module V3
     class TopProfile < BlueTable
