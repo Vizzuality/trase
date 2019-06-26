b7d436fef279c2734efe51bcd32be6b3a2b0b53d
sorodrigo <rodrigo.solis@vizzuality.com>
2019-06-27 01:31:17 +0200

    adds selectedMapDimensions urlPropHandler

--- frontend/scripts/react-components/tool-layers/tool-layers.serializers.js
+++ frontend/scripts/react-components/tool-layers/tool-layers.serializers.js
@@ -10,3 +10,38 @@ export const mapView = {
     return { latitude, longitude, zoom };
   }
 };
+
+export const selectedMapDimensions = {
+  stringify(prop, DONT_SERIALIZE) {
+    if (!prop) {
+      return DONT_SERIALIZE;
+    }
+    const dimensions = prop.filter(Boolean);
+
+    // no map dimensions is not necessarily the default state, we have to be able to share it too
+    if (dimensions.length === 0) {
+      return 'none';
+    }
+
+    return dimensions.reduce((acc, id) => {
+      if (!acc) {
+        return id;
+      }
+      return `${acc}-${id}`;
+    }, '');
+  },
+  parse(param) {
+    if (param === 'none') {
+      return [null, null];
+    }
+
+    const tuples = param.split('-');
+    return tuples.reduce(
+      (acc, id, index) => {
+        acc[index] = id;
+        return acc;
+      },
+      [null, null]
+    );
+  }
+};
