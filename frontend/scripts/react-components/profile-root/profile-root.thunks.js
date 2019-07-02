51f7e067e0266bfc66643975738dcc3a55255730
Alvaro Leal <pursuance@gmail.com>
2019-07-02 13:19:50 +0200

    Remove fetch clause

--- frontend/scripts/react-components/profile-root/profile-root.thunks.js
+++ frontend/scripts/react-components/profile-root/profile-root.thunks.js
@@ -1,9 +1,5 @@
 import { getTopProfiles } from 'react-components/profile-root/profile-root.actions';
 
-export const loadTopNodes = (dispatch, getState) => {
-  const state = getState();
-  if (state?.app?.profileRoot?.topProfiles) {
-    return;
-  }
+export const loadTopNodes = dispatch => {
   dispatch(getTopProfiles());
 };
