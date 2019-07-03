b4ca0b8dbb8f93b6f4d967d598a43e7995adbb2b
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-07-03 17:47:10 +0100

    For top profile; return last available year of data per node - use nodes materialized view

--- app/admin/api/v3/top_profile.rb
+++ app/admin/api/v3/top_profile.rb
@@ -27,7 +27,7 @@ ActiveAdmin.register Api::V3::TopProfile, as: 'Top Profile' do
     def derive_top_profile_details(top_profile)
       profile_type = top_profile.node.node_type.context_node_types.find_by(context_id: top_profile.context_id).profile.name
       top_profile.profile_type = profile_type
-      year = top_profile.context.years.max
+      year = Api::V3::Readonly::Node.find(top_profile.node.id).years.max
       top_profile.year = year
       service = "Api::V3::#{profile_type.pluralize.capitalize}::BasicAttributes".constantize
       top_profile.summary = service.new(
