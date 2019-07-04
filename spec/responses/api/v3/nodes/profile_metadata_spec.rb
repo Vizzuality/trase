77a0d3242f92ba6b766ee0b9b0d465d5a5d33409
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-07-04 18:18:07 +0100

    Fix profile metadata spec - refresh materialized view before running tests

--- spec/responses/api/v3/nodes/profile_metadata_spec.rb
+++ spec/responses/api/v3/nodes/profile_metadata_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe 'Profile metadata', type: :request do
   include_context 'api v3 brazil flows quants'
   include_context 'api v3 brazil municipality place profile'
 
+  before(:each) do
+    Api::V3::Readonly::Node.refresh
+  end
+
   describe 'GET /api/v3/contexts/:context_id/nodes/:node_id/profile_metadata' do
     it 'has the correct response structure for countries' do
       get "/api/v3/contexts/#{api_v3_context.id}/nodes/#{api_v3_municipality_node.id}/profile_metadata"
-
       expect(@response).to have_http_status(:ok)
       expect(@response).to match_response_schema('v3_profile_metadata')
     end
