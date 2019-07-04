bad9f6ccfcfb1c533bedd3a31a6d54c51bec8a22
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-07-04 12:34:30 +0100

    Improve specs coverage

--- spec/controllers/admin/top_profiles_controller_spec.rb
+++ spec/controllers/admin/top_profiles_controller_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Admin::TopProfilesController, type: :controller do
+  render_views
   let(:user) { FactoryBot.create(:user) }
   before { sign_in user }
   before :each do
     allow(controller).to receive(:derive_top_profile_details).and_return(true)
   end
+
   let(:context) { FactoryBot.create(:api_v3_context) }
+  let(:node) { FactoryBot.create(:api_v3_node) }
+  let(:top_profile) { FactoryBot.create(:api_v3_top_profile) }
 
   describe 'POST create' do
+    subject { Api::V3::TopProfile.new(context: context, node: node) }
+
+    let(:top_profile) {
+      FactoryBot.create(
+        :api_v3_top_profile, context: context, node: FactoryBot.create(:api_v3_node)
+      )
+    }
     let(:valid_attributes) {
       {context_id: context.id, node_id: FactoryBot.create(:api_v3_node).id}
     }
@@ -17,6 +28,21 @@ RSpec.describe Admin::TopProfilesController, type: :controller do
         context_id: context.id, api_v3_top_profile: valid_attributes
       }
       expect(response).to redirect_to(admin_context_top_profiles_path(context))
+    end
+
+    it 'renders index' do
+      get :index, params: { context_id: context.id, id: top_profile.id }
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders show' do
+      get :show, params: {context_id: context.id, id: top_profile.id}
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders index' do
+      get :new, params: { context_id: context.id }
+      expect(response).to render_template(:new)
     end
   end
 
