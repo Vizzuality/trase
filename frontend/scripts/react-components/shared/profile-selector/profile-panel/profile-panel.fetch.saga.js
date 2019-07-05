67aa62ce76d40b10765133c1b7cd58daf7ae0717
Alvaro Leal <pursuance@gmail.com>
2019-07-05 14:24:20 +0200

    Fix On tab get and change - WIP

--- frontend/scripts/react-components/shared/profile-selector/profile-panel/profile-panel.fetch.saga.js
+++ frontend/scripts/react-components/shared/profile-selector/profile-panel/profile-panel.fetch.saga.js
@@ -1,9 +1,11 @@
-import { put, call, cancelled, fork } from 'redux-saga/effects';
+import { put, call, cancelled, fork, select } from 'redux-saga/effects';
 import getPanelName from 'utils/getProfilePanelName';
 import {
   PROFILES__SET_PANEL_DATA,
   PROFILES__SET_PANEL_TABS,
-  getProfilesParams
+  PROFILES__SET_MORE_PANEL_DATA,
+  getProfilesParams,
+  setProfilesLoadingItems
 } from 'react-components/shared/profile-selector/profile-selector.actions';
 import {
   getURLFromParams,
@@ -13,15 +15,13 @@ import {
 import { PROFILE_STEPS } from 'constants';
 import { fetchWithCancel, setLoadingSpinner } from 'utils/saga-utils';
 
-export function* getProfilesData(profileSelector, subPanelName, options) {
+export function* getProfilesData(subPanelName) {
+  const profileSelector = yield select(state => state.profileSelector);
   if (!profileSelector.activeStep || profileSelector.activeStep === PROFILE_STEPS.types) return;
   const panelName = subPanelName || getPanelName(profileSelector.activeStep);
   const { page, activeTab } = profileSelector.panels[panelName];
   const tab = activeTab?.id;
-  const params = getProfilesParams(profileSelector, panelName, {
-    page,
-    ...options
-  });
+  const params = getProfilesParams(profileSelector, panelName, { page });
   const url = getURLFromParams(GET_DASHBOARD_OPTIONS_URL, params);
   const task = yield fork(setLoadingSpinner, 750, {
     type: PROFILES__SET_PANEL_DATA,
@@ -61,48 +61,49 @@ export function* getProfilesData(profileSelector, subPanelName, options) {
   }
 }
 
-// export function* getMoreProfilesData(dashboardElement, optionsType, activeTab, direction) {
-//   const { page } = dashboardElement[`${dashboardElement.activePanelId}Panel`];
-//   const params = getProfilesParams(dashboardElement, optionsType, {
-//     page
-//   });
-//   const task = yield fork(setLoadingSpinner, 350, setProfilesLoadingItems(true));
-//   const url = getURLFromParams(GET_DASHBOARD_OPTIONS_URL, params);
-//   const { source, fetchPromise } = fetchWithCancel(url);
-//   try {
-//     const { data } = yield call(fetchPromise);
-//     yield put({
-//       type: PROFILES__SET_MORE_PANEL_DATA,
-//       payload: {
-//         key: optionsType,
-//         tab: activeTab && activeTab.id,
-//         direction,
-//         data: data.data
-//       }
-//     });
-//     if (task.isRunning()) {
-//       task.cancel();
-//     } else {
-//       yield call(setLoadingSpinner, 750, setProfilesLoadingItems(false));
-//     }
-//   } catch (e) {
-//     console.error('Error', e);
-//     if (task.isRunning()) {
-//       task.cancel();
-//     } else {
-//       yield call(setLoadingSpinner, 750, setProfilesLoadingItems(false));
-//     }
-//   } finally {
-//     if (yield cancelled()) {
-//       if (NODE_ENV_DEV) console.error('Cancelled', url);
-//       if (source) {
-//         source.cancel();
-//       }
-//     }
-//   }
-// }
+export function* getMoreProfilesData(dashboardElement, optionsType, activeTab, direction) {
+  const { page } = dashboardElement[`${dashboardElement.activePanelId}Panel`];
+  const params = getProfilesParams(dashboardElement, optionsType, {
+    page
+  });
+  const task = yield fork(setLoadingSpinner, 350, setProfilesLoadingItems(true));
+  const url = getURLFromParams(GET_DASHBOARD_OPTIONS_URL, params);
+  const { source, fetchPromise } = fetchWithCancel(url);
+  try {
+    const { data } = yield call(fetchPromise);
+    yield put({
+      type: PROFILES__SET_MORE_PANEL_DATA,
+      payload: {
+        key: optionsType,
+        tab: activeTab && activeTab.id,
+        direction,
+        data: data.data
+      }
+    });
+    if (task.isRunning()) {
+      task.cancel();
+    } else {
+      yield call(setLoadingSpinner, 750, setProfilesLoadingItems(false));
+    }
+  } catch (e) {
+    console.error('Error', e);
+    if (task.isRunning()) {
+      task.cancel();
+    } else {
+      yield call(setLoadingSpinner, 750, setProfilesLoadingItems(false));
+    }
+  } finally {
+    if (yield cancelled()) {
+      if (NODE_ENV_DEV) console.error('Cancelled', url);
+      if (source) {
+        source.cancel();
+      }
+    }
+  }
+}
 
-export function* getProfilesTabs(profileSelector, optionsType) {
+export function* getProfilesTabs(optionsType) {
+  const profileSelector = yield select(state => state.profileSelector);
   const params = getProfilesParams(profileSelector, optionsType);
   const url = getURLFromParams(GET_DASHBOARD_OPTIONS_TABS_URL, params);
   const { source, fetchPromise } = fetchWithCancel(url);
