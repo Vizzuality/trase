ece397e3dc285b5584e17ba8ba13ddb156fc4046
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-07-08 12:58:00 +0100

    Moved Resize / Recolor Attributes under 'Sankey & Map' and removed them from Top Profiles

--- app/admin/api/v3/context.rb
+++ app/admin/api/v3/context.rb
@@ -1,7 +1,7 @@
 # This is not an editable resource, but useful to define in order to have
 # nested resource routes.
 ActiveAdmin.register Api::V3::Context, as: 'Context' do
-  menu parent: 'General', priority: 1
+  menu label: 'Resize / Recolor Attributes', parent: 'Sankey & Map', priority: 1
 
   config.filters = false
 
@@ -9,13 +9,12 @@ ActiveAdmin.register Api::V3::Context, as: 'Context' do
     :country,
     :commodity,
     :resize_by_attributes,
-    :recolor_by_attributes,
-    :top_profiles
+    :recolor_by_attributes
   ]
 
   actions :index, :show
 
-  index do
+  index title: 'Resize / Recolor Attributes' do
     column('Country') do |context|
       context&.country&.name
     end
@@ -33,7 +32,7 @@ ActiveAdmin.register Api::V3::Context, as: 'Context' do
     actions
   end
 
-  show do
+  show title: 'Resize / Recolor Attributes' do
     attributes_table do
       row('Country') { |property| property.country&.name }
       row('Commodity') { |property| property.commodity&.name }
@@ -56,8 +55,6 @@ ActiveAdmin.register Api::V3::Context, as: 'Top Profiles For Context' do
   includes [
     :country,
     :commodity,
-    :resize_by_attributes,
-    :recolor_by_attributes,
     :top_profiles
   ]
 
@@ -77,14 +74,6 @@ ActiveAdmin.register Api::V3::Context, as: 'Top Profiles For Context' do
     column('Commodity') do |context|
       context&.commodity&.name
     end
-    column 'Resize by' do |context|
-      link_to "#{context.resize_by_attributes.length} attributes",
-              admin_context_resize_by_attributes_path(context)
-    end
-    column 'Recolor by' do |context|
-      link_to "#{context.recolor_by_attributes.length} attributes",
-              admin_context_recolor_by_attributes_path(context)
-    end
     column 'Top profiles' do |context|
       link_to "#{context.top_profiles.length} top profiles",
               admin_context_top_profiles_path(context)
@@ -97,8 +86,6 @@ ActiveAdmin.register Api::V3::Context, as: 'Top Profiles For Context' do
       row('Country') { |property| property.country&.name }
       row('Commodity') { |property| property.commodity&.name }
       row :created_at
-      row('Resize by attributes') { |context| link_to('Resize by attributes', admin_context_resize_by_attributes_path(context)) }
-      row('Recolor by attributes') { |context| link_to('Recolor by attributes', admin_context_recolor_by_attributes_path(context)) }
       row('Top profiles') { |property| link_to('Top profiles', admin_context_top_profiles_path(context_id: property.id)) }
     end
   end
