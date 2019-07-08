a05379c045dd942dddc774bc0b3b0591927ea180
sorodrigo <rodrigo.solis@vizzuality.com>
2019-07-08 14:17:03 +0200

    extracts test env variables into a single file

--- frontend/scripts/react-components/shared/in-view.component.jsx
+++ frontend/scripts/react-components/shared/in-view.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { InView } from 'react-intersection-observer';
 
 function InViewFallback(props) {
-  if ('IntersectionObserver' in window && !NODE_ENV_TEST) {
+  if ('IntersectionObserver' in window && ENABLE_INTERSECTION_OBSERVER) {
     return <InView {...props} />;
   }
 
