ff1a228d46108c99365c4d67f0636ca08ac40219
Jesús González <jesus198927@gmail.com>
2019-07-15 17:34:22 +0200

    add test for profile_metadata_controller

---
+++ spec/controllers/api/v3/profile_metadata_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Api::V3::ProfileMetadataController, type: :controller do
+  include_context 'api v3 brazil flows'
+
+  describe 'GET index' do
+    before do
+      Api::V3::Readonly::Node.refresh(sync: true, skip_dependencies: true)
+    end
+
+    context 'when node does not support profile pages' do
+      let(:node) { api_v3_logistics_hub_node }
+
+      it 'is not found' do
+        get :index, params: {context_id: api_v3_context.id, id: node.id}
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context 'when node supports profile pages' do
+      let(:node) { api_v3_municipality_node }
+
+      it 'is successful' do
+        get :index, params: {context_id: api_v3_context.id, id: node.id}
+        expect(response).to be_successful
+      end
+    end
+  end
+end
