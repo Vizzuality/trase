6160d4b53e444cb8dad42b61f5bd84de9457cbd3
Alvaro Leal <pursuance@gmail.com>
2019-07-16 18:38:38 +0200

    Remove maps, Reorder title and fix styles

--- frontend/scripts/react-components/profiles/place-summary.component.jsx
+++ frontend/scripts/react-components/profiles/place-summary.component.jsx
@@ -6,236 +6,144 @@ import capitalize from 'lodash/capitalize';
 import HelpTooltip from 'react-components/shared/help-tooltip/help-tooltip.component';
 import TitleGroup from 'react-components/profiles/title-group';
 import Map from 'react-components/profiles/map.component';
+import Text from 'react-components/shared/text';
 import Button from 'react-components/shared/button';
 import formatValue from 'utils/formatValue';
 
-class PlaceSummary extends React.PureComponent {
-  render() {
-    const {
-      year,
-      tooltips,
-      onYearChange,
-      context,
-      openModal,
-      data: {
-        columnName,
-        countryName,
-        jurisdiction2,
-        jurisdiction2GeoId,
-        jurisdiction1,
-        jurisdiction1GeoId,
-        summary,
-        area,
-        commodityProduction,
-        commodityArea,
-        jurisdictionName,
-        jurisdictionGeoId
-      } = {},
-      profileMetadata: {
-        adm1Name,
-        adm1TopojsonPath,
-        adm1TopojsonRoot,
-        adm2Name,
-        adm2TopojsonPath,
-        adm2TopojsonRoot,
-        mainTopojsonPath,
-        mainTopojsonRoot
-      } = {}
-    } = this.props;
-    const { commodityName } = context;
-    const titles = [
-      { name: jurisdictionName, label: capitalize(columnName) },
-      { name: commodityName, label: 'Commodity' },
-      {
-        dropdown: true,
-        label: 'Year',
-        value: { label: `${year}`, value: year },
-        options: (context.years
-          ? context.years.map(_year => ({ label: `${_year}`, value: _year }))
-          : []
-        ).sort((a, b) => b.value - a.value),
-        onYearChange
-      }
-    ];
-    const commodityAreaValue = formatValue(commodityArea, 'area');
-    const areaValue = formatValue(area, 'area');
-    const commodityProductionValue = formatValue(commodityProduction, 'tons');
+function PlaceSummary(props) {
+  const {
+    year,
+    tooltips,
+    onYearChange,
+    context,
+    openModal,
+    data: {
+      countryName,
+      jurisdiction1GeoId,
+      summary,
+      area,
+      commodityProduction,
+      commodityArea,
+      jurisdictionName,
+      jurisdictionGeoId,
+      jurisdiction1: stateName,
+      jurisdiction2: biomeName
+    } = {},
+    profileMetadata: { mainTopojsonPath, mainTopojsonRoot } = {}
+  } = props;
 
-    return (
-      <React.Fragment>
-        <div className="c-overall-map hide-for-small" data-test="place-summary">
-          <div className="row">
-            <div className="small-4 columns map-item">
-              <div className="row">
-                <div className="small-6 columns">
-                  <div className="c-locator-map">
-                    <Map
-                      topoJSONPath="./vector_layers/WORLD.topo.json"
-                      topoJSONRoot="world"
-                      useRobinsonProjection
-                      getPolygonClassName={d =>
-                        d.properties.name.toUpperCase() === countryName ? '-isCurrent' : ''
-                      }
-                    />
-                  </div>
-                </div>
-                <div className="small-6 columns">
-                  <div className="c-info">
-                    <div className="legend">country</div>
-                    <div className="name -medium">
-                      {countryName ? capitalize(countryName) : '-'}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="small-4 columns map-item">
-              {adm1TopojsonPath && adm1TopojsonRoot && (
-                <div className="row">
-                  <div className="small-3 columns">
-                    <div className="c-locator-map">
-                      {countryName && (
-                        <Map
-                          topoJSONPath={`./vector_layers${adm1TopojsonPath.replace(
-                            '$stateGeoId$',
-                            jurisdiction1GeoId
-                          )}`}
-                          topoJSONRoot={adm1TopojsonRoot}
-                          getPolygonClassName={d =>
-                            d.properties.geoid === jurisdiction2GeoId ? '-isCurrent' : ''
-                          }
-                        />
-                      )}
-                    </div>
-                  </div>
-                  <div className="small-9 columns">
-                    <div className="c-info">
-                      <div className="legend">{adm1Name}</div>
-                      <div className="name -medium">
-                        {jurisdiction2 ? capitalize(jurisdiction2) : '-'}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-            <div className="small-4 columns map-item">
-              {adm2TopojsonPath && adm2TopojsonRoot && (
-                <div className="row">
-                  <div className="small-3 columns">
-                    <div className="c-locator-map">
-                      {countryName && (
-                        <Map
-                          topoJSONPath={`./vector_layers${adm2TopojsonPath.replace(
-                            '$stateGeoId$',
-                            jurisdiction1GeoId
-                          )}`}
-                          topoJSONRoot={adm2TopojsonRoot}
-                          getPolygonClassName={d =>
-                            d.properties.geoid === jurisdiction1GeoId ? '-isCurrent' : ''
-                          }
-                        />
-                      )}
-                    </div>
-                  </div>
-                  <div className="small-9 columns">
-                    <div className="c-info">
-                      <div className="legend">{adm2Name}</div>
-                      <div className="name -medium">
-                        {jurisdiction1 ? capitalize(jurisdiction1) : '-'}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )}
+  const { commodityName } = context;
+  const titles = [
+    { name: commodityName, label: 'Commodity' },
+    {
+      dropdown: true,
+      label: 'Year',
+      value: { label: `${year}`, value: year },
+      options: (context.years
+        ? context.years.map(_year => ({ label: `${_year}`, value: _year }))
+        : []
+      ).sort((a, b) => b.value - a.value),
+      onYearChange
+    },
+    { name: capitalize(countryName), label: 'Country' },
+    { name: capitalize(biomeName), label: 'Biome' },
+    { name: capitalize(stateName), label: 'State' }
+  ];
+  const commodityAreaValue = formatValue(commodityArea, 'area');
+  const areaValue = formatValue(area, 'area');
+  const commodityProductionValue = formatValue(commodityProduction, 'tons');
+
+  const renderMunicipalityMap = () => (
+    <div className="c-overall-info page-break-inside-avoid">
+      <div className="c-locator-map map-municipality-banner">
+        {countryName && (
+          <Map
+            topoJSONPath={`./vector_layers${mainTopojsonPath.replace(
+              '$stateGeoId$',
+              jurisdiction1GeoId
+            )}`}
+            topoJSONRoot={mainTopojsonRoot.replace('$stateGeoId$', jurisdiction1GeoId)}
+            getPolygonClassName={d =>
+              d.properties.geoid === jurisdictionGeoId ? '-isCurrent' : ''
+            }
+          />
+        )}
+      </div>
+    </div>
+  );
+
+  const renderStats = () =>
+    console.log(areaValue, commodityAreaValue, commodityProductionValue) ||
+    ((areaValue !== '-' || commodityAreaValue !== '-' || commodityProductionValue !== '-') && (
+      <div className="small-12">
+        {areaValue !== '-' && (
+          <div className="stat-item">
+            <Text variant="mono" color="grey-faded" transform="uppercase" className="legend">
+              Area
+            </Text>
+            <div className="value">{areaValue}</div>
+            <div className="unit">
+              km<sup>2</sup>
             </div>
           </div>
-        </div>
+        )}
+        {commodityAreaValue !== '-' && (
+          <div className="stat-item">
+            <Text variant="mono" color="grey-faded" transform="uppercase" className="legend">
+              {commodityName} land
+              <HelpTooltip text={get(tooltips, 'profileNode.soyLand')} position="bottom" />
+            </Text>
+            <div className="value">{commodityAreaValue}</div>
+            <div className="unit">ha</div>
+          </div>
+        )}
+        {commodityProductionValue !== '-' && (
+          <div className="stat-item">
+            <Text variant="mono" color="grey-faded" transform="uppercase" className="legend">
+              Soy production
+              <HelpTooltip
+                text={get(tooltips, 'profileNode.commodityProduction')}
+                position="bottom"
+              />
+            </Text>
+            <div className="value">{commodityProductionValue}</div>
+            <div className="unit">t</div>
+          </div>
+        )}
+      </div>
+    ));
 
-        <div className="c-overall-info page-break-inside-avoid">
-          <div className="row">
-            {mainTopojsonPath && mainTopojsonRoot && (
-              <div className="small-12 medium-4 large-3 columns">
-                <div className="c-locator-map map-municipality-banner">
-                  {countryName && (
-                    <Map
-                      topoJSONPath={`./vector_layers${mainTopojsonPath.replace(
-                        '$stateGeoId$',
-                        jurisdiction1GeoId
-                      )}`}
-                      topoJSONRoot={mainTopojsonRoot.replace('$stateGeoId$', jurisdiction1GeoId)}
-                      getPolygonClassName={d =>
-                        d.properties.geoid === jurisdictionGeoId ? '-isCurrent' : ''
-                      }
-                    />
-                  )}
-                </div>
-              </div>
-            )}
-            <div className="small-12 medium-8 large-9 columns">
-              <div className="row">
-                <div className="small-12 columns">
-                  <TitleGroup titles={titles} on={onYearChange} />
-                  <Button
-                    size="sm"
-                    color="pink-transparent"
-                    className="profiles-selector-button"
-                    onClick={openModal}
-                  >
-                    Change profile
-                  </Button>
-                </div>
-                {(areaValue !== '-' ||
-                  commodityAreaValue !== '-' ||
-                  commodityProductionValue !== '-') && (
-                  <div className="small-12 columns">
-                    {areaValue !== '-' && (
-                      <div className="stat-item">
-                        <div className="legend">area</div>
-                        <div className="value">{areaValue}</div>
-                        <div className="unit">
-                          km<sup>2</sup>
-                        </div>
-                      </div>
-                    )}
-                    {commodityAreaValue !== '-' && (
-                      <div className="stat-item">
-                        <div className="legend">
-                          {commodityName} land
-                          <HelpTooltip
-                            text={get(tooltips, 'profileNode.soyLand')}
-                            position="bottom"
-                          />
-                        </div>
-                        <div className="value">{commodityAreaValue}</div>
-                        <div className="unit">ha</div>
-                      </div>
-                    )}
-                    {commodityProductionValue !== '-' && (
-                      <div className="stat-item">
-                        <div className="legend">
-                          soy production
-                          <HelpTooltip
-                            text={get(tooltips, 'profileNode.commodityProduction')}
-                            position="bottom"
-                          />
-                        </div>
-                        <div className="value">{commodityProductionValue}</div>
-                        <div className="unit">t</div>
-                      </div>
-                    )}
-                  </div>
-                )}
-                <div className="small-12 columns">
-                  <p className="summary" dangerouslySetInnerHTML={{ __html: summary }} />
-                </div>
-              </div>
+  return (
+    <React.Fragment>
+      <div className="c-overall-info page-break-inside-avoid">
+        <div className="row">
+          <div className="small-12 medium-9 columns">
+            <div className="profiles-title-container">
+              <h2 className="profiles-title" data-test="profiles-title">
+                {capitalize(jurisdictionName)}
+              </h2>
+              <Button
+                size="sm"
+                color="pink-transparent"
+                className="profiles-selector-button"
+                onClick={openModal}
+              >
+                Change profile
+              </Button>
             </div>
+            <TitleGroup titles={titles} on={onYearChange} />
+            {renderStats()}
+          </div>
+          <div className="small-12 medium-3 columns">{renderMunicipalityMap()}</div>
+        </div>
+        <div className="row">
+          <div className="small-12 columns">
+            <p className="summary" dangerouslySetInnerHTML={{ __html: summary }} />
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
+      </div>
+    </React.Fragment>
+  );
 }
 
 PlaceSummary.propTypes = {
