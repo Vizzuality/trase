5e423824da0e91aa0d9f9cf7f0c118beb865b31e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-08-01 15:56:40 +0200

    Aggregating flows for crosstab

---
+++ db/migrate/20190801121907_aggregate_flows_for_download.rb
@@ -0,0 +1,13 @@
+class AggregateFlowsForDownload < ActiveRecord::Migration[5.2]
+  def change
+    update_view :download_flows_v,
+      version: 2,
+      revert_to_version: 1,
+      materialized: false
+
+    remove_column :download_flows, :id
+
+    # this takes a long time
+    Api::V3::Readonly::DownloadFlow.refresh_now
+  end
+end
