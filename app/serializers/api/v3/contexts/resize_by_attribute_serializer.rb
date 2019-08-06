9ffe8b3798501d7d7abac754ba4f7d318c69784b
Jesús González <jesus198927@gmail.com>
2019-08-06 18:02:57 +0200

    Add unit to the resize_by_attributes readonly model and the serializer

--- app/serializers/api/v3/contexts/resize_by_attribute_serializer.rb
+++ app/serializers/api/v3/contexts/resize_by_attribute_serializer.rb
@@ -3,7 +3,7 @@ module Api
     module Contexts
       class ResizeByAttributeSerializer < ActiveModel::Serializer
         attributes :is_default, :is_disabled, :group_number, :position, :name,
-                   :years, :attribute_id
+                   :unit, :years, :attribute_id
         attribute :display_name, key: :label
         attribute :tooltip_text, key: :description
       end
