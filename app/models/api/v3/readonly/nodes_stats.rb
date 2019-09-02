7b4761ee919641a71b2f3bb3c4333c262e3e79fb
Jesús González <jesus198927@gmail.com>
2019-09-02 16:33:22 +0200

    Add year to the nodes_stats index to ensure that it is unique

--- app/models/api/v3/readonly/nodes_stats.rb
+++ app/models/api/v3/readonly/nodes_stats.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: nodes_stats_mv
+#
+#  context_id   :integer
+#  year         :integer
+#  quant_id     :integer
+#  node_type_id :integer
+#  node_id      :integer
+#  name         :text
+#  geo_id       :text
+#  value        :float
+#  height       :float
+#
+# Indexes
+#
+#  nodes_stats_mv_context_year_quant_node_node_type_idx  (context_id,year,quant_id,node_id,node_type_id) UNIQUE
+#
+
 module Api
   module V3
     module Readonly
--- db/migrate/20190823135415_add_nodes_stats_mv.rb
+++ db/migrate/20190823135415_add_nodes_stats_mv.rb
@@ -4,9 +4,9 @@ class AddNodesStatsMv < ActiveRecord::Migration[5.2]
       materialized: true,
       version: 1
 
-      add_index :nodes_stats_mv,
-      [:context_id, :quant_id, :node_id, :node_type_id],
+    add_index :nodes_stats_mv,
+      [:context_id, :year, :quant_id, :node_id, :node_type_id],
       unique: true,
-      name: 'nodes_stats_mv_context_id_quant_id_node_id_node_type_id_idx'
+      name: 'nodes_stats_mv_context_year_quant_node_node_type_idx'
   end
 end
