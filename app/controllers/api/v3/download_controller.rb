8e1bfd20ddd88cfb2c58e41d40e9dbd9316aeb32
Jesús González <jesus198927@gmail.com>
2019-09-09 16:43:48 +0200

    Remove headers used for caching download sources

--- app/controllers/api/v3/download_controller.rb
+++ app/controllers/api/v3/download_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V3
     class DownloadController < ApiController
+      before_action :set_no_cache_headers
+
       def index
         download = Api::V3::Download::FlowDownload.new(@context, params)
 
@@ -18,6 +20,19 @@ module Api
                       disposition: 'attachment'
           end
         end
+      end
+
+      private
+
+      def set_no_cache_headers
+        response.delete_header('ETag')
+
+        response.set_header('Pragma', 'no-cache')
+        response.set_header('Expires', DateTime.yesterday)
+        response.set_header(
+          'Cache-Control',
+          'max-age=0, no-store, no-cache, must-revalidate'
+        )
       end
     end
   end
