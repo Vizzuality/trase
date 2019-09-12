9ab8ac5061a7f304a2c65a0083d20ba687b71cea
Alvaro Leal <pursuance@gmail.com>
2019-09-12 15:18:45 +0200

    Add real data to bubble

--- frontend/scripts/react-components/explore/explore.component.jsx
+++ frontend/scripts/react-components/explore/explore.component.jsx
@@ -11,6 +11,7 @@ import { EXPLORE_STEPS } from 'constants';
 import getTopNodesKey from 'utils/getTopNodesKey';
 import cx from 'classnames';
 import Responsive from 'react-components/shared/responsive.hoc';
+import { format } from 'd3-format';
 
 import 'react-components/explore/explore.scss';
 
@@ -31,7 +32,7 @@ function Explore({
   getTopCountries,
   getQuickFacts,
   commodityContexts,
-  quickFactsIndicators
+  countryQuickFacts
 }) {
   const [highlightedContext, setHighlightedContext] = useState(null);
   const [highlightedCountryIds, setHighlightedCountries] = useState(null);
@@ -139,6 +140,10 @@ function Explore({
   ]);
   const ITEMS_PER_ROW = 7;
   const rowsNumber = items.length && Math.ceil(items.length / ITEMS_PER_ROW);
+  const quickFacts =
+    countryQuickFacts &&
+    (country?.id || highlightedContext?.countryId) &&
+    countryQuickFacts[country?.id || highlightedContext?.countryId];
   return (
     <div className="c-explore">
       <div className="explore-selector">
@@ -177,20 +182,24 @@ function Explore({
               </div>
             </div>
             <div className="small-4 medium-2 columns hide-for-small">
-              <div className="quick-facts">
-                <div className="bubble-container">
-                  {quickFactsIndicators.map(indicator => (
-                    <div className="bubble">
-                      <Text size="rg" align="center" variant="mono">
-                        {indicator.name}
-                      </Text>
-                      <Text size="lg" weight="regular" align="center" className="quick-facts-value">
-                        {indicator.value} {indicator.unit}
-                      </Text>
-                    </div>
-                  ))}
+              {step > EXPLORE_STEPS.selectCommodity && (
+                <div className="quick-facts">
+                  {quickFacts ? (
+                    quickFacts.map(indicator => (
+                      <div className="bubble">
+                        <Text size="rg" align="center" variant="mono">
+                          {indicator.name} {indicator.year}
+                        </Text>
+                        <Text size="lg" weight="regular" align="center" className="quick-facts-value">
+                          {format(',')(Math.round(indicator.total))} {indicator.unit}
+                        </Text>
+                      </div>
+                    ))
+                  ) : (
+                    <div className="bubble" />
+                  )}
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </div>
@@ -225,7 +234,7 @@ Explore.propTypes = {
   commodityContexts: PropTypes.array,
   getTopCountries: PropTypes.func.isRequired,
   getQuickFacts: PropTypes.func.isRequired,
-  quickFactsIndicators: PropTypes.object
+  countryQuickFacts: PropTypes.object
 };
 
 export default Explore;
