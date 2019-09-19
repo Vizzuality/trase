4806fbc6c66fbffb940c38f8348211b3d3f1b4c7
sorodrigo <rodrigo.solis@vizzuality.com>
2019-09-19 15:13:17 +0200

    fix plural nodetype name logistics hub

--- frontend/scripts/utils/pluralize.js
+++ frontend/scripts/utils/pluralize.js
@@ -2,11 +2,18 @@ import camelCase from 'lodash/camelCase';
 
 export default name => {
   const camelCasedName = camelCase(name);
-  return {
+  const plural = {
+    logisticsHub: 'logistics hubs',
     portOfImport: 'ports of import',
     portOfExport: 'ports of export',
     districtOfExport: 'districts of export'
-  }[camelCasedName] || camelCasedName.endsWith('y')
-    ? camelCasedName.replace(/y$/, 'ies').toLowerCase()
-    : `${camelCasedName}s`.toLowerCase();
+  }[camelCasedName];
+
+  if (!plural) {
+    return camelCasedName.endsWith('y')
+      ? camelCasedName.replace(/y$/, 'ies').toLowerCase()
+      : `${camelCasedName}s`.toLowerCase();
+  }
+
+  return plural;
 };
