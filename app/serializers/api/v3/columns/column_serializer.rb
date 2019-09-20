5ee1c201e6d94531a04e91293e7b1372657829c3
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-09-20 14:36:40 +0200

    Added role to columns endpoint

--- app/serializers/api/v3/columns/column_serializer.rb
+++ app/serializers/api/v3/columns/column_serializer.rb
@@ -2,7 +2,7 @@ module Api
   module V3
     module Columns
       class ColumnSerializer < ActiveModel::Serializer
-        attributes :id, :name, :position, :group, :is_default, :is_geo,
+        attributes :id, :name, :position, :group, :role, :is_default, :is_geo,
                    :is_choropleth_disabled, :profile_type
       end
     end
