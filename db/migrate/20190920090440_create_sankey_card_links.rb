7387770cfdcb31b7d3572b19ef85b3d10e497eae
Jesús González <jesus198927@gmail.com>
2019-09-20 13:54:11 +0200

    Remove link and add host and query_params to sankey_card_links

--- db/migrate/20190920090440_create_sankey_card_links.rb
+++ db/migrate/20190920090440_create_sankey_card_links.rb
@@ -1,7 +1,8 @@
 class CreateSankeyCardLinks < ActiveRecord::Migration[5.2]
   def change
     create_table :sankey_card_links do |t|
-      t.json :link, null: false
+      t.text :host, null: false
+      t.json :query_params, null: false
       t.text :title, null: false
       t.text :subtitle
 
--- spec/factories/api/v3/api_v3_sankey_card_link.rb
+++ spec/factories/api/v3/api_v3_sankey_card_link.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :api_v3_sankey_card_link, class: 'Api::V3::SankeyCardLink' do
     sequence(:title) { |n| "Title#{n}" }
-    sequence(:link) { |n| {property: n} }
+    sequence(:host) { |n| "host#{n}.com" }
+    sequence(:query_params) { |n| {property: n} }
   end
 end
