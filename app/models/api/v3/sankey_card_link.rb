03ffb8034fb3ffa6e622d5c4ad1578568dfaff1d
Jesús González <jesus198927@gmail.com>
2019-09-20 13:54:11 +0200

    Remove link and add host and query_params to sankey_card_links

--- app/models/api/v3/sankey_card_link.rb
+++ app/models/api/v3/sankey_card_link.rb
@@ -1,8 +1,37 @@
+# == Schema Information
+#
+# Table name: sankey_card_links
+#
+#  id         :bigint(8)        not null, primary key
+#  link       :json             not null
+#  title      :text             not null
+#  subtitle   :text
+#
+
 module Api
   module V3
     class SankeyCardLink < YellowTable
-      validates :link, presence: true
+      attr_accessor :link_param
+
+      validates :host, presence: true
+      validates :query_params, presence: true
       validates :title, presence: true
+
+      before_save :extract_link_params
+
+      def link
+        "#{self.host}?#{self.query_params.to_query}"
+      end
+
+      private
+
+      def extract_link_params
+        uri = URI.parse link_param
+        self.host = uri.host
+
+        ary = URI.decode_www_form(uri.query).to_h
+        self.query_params = ary
+      end
     end
   end
 end
--- spec/models/api/v3/sankey_card_link_spec.rb
+++ spec/models/api/v3/sankey_card_link_spec.rb
@@ -1,20 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Api::V3::SankeyCardLink, type: :model do
+  let(:sankey_card_link) {
+    FactoryBot.build(:api_v3_sankey_card_link)
+  }
+
   describe :validate do
-    let(:sankey_card_link_without_link) {
-      FactoryBot.build(:api_v3_sankey_card_link, link: nil)
+    let(:sankey_card_link_without_host) {
+      FactoryBot.build(:api_v3_sankey_card_link, host: nil)
+    }
+    let(:sankey_card_link_without_query_params) {
+      FactoryBot.build(:api_v3_sankey_card_link, query_params: nil)
     }
     let(:sankey_card_link_without_title) {
       FactoryBot.build(:api_v3_sankey_card_link, title: nil)
     }
 
-    it 'fails when link blank' do
-      expect(sankey_card_link_without_link).to have(1).errors_on(:link)
+    it 'fails when host blank' do
+      expect(sankey_card_link_without_host).to have(1).errors_on(:host)
+    end
+
+    it 'fails when query_params blank' do
+      expect(sankey_card_link_without_query_params).to have(1).errors_on(:query_params)
     end
 
     it 'fails when title blank' do
       expect(sankey_card_link_without_title).to have(1).errors_on(:title)
+    end
+  end
+
+  describe :methods do
+    describe '#link' do
+      it 'return complete link' do
+        expect(sankey_card_link.link).to eql(
+          "#{sankey_card_link.host}?#{sankey_card_link.query_params.to_query}"
+        )
+      end
+    end
+  end
+
+  describe :callbacks do
+    describe 'before_save' do
+      describe '#extract_link_params' do
+        it 'extract parameters from link' do
+          sankey_card_link.update_attributes(title: 'test', link_param: 'http://test.com?one=1')
+          expect(sankey_card_link.host).to eql 'test.com'
+          expect(sankey_card_link.query_params).to eql({'one' => '1'})
+        end
+      end
     end
   end
 end
