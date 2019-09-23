7a99712fb3c75201d2cce09ea8d8c7cddb58cd04
Alvaro Leal <pursuance@gmail.com>
2019-09-23 11:23:17 +0200

    Vertical position of arrows and animate

--- frontend/scripts/react-components/tool/layout-arrows/layout-arrows.component.jsx
+++ frontend/scripts/react-components/tool/layout-arrows/layout-arrows.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import 'react-components/tool/layout-arrows/layout-arrows.scss';
 import { TOOL_LAYOUT } from 'constants';
@@ -7,15 +8,24 @@ import { TOOL_LAYOUT } from 'constants';
 export default function LayoutArrows({ toolLayout, changeLayout }) {
   return (
     <>
-      {toolLayout > TOOL_LAYOUT.left && (
-        <button className="layout-arrow-button" onClick={() => changeLayout(toolLayout - 1)} />
-      )}
-      {toolLayout < TOOL_LAYOUT.right && (
-        <button
-          className="layout-arrow-button -right"
-          onClick={() => changeLayout(toolLayout + 1)}
-        />
-      )}
+      <button
+        className={cx(
+          'layout-arrow-button',
+          { '-centered-arrow': toolLayout !== TOOL_LAYOUT.splitted },
+          { '-disabled': toolLayout === TOOL_LAYOUT.left }
+        )}
+        disabled={toolLayout === TOOL_LAYOUT.left}
+        onClick={() => changeLayout(toolLayout - 1)}
+      />
+      <button
+        className={cx(
+          'layout-arrow-button -right',
+          { '-centered-arrow': toolLayout !== TOOL_LAYOUT.splitted },
+          { '-disabled': toolLayout === TOOL_LAYOUT.right }
+        )}
+        disabled={toolLayout === TOOL_LAYOUT.right}
+        onClick={() => changeLayout(toolLayout + 1)}
+      />
     </>
   );
 }
