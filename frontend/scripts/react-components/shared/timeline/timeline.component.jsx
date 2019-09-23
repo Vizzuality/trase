709d15c9ef55bba91aeb76fc69b15c2178cd22b7
sorodrigo <rodrigo.solis@vizzuality.com>
2019-09-23 17:27:12 +0200

    create timeline component

---
+++ frontend/scripts/react-components/shared/timeline/timeline.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+function Timeline(props) {
+  const {} = props;
+  return <div className="c-timeline" />;
+}
+
+Timeline.propTypes = {};
+
+export default Timeline;
