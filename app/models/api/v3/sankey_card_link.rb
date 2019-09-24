d8ca5e8841d6a2304e16b7c3d1619dc45e1823fa
Jesús González <jesus198927@gmail.com>
2019-09-24 17:18:38 +0200

    Add logic for creation/edition of sankey card links

--- app/models/api/v3/sankey_card_link.rb
+++ app/models/api/v3/sankey_card_link.rb
@@ -2,11 +2,29 @@
 #
 # Table name: sankey_card_links
 #
-#  id           :bigint(8)        not null, primary key
-#  host         :text             not null
-#  query_params :json             not null
-#  title        :text             not null
-#  subtitle     :text
+#  id                 :bigint(8)        not null, primary key
+#  host               :text             not null
+#  query_params       :json             not null
+#  title              :text             not null
+#  subtitle           :text
+#  commodity_id       :bigint(8)
+#  country_id         :bigint(8)
+#  cont_attribute_id  :bigint(8)
+#  ncont_attribute_id :bigint(8)
+#
+# Indexes
+#
+#  index_sankey_card_links_on_commodity_id        (commodity_id)
+#  index_sankey_card_links_on_cont_attribute_id   (cont_attribute_id)
+#  index_sankey_card_links_on_country_id          (country_id)
+#  index_sankey_card_links_on_ncont_attribute_id  (ncont_attribute_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (commodity_id => commodities.id)
+#  fk_rails_...  (cont_attribute_id => attributes.id)
+#  fk_rails_...  (country_id => countries.id)
+#  fk_rails_...  (ncont_attribute_id => attributes.id)
 #
 
 module Api
@@ -14,7 +32,7 @@ module Api
     class SankeyCardLink < YellowTable
       attr_accessor :link_param
 
-      VALID_QUERY_PARAMS =%w[
+      VALID_QUERY_PARAMS = %w[
         commodity_id
         country_id
         nodes_ids
@@ -24,18 +42,42 @@ module Api
         end_year
       ]
 
+      REQUIRED_QUERY_PARAMS = %w[
+        commodity_id
+        country_id
+        cont_attribute_id
+        ncont_attribute_id
+      ]
+
       scope :link_contains, ->(link) {
         where("host LIKE '%#{link}%' OR
                query_params->>'#{link}' IS NOT NULL OR
-               query_params->>'commodity_id' LIKE '%#{link}%' OR
-               query_params->>'country_id' LIKE '%#{link}%' OR
+               commodity_id = '%#{link}%' OR
+               country_id = '%#{link}%' OR
+               cont_attribute_id = '%#{link}%' OR
+               ncont_attribute_id = '%#{link}%' OR
                query_params->>'nodes_ids' LIKE '%#{link}%' OR
-               query_params->>'cont_attribute_id' LIKE '%#{link}%' OR
-               query_params->>'ncont_attribute_id' LIKE '%#{link}%' OR
                query_params->>'start_year' LIKE '%#{link}%' OR
                query_params->>'end_year' LIKE '%#{link}%'"
         )
       }
+
+      belongs_to :commodity
+      belongs_to :country
+      belongs_to :cont_attribute,
+        class_name: 'Api::V3::Readonly::Attribute',
+        foreign_key: 'cont_attribute_id',
+        inverse_of: :cont_attribute_sankey_card_links,
+        dependent: :destroy
+      belongs_to :ncont_attribute,
+        class_name: 'Api::V3::Readonly::Attribute',
+        foreign_key: 'ncont_attribute_id',
+        inverse_of: :ncont_attribute_sankey_card_links,
+        dependent: :destroy
+      has_many :sankey_card_link_nodes
+      has_many :nodes,
+        class_name: 'Api::V3::SankeyCardLinkNode',
+        through: :sankey_card_link_nodes
 
       validates :host, presence: true
       validates :query_params, presence: true
@@ -44,6 +86,8 @@ module Api
       validate :check_valid_query_params
 
       before_validation :extract_link_params
+      before_validation :extract_required_params
+      after_commit :add_nodes_relations
 
       def link
         return '' unless self.host && self.query_params
@@ -55,22 +99,64 @@ module Api
         %i(link_contains)
       end
 
+      def self.blue_foreign_keys
+        [
+          {name: :commodity_id, table_class: Api::V3::Commodity},
+          {name: :country_id, table_class: Api::V3::Country},
+          {name: :cont_attribute_id, table_class: Api::V3::Readonly::Attribute},
+          {name: :ncont_attribute_id, table_class: Api::V3::Readonly::Attribute}
+        ]
+      end
+
       private
 
       def extract_link_params
         return unless link_param
-
         uri = URI.parse link_param
         self.host = uri.host
 
+        return unless uri.query
         ary = URI.decode_www_form(uri.query).to_h
         self.query_params = ary
       end
 
-      def check_valid_query_params
-        return unless ((query_params || {}).keys - VALID_QUERY_PARAMS).any?
+      def extract_required_params
+        return unless self.query_params
 
-        errors.add(:query_params, 'includes invalid parameters')
+        REQUIRED_QUERY_PARAMS.each do |required_parameter|
+          self.send("#{required_parameter}=", self.query_params[required_parameter])
+        end
+      end
+
+      def add_nodes_relations
+        nodes_ids = self.query_params['nodes_ids'].split(',')
+        nodes_ids.each do |node_id|
+          Api::V3::SankeyCardLinkNode.find_or_create_by!(
+            node_id: node_id,
+            sankey_card_link_id: self.id
+          )
+        end
+
+        # Remove old sankey card link nodes relations
+        Api::V3::SankeyCardLinkNode.
+          where(sankey_card_link_id: self.id).
+          where.not(node_id: nodes_ids).
+          destroy_all
+      end
+
+      def check_valid_query_params
+        # Check if we are indicating only permitted params
+        if ((query_params || {}).keys - VALID_QUERY_PARAMS).any?
+          errors.add(:link_param, 'includes invalid parameters')
+        end
+
+        # Check if we are including all obligatory params
+        if (REQUIRED_QUERY_PARAMS - (query_params || {}).keys).any?
+          errors.add(
+            :link_param,
+            'must specify commodity, country, cont_attribute and ncont_attribute'
+          )
+        end
       end
     end
   end
---
+++ app/models/api/v3/sankey_card_link_node.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: sankey_card_link_nodes
+#
+#  id                  :bigint(8)        not null, primary key
+#  sankey_card_link_id :bigint(8)
+#  node_id             :bigint(8)
+#
+# Indexes
+#
+#  index_sankey_card_link_nodes_on_node_id              (node_id)
+#  index_sankey_card_link_nodes_on_sankey_card_link_id  (sankey_card_link_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (node_id => nodes.id)
+#  fk_rails_...  (sankey_card_link_id => sankey_card_links.id)
+#
+
+module Api
+  module V3
+    class SankeyCardLinkNode < YellowTable
+      belongs_to :sankey_card_link,
+        class_name: 'Api::V3::SankeyCardLink',
+        inverse_of: :sankey_card_link_nodes
+      belongs_to :node,
+        class_name: 'Api::V3::Node',
+        inverse_of: :sankey_card_link_nodes
+
+      validates :sankey_card_link_id, uniqueness: {scope: :node_id}
+    end
+  end
+end
