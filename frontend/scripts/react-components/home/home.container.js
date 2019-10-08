43e07cc95d3dbfa22b0410e05e2c4dd6e567bfa3
Alvaro Leal <pursuance@gmail.com>
2019-10-08 17:16:24 +0200

    Remove entrypoints and merge posts into insights

--- frontend/scripts/react-components/home/home.container.js
+++ frontend/scripts/react-components/home/home.container.js
@@ -2,35 +2,25 @@ import { connect } from 'react-redux';
 import Home from 'react-components/home/home.component';
 import { BREAKPOINTS, HOME_VIDEO } from 'constants';
 import { setContextIsUserSelected, getTopCountries } from 'scripts/actions/app.actions';
-import {
-  playHomeVideo,
-  clickNextEntrypoint,
-  clickEntrypoint
-} from 'scripts/react-components/home/home.actions';
+import { playHomeVideo } from 'scripts/react-components/home/home.actions';
 import { getSelectedContext, getSelectedYears } from 'reducers/app.selectors';
 import { getDestinationCountries } from 'react-components/home/home.selectors';
+import sortBy from 'lodash/sortBy';
 
 function mapStateToProps(state) {
   const { query = {} } = state.location;
   const { tweets, posts, testimonials } = state.home;
   // not ideal, we could use redux-responsive if we need more changes like the one below
   const isSmallResolution = window.innerWidth <= BREAKPOINTS.small;
-  const INSIGHTS = ['INSIGHT', 'INFO BRIEF', 'ISSUE BRIEF', 'LONGER READ'];
   const promotedPost = posts.find(post => post.highlighted);
-  const blogPosts = posts.filter(
-    post => !(post.highlighted && !isSmallResolution) && !INSIGHTS.includes(post.category)
-  );
-  const insightsPosts = posts.filter(
-    post => !(post.highlighted && !isSmallResolution) && INSIGHTS.includes(post.category)
-  );
+  const insightsPosts = posts
+    .filter(post => !(post.highlighted && !isSmallResolution))
+    .concat(testimonials);
   const homeVideo = HOME_VIDEO[query.lang] || HOME_VIDEO.en;
-
   return {
     homeVideo,
-    blogPosts,
-    insightsPosts,
+    insightsPosts: sortBy(insightsPosts, 'date').reverse(),
     promotedPost,
-    testimonials,
     tweets: tweets.length > 0 ? tweets : null,
     selectedContext: getSelectedContext(state),
     destinationCountries: getDestinationCountries(state),
@@ -44,8 +34,6 @@ const mapDispatchToProps = dispatch => ({
     dispatch({ type: 'explore' });
   },
   onPlayVideo: videoId => dispatch(playHomeVideo(videoId)),
-  clickEntrypoint: link => dispatch(clickEntrypoint(link)),
-  clickNextEntrypoint: () => dispatch(clickNextEntrypoint()),
   getTopCountries: () => dispatch(getTopCountries())
 });
 export default connect(
