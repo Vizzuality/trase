b2091d2daf279429d67baf313ba8b99b2867506c
Alvaro Leal <pursuance@gmail.com>
2019-10-10 13:22:17 +0200

    Remove remaining legacy code

--- frontend/scripts/react-components/profile-root/profile-root.actions.js
+++ frontend/scripts/react-components/profile-root/profile-root.actions.js
@@ -8,18 +8,6 @@ export const SET_PROFILE_ROOT_ERROR_MESSAGE = 'SET_PROFILE_ROOT_ERROR_MESSAGE';
 export const GET_TOP_PROFILES = 'GET_TOP_PROFILES';
 export const SET_TOP_PROFILES = 'SET_TOP_PROFILES';
 
-export const goToNodeProfilePageLegacy = (node, { year }) => ({
-  type: 'profileNode',
-  payload: {
-    query: {
-      nodeId: node.id,
-      contextId: node.contextId,
-      year
-    },
-    profileType: node.profile
-  }
-});
-
 export const goToNodeProfilePage = node => ({
   type: 'profileNode',
   payload: {
@@ -37,42 +25,6 @@ export function resetProfileSearchResults() {
     payload: { term: '', results: [] }
   };
 }
-
-export const searchNodeWithTermLegacy = (searchTerm, { contextId }) => dispatch => {
-  const nodeResultsURL = getURLFromParams(GET_NODES_WITH_SEARCH_URL, {
-    query: searchTerm,
-    context_id: contextId,
-    profile_only: true
-  });
-
-  if (isEmpty(searchTerm)) {
-    dispatch(resetProfileSearchResults());
-    return;
-  }
-
-  dispatch({
-    type: SET_PROFILE_SEARCH_TERM,
-    payload: { term: searchTerm, isLoading: true }
-  });
-
-  axios(nodeResultsURL)
-    .then(res => {
-      const results = res.data;
-      if (!results) return;
-
-      dispatch({
-        type: LOAD_PROFILE_SEARCH_RESULTS,
-        payload: results.data
-      });
-    })
-    .catch(reason => {
-      console.error('Error loading profile search nodes', reason);
-      dispatch({
-        type: SET_PROFILE_ROOT_ERROR_MESSAGE,
-        payload: { errorMessage: reason.message }
-      });
-    });
-};
 
 export const searchNodeWithTerm = searchTerm => dispatch => {
   const nodeResultsURL = getURLFromParams(GET_NODES_WITH_SEARCH_URL, {
--- frontend/scripts/react-components/profile-root/profile-root.component.jsx
+++ frontend/scripts/react-components/profile-root/profile-root.component.jsx
@@ -1,80 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ProfileSearchLegacy from 'react-components/profile-root/profile-search-legacy.container';
 import ProfileSearch from 'react-components/profile-root/profile-search.container';
 import ProfileSelector from 'react-components/shared/profile-selector';
-import ContextSelector from 'react-components/shared/context-selector/context-selector.container';
-import ErrorMessage from 'react-components/profile-root/error-message/error-message.component';
-import cx from 'classnames';
-import Img from 'react-components/shared/img';
 import Button from 'react-components/shared/button';
 import SliderSection from 'react-components/home/slider-section/slider-section.component';
 
 import 'scripts/react-components/profile-root/profile-root.scss';
-
-const renderLegacyProfiles = props => {
-  const { errorMessage, activeContext, selectContexts } = props;
-  return (
-    <div className="l-profile-root-legacy">
-      {!errorMessage && (
-        <div className="c-profile-root-legacy">
-          <div className="row column">
-            <div className="profile-root-search-container row align-center">
-              <div className="column small-12 medium-9 large-6">
-                <div className="profile-root-heading-container">
-                  <div className="profile-root-heading-wrapper">
-                    <ContextSelector
-                      selectContexts={selectContexts}
-                      selectedContext={activeContext}
-                      className={cx('profile-root-context-selector', {
-                        '-readonly': DISABLE_MULTIPLE_CONTEXT_PROFILES
-                      })}
-                    />
-                  </div>
-                </div>
-                <ProfileSearchLegacy
-                  testId="profile-root"
-                  className="profile-search"
-                  resultClassName="profile-search-result"
-                  placeholderSmall="Search"
-                  placeholder="Search a company or production place"
-                  getResultTestId={item =>
-                    `search-result-${item.nodeType.toLowerCase()}-${item.name.toLowerCase()}`
-                  }
-                />
-                <p className="profile-root-explanatory-text">
-                  Enter the name of a company or production municipality, state or biome for key
-                  sustainability indicators and statistics on linked traders and consumer markets.
-                </p>
-              </div>
-            </div>
-            <div className="row align-center">
-              <div className="column small-12 medium-10 large-8">
-                <Img
-                  className="profile-root-mock hide-for-small"
-                  src="/images/backgrounds/profile-mock.jpg"
-                  alt="Profile Page Example"
-                />
-                <Img
-                  className="profile-root-mock show-for-small"
-                  src="/images/backgrounds/profile-mobile-mock.png"
-                  alt="Profile Mobile Page Example"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
-    </div>
-  );
-};
-
-renderLegacyProfiles.propTypes = {
-  errorMessage: PropTypes.string,
-  activeContext: PropTypes.object,
-  selectContexts: PropTypes.func.isRequired
-};
 
 const ProfileRoot = props => {
   const nodeTypeRenderer = node => {
