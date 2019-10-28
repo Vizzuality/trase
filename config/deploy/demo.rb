a529bc8a2b09d54aeb183a4881facac21dad6365
sorodrigo <rodrigo.solis@vizzuality.com>
2019-10-28 16:41:29 +0100

    use plain 12 instead of 12.12

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -2,7 +2,7 @@ server 'demo.trase.earth', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, forward_agent: true
 
 set :nvm_type, :user
-set :nvm_node, 'v12.12.0'
+set :nvm_node, '12'
 set :nvm_map_bins, %w{node npm yarn}
 
 set :branch, 'develop'
