56d9129c6b96d83bcd015b6c764e18f5ad0acf0d
sorodrigo <rodrigo.solis@vizzuality.com>
2019-10-28 16:23:56 +0100

    updates sandbox, demo, indonesiademo to node v12

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,4 +1,8 @@
 server 'demo.trase.earth', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, forward_agent: true
 
+set :nvm_type, :user
+set :nvm_node, 'v12.12.0'
+set :nvm_map_bins, %w{node npm yarn}
+
 set :branch, 'develop'
