ae019bf0a4600811175941bbae255628dbb74539
Alvaro Leal <pursuance@gmail.com>
2019-10-31 13:27:50 +0100

    Start serializing and fix canProceed

--- frontend/scripts/react-components/tool/tool-modal/tool-selection-modal/tool-panel/tool-panel.js
+++ frontend/scripts/react-components/tool/tool-modal/tool-selection-modal/tool-panel/tool-panel.js
@@ -4,6 +4,7 @@ import {
   getDynamicSentence
 } from 'react-components/tool/tool-modal/tool-selection-modal/tool-panel/tool-panel.selectors';
 import ToolSelectionModal from 'react-components/tool/tool-modal/tool-selection-modal/tool-panel/tool-panel.component';
+import { getCanProceed } from 'react-components/nodes-panel/nodes-panel.selectors';
 import { getCountryNamesByCountryId } from 'reducers/app.selectors';
 import { clearPanel, setSelectedItems } from 'react-components/nodes-panel/nodes-panel.actions';
 
@@ -14,7 +15,8 @@ const mapStateToProps = (state, ownProps) => {
     loading,
     isDisabled: getIsDisabled(state, ownProps),
     dynamicSentenceParts: getDynamicSentence(state),
-    countryNames: getCountryNamesByCountryId(state)
+    countryNames: getCountryNamesByCountryId(state),
+    canProceed: getCanProceed(state)
   };
 };
 
