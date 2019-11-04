9dcffdacd1a5dd0736a7a6e46746c18067817d65
Alvaro Leal <pursuance@gmail.com>
2019-11-04 20:10:04 +0100

    Fix nodes panel saga - WIP

--- frontend/scripts/react-components/tool/tool.thunks.js
+++ frontend/scripts/react-components/tool/tool.thunks.js
@@ -1,5 +1,6 @@
 import { loadDisclaimer, resize } from 'actions/app.actions';
 import { getToolColumns } from 'react-components/tool-links/tool-links.actions';
+import { getMissingItems } from 'react-components/nodes-panel/nodes-panel.actions';
 
 export const loadDisclaimerTool = dispatch => dispatch(loadDisclaimer());
 
@@ -8,5 +9,6 @@ export const resizeSankeyTool = dispatch => dispatch(resize());
 export const loadToolInitialData = (dispatch, getState, bag) => {
   if (bag?.action?.meta?.location?.kind !== 'redirect') {
     dispatch(getToolColumns());
+    dispatch(getMissingItems());
   }
 };
