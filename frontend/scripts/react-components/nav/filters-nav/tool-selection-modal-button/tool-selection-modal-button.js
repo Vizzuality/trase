8d95ab1f13b027a90f4c23dccbdb66d27dbfbacf
Alvaro Leal <pursuance@gmail.com>
2019-11-06 12:28:58 +0100

    Dont use dashboards saga in tool. Change tool button and selection of contexts

--- frontend/scripts/react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.js
+++ frontend/scripts/react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.js
@@ -1,10 +1,18 @@
 import { connect } from 'react-redux';
 import ToolSelectionModalButton from 'react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.component';
 import { setActiveModal } from 'react-components/tool/tool.actions';
+import { getNodesPanelValues } from 'react-components/dashboard-element/dashboard-element.selectors';
 
+const mapStateToProps = state => {
+  const panelValues = getNodesPanelValues(state);
+  return {
+    countryName: panelValues.countries[0] && panelValues.countries[0].name,
+    commodityName: panelValues.commodities[0] && panelValues.commodities[0].name
+  };
+};
 const mapDispatchToProps = { setActiveModal };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(ToolSelectionModalButton);
