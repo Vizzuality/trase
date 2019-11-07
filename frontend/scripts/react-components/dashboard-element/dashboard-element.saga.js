d6d9944a91690f7004bb7b752f02eae6bc873446
Alvaro Leal <pursuance@gmail.com>
2019-11-07 18:28:36 +0100

    Add draft to panels

--- frontend/scripts/react-components/dashboard-element/dashboard-element.saga.js
+++ frontend/scripts/react-components/dashboard-element/dashboard-element.saga.js
@@ -24,7 +24,7 @@ import {
   getImportersPreviousSteps
 } from 'react-components/nodes-panel/nodes-panel.selectors';
 
-function* updateIndicatorsOnItemChange() {
+export function* updateIndicatorsOnItemChange() {
   const selectedContext = yield select(getDashboardsContext);
   if (selectedContext) {
     yield fork(fetchDashboardCharts);
--- frontend/scripts/react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.js
+++ frontend/scripts/react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
 import ToolSelectionModalButton from 'react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.component';
 import { setActiveModal } from 'react-components/tool/tool.actions';
+import { editPanels } from 'react-components/nodes-panel/nodes-panel.actions';
+
 import { getNodesPanelValues } from 'react-components/dashboard-element/dashboard-element.selectors';
 
 const mapStateToProps = state => {
@@ -10,7 +12,7 @@ const mapStateToProps = state => {
     commodityName: panelValues.commodities[0] && panelValues.commodities[0].name
   };
 };
-const mapDispatchToProps = { setActiveModal };
+const mapDispatchToProps = { setActiveModal, editPanels };
 
 export default connect(
   mapStateToProps,
