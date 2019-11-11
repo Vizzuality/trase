bd823bf6971a7264a15d654d41604315e1b7903e
sorodrigo <rodrigo.solis@vizzuality.com>
2019-11-11 14:43:08 -0600

    fix default selected context

--- frontend/scripts/react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.js
+++ frontend/scripts/react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.js
@@ -1,17 +1,13 @@
 import { connect } from 'react-redux';
+import { getSelectedContext } from 'reducers/app.selectors';
 import ToolSelectionModalButton from 'react-components/nav/filters-nav/tool-selection-modal-button/tool-selection-modal-button.component';
 import { setActiveModal } from 'react-components/tool/tool.actions';
 import { editPanels } from 'react-components/nodes-panel/nodes-panel.actions';
 
-import { getNodesPanelValues } from 'react-components/dashboard-element/dashboard-element.selectors';
+const mapStateToProps = state => ({
+  selectedContext: getSelectedContext(state)
+});
 
-const mapStateToProps = state => {
-  const panelValues = getNodesPanelValues(state);
-  return {
-    countryName: panelValues.countries[0] && panelValues.countries[0].name,
-    commodityName: panelValues.commodities[0] && panelValues.commodities[0].name
-  };
-};
 const mapDispatchToProps = { setActiveModal, editPanels };
 
 export default connect(
