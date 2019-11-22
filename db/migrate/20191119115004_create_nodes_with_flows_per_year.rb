4df9ce85c10405121c5ef867229cb7daa9512c2d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-11-22 12:20:52 +0100

    New table nodes_with_flows_per_year

---
+++ db/migrate/20191119115004_create_nodes_with_flows_per_year.rb
@@ -0,0 +1,30 @@
+class CreateNodesWithFlowsPerYear < ActiveRecord::Migration[5.2]
+  def up
+    create_table :nodes_with_flows_per_year, primary_key: %i[node_id context_id year] do |t|
+      t.integer :node_id
+      t.integer :context_id
+      t.integer :country_id
+      t.integer :commodity_id
+      t.integer :node_type_id
+      t.integer :context_node_type_id
+      t.column :year, 'smallint'
+      t.column :nodes_ids, 'int[]'
+      t.text :name
+      t.column :name_tsvector, 'tsvector'
+      t.text :node_type
+      t.text :geo_id
+      t.boolean :is_unknown
+    end
+
+    create_view 'nodes_with_flows_per_year_v', materialized: false
+
+    Api::V3::Readonly::NodeWithFlowsPerYear.refresh(
+      sync: (Rails.env.development? || Rails.env.test?)
+    )
+  end
+
+  def down
+    drop_table :nodes_with_flows_per_year
+    drop_view 'nodes_with_flows_per_year_v', materialized: false
+  end
+end
---
+++ db/views/nodes_with_flows_per_year_v_v01.sql
@@ -0,0 +1,39 @@
+SELECT
+  nodes_with_co_nodes.node_id,
+  nodes_with_co_nodes.context_id,
+  contexts.country_id,
+  contexts.commodity_id,
+  nodes.node_type_id,
+  cnt.id AS context_node_type_id,
+  nodes_with_co_nodes.year,
+  nodes_with_co_nodes.nodes_ids,
+  TRIM(nodes.name) AS name,
+  TO_TSVECTOR('simple', COALESCE(nodes.name, '')) AS name_tsvector,
+  node_types.name AS node_type,
+  nodes.geo_id,
+  nodes.is_unknown
+FROM (
+  SELECT
+    flow_nodes.node_id,
+    flow_nodes.context_id,
+    flow_nodes.position,
+    flow_nodes.year,
+    ARRAY_AGG(DISTINCT co_flow_nodes.node_id) AS nodes_ids
+  FROM flow_nodes
+  JOIN flow_nodes co_flow_nodes ON flow_nodes.flow_id = co_flow_nodes.flow_id
+  WHERE flow_nodes.node_id <> co_flow_nodes.node_id
+  GROUP BY
+    flow_nodes.node_id,
+    flow_nodes.context_id,
+    flow_nodes.position,
+    flow_nodes.year
+) nodes_with_co_nodes
+JOIN nodes ON nodes_with_co_nodes.node_id = nodes.id
+JOIN node_types ON nodes.node_type_id = node_types.id
+JOIN contexts ON nodes_with_co_nodes.context_id = contexts.id
+JOIN context_node_types cnt ON
+  nodes_with_co_nodes.context_id = cnt.context_id
+  AND nodes_with_co_nodes.position = cnt.column_position + 1
+  AND contexts.id = cnt.context_id
+  AND nodes.node_type_id = cnt.node_type_id
+  AND node_types.id = cnt.node_type_id;
