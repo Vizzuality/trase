8fcb2b26169e3fabdf0c2913f9c27c21aaf6eac0
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2019-12-18 11:19:45 +0100

    Added second production instance

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
-server 'trase.earth', user: 'ubuntu', roles: %w{web app db}, primary: true
+server 'ec2-54-68-137-160.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app db}, primary: true
+server 'ec2-54-244-16-87.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app}, primary: false
+
 set :ssh_options, forward_agent: true
 
+set :sitemap_roles, :db
 after 'deploy:finishing', 'sitemap:refresh'
