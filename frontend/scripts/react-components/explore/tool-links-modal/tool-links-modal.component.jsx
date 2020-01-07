0ebe6391b201cb0020b850adfa35237ae7292c3e
sorodrigo <rodrigo.solis@vizzuality.com>
2020-01-07 19:32:26 +0100

    updates explore page styles

--- frontend/scripts/react-components/explore/tool-links-modal/tool-links-modal.component.jsx
+++ frontend/scripts/react-components/explore/tool-links-modal/tool-links-modal.component.jsx
@@ -7,11 +7,18 @@ import { ImgBackground } from 'react-components/shared/img';
 import 'react-components/explore/tool-links-modal/tool-links-modal.scss';
 
 function ToolLinksModal({ goToTool }) {
+  const bulletPoints = {
+    left: ['Visualize the data in real flows.', 'Highlight and manage the data as you wish.'],
+    right: ['Visualize the data in our charts.', 'Compare the data between different actors.']
+  };
   return (
     <div className="c-tool-links-modal">
       <div className="row columns">
-        <Heading size="md" className="modal-title">
-          Choose type of visualization
+        <Heading size="md" className="modal-title" align="center">
+          Choose the{' '}
+          <Heading as="span" size="md" weight="bold">
+            type of visualization
+          </Heading>
         </Heading>
       </div>
       <div className="links-container">
@@ -45,22 +52,24 @@ function ToolLinksModal({ goToTool }) {
       <div className="row">
         <div className="column small-12 medium-6">
           <ul className="description-list">
-            <li>
-              <Text>Visualize the data in real flows.</Text>
-            </li>
-            <li>
-              <Text>Highlight and manage the data as you wish.</Text>
-            </li>
+            {bulletPoints.left.map((point, i) => (
+              <li className="bullet-point" key={i}>
+                <Text size="rg" weight="bold" lineHeight="lg">
+                  {point}
+                </Text>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="column small-12 medium-6">
           <ul className="description-list">
-            <li>
-              <Text>Visualize the data in our charts.</Text>
-            </li>
-            <li>
-              <Text>Compare the data between different actors.</Text>
-            </li>
+            {bulletPoints.right.map((point, i) => (
+              <li className="bullet-point" key={i}>
+                <Text size="rg" weight="bold" lineHeight="lg">
+                  {point}
+                </Text>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
