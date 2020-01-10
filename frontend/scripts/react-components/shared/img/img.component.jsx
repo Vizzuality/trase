2851a4fa947168f3f215a0e458da54a134d45fcc
sorodrigo <rodrigo.solis@vizzuality.com>
2020-01-10 17:02:50 +0100

    remove stale code

--- frontend/scripts/react-components/shared/img/img.component.jsx
+++ frontend/scripts/react-components/shared/img/img.component.jsx
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import InView from 'react-components/shared/in-view.component';
 
 function Img(props) {
-  // srcSet is just using the first item, the idea is to pass it as it to the img element
-  // and expose an ImgContextProvider that the users can use to pass their sizes media-queries
   const { as, alt, src, srcSet, sizes, ...rest } = props;
 
   return (
@@ -13,20 +11,9 @@ function Img(props) {
         const srcSetProp = inView && srcSet ? srcSet.join(', ') : undefined;
         const srcProp = inView && src ? src : undefined;
 
-        // for now we pass sizes as is, but the idea is to also fallback to ImgContextProvider version
-        // so the user could either provided it per IMG or one context per all IMGs
         return (
           <>
             <img ref={ref} alt={alt} sizes={sizes} srcSet={srcSetProp} src={srcProp} {...rest} />
-            {!inView && (
-              <a
-                href={src}
-                style={{ display: 'block', visibility: 'hidden', position: 'absolute' }}
-                aria-hidden="true"
-              >
-                {alt}
-              </a>
-            )}
           </>
         );
       }}
