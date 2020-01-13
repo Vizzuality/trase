0b1a62cb8575d33229d68d772a8e36b2bfcbbade
sorodrigo <rodrigo.solis@vizzuality.com>
2020-01-13 14:25:45 +0100

    adds tooltips to toolbar

--- frontend/scripts/react-components/shared/tool-bar/tool-bar.component.jsx
+++ frontend/scripts/react-components/shared/tool-bar/tool-bar.component.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import { Manager, Reference, Popper } from 'react-popper';
+import Text from 'react-components/shared/text';
 import EditFilter from './edit-filter/edit-filter.component';
 import OptionsMenuFilter from './options-menu-filter/options-menu-filter.component';
 import ToolSwitch from './tool-switch';
@@ -15,37 +17,59 @@ const types = {
 
 function ToolBar(props) {
   const { leftSlot, rightSlot, className } = props;
+  const [showWithId, setId] = useState(null);
+
+  function renderItem(item) {
+    if (types[item.type] && item.show) {
+      return (
+        <Manager key={item.id}>
+          <Reference>
+            {({ ref }) => (
+              <li
+                ref={ref}
+                className={cx('slot-item', { '-no-hover': item.noHover })}
+                onMouseEnter={() => setId(item.id)}
+                onMouseLeave={() => setId(null)}
+              >
+                {React.createElement(types[item.type], {
+                  ...item,
+                  onClick: props[`${item.id}_onClick`]
+                })}
+              </li>
+            )}
+          </Reference>
+          <Popper placement="bottom-start">
+            {({ ref, style, placement, scheduleUpdate }) => {
+              const isVisible = item.id === showWithId && item.tooltip;
+              if (isVisible) {
+                // side-effect in render is not great, but fixes issue with dynamic content
+                scheduleUpdate();
+              }
+              return (
+                <div
+                  ref={ref}
+                  className="toolbar-tooltip"
+                  style={style}
+                  data-placement={placement}
+                  hidden={!isVisible}
+                >
+                  <Text color="white">{item.tooltip}</Text>
+                </div>
+              );
+            }}
+          </Popper>
+        </Manager>
+      );
+    }
+
+    // this will get flatten at render
+    return [];
+  }
 
   return (
     <aside className={cx('c-tool-bar', className)}>
-      <ul className="slot">
-        {leftSlot.flatMap(item =>
-          types[item.type] && item.show ? (
-            <li className={cx('slot-item', { '-no-hover': item.noHover })} key={item.id}>
-              {React.createElement(types[item.type], {
-                ...item,
-                onClick: props[`${item.id}_onClick`]
-              })}
-            </li>
-          ) : (
-            []
-          )
-        )}
-      </ul>
-      <ul className="slot">
-        {rightSlot.flatMap(item =>
-          types[item.type] && item.show ? (
-            <li className={cx('slot-item', { '-no-hover': item.noHover })} key={item.id}>
-              {React.createElement(types[item.type], {
-                ...item,
-                onClick: props[`${item.id}_onClick`]
-              })}
-            </li>
-          ) : (
-            []
-          )
-        )}
-      </ul>
+      <ul className="slot">{leftSlot.flatMap(renderItem)}</ul>
+      <ul className="slot">{rightSlot.flatMap(renderItem)}</ul>
     </aside>
   );
 }
