4bdc7a153c0097e849eb701dd1486ab9d8ea3eaf
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2020-01-20 08:54:01 +0100

    Fixed worker name matcher

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ Sidekiq.configure_server do |config|
   # this is an error handler for sidekiq; it is used to ensure the job that failed
   # is marked as FAILED in the database_updates tables
   config.error_handlers << Proc.new do |e, ctx_hash|
-    return unless ctx_hash[:job] && ctx_hash[:job]['class'] == 'DatabaseUpdateWorker'
+    return unless ctx_hash[:job] && ctx_hash[:job]['class'] =~ /.+DatabaseUpdateWorker/
     jid = ctx_hash[:job]['jid']
     du = Api::V3::DatabaseUpdate.where(jid: jid).first
     du&.update_attribute(:status, Api::V3::DatabaseUpdate::FAILED)
