67c434430a8e85e837970488c4b0ef2f5a1ef388
sorodrigo <rodrigo.solis@vizzuality.com>
2020-01-28 12:46:42 +0100

    updates bullet points styles

--- frontend/scripts/react-components/tool/map-layout/map-layout.js
+++ frontend/scripts/react-components/tool/map-layout/map-layout.js
@@ -1,8 +1,4 @@
 import { connect } from 'react-redux';
-<<<<<<< HEAD
-=======
-import { toggleMapLayerMenu } from 'app/app.actions';
->>>>>>> moves everything under one react root
 import MapLayout from 'react-components/tool/map-layout/map-layout.component';
 
 const mapStateToProps = state => ({
