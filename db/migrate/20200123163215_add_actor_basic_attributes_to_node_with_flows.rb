c6e2956033161742d88fba9035f782c740f6bb84
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2020-02-03 23:45:00 +0100

    Only run mass refresh for actor profile nodes

--- db/migrate/20200123163215_add_actor_basic_attributes_to_node_with_flows.rb
+++ db/migrate/20200123163215_add_actor_basic_attributes_to_node_with_flows.rb
@@ -1,11 +1,15 @@
 class AddActorBasicAttributesToNodeWithFlows < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :nodes_with_flows,
       :actor_basic_attributes,
       :json
 
-    Api::V3::Readonly::NodeWithFlows.all.each do |node_with_flows|
-      node_with_flows.set(actor_basic_attributes: actor_basic_attributes)
+    Api::V3::Readonly::NodeWithFlows.where(profile: :actor).each do |node_with_flows|
+      node_with_flows.refresh_actor_basic_attributes
     end
+  end
+
+  def down
+    remove_column :nodes_with_flows, :actor_basic_attributes
   end
 end
