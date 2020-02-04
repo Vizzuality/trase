df41166bdfb5d63d367f3e40649f7a40c064c270
sorodrigo <rodrigo.solis@vizzuality.com>
2020-02-04 22:33:20 +0100

    remove profile-node reducer

--- frontend/scripts/react-components/profile-node/profile-node.thunks.js
+++
@@ -1,7 +0,0 @@
-import { getColumns } from 'react-components/profile-node/profile-node.actions';
-
-export const loadColumnsData = (dispatch, getState, bag) => {
-  if (bag?.action?.meta?.location?.kind !== 'redirect') {
-    dispatch(getColumns());
-  }
-};
