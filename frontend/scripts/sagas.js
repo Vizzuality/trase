6638fcf620c4c980338b3b3217f375780e261464
sorodrigo <rodrigo.solis@vizzuality.com>
2020-02-04 22:33:20 +0100

    remove profile-node reducer

--- frontend/scripts/sagas.js
+++ frontend/scripts/sagas.js
@@ -1,12 +1,11 @@
 import { all, fork } from 'redux-saga/effects';
-import profileNode from 'react-components/profile-node/profile-node.saga';
 import dashboardElement from 'react-components/dashboard-element/dashboard-element.saga';
 import toolLinks from 'react-components/tool-links/tool-links.saga';
 import toolLayers from 'react-components/tool-layers/tool-layers.saga';
 import profilePanel from 'react-components/shared/profile-selector/profile-panel/profile-panel.saga';
 import nodesPanel from 'react-components/nodes-panel/nodes-panel.saga';
 
-const sagas = [profileNode, dashboardElement, toolLinks, toolLayers, profilePanel, nodesPanel];
+const sagas = [dashboardElement, toolLinks, toolLayers, profilePanel, nodesPanel];
 
 export function* rootSaga() {
   yield all(sagas.map(saga => fork(saga)));
