9f1753f99fd3f9634721127ad317baeb18616ecb
sorodrigo <rodrigo.solis@vizzuality.com>
2020-02-04 22:44:12 +0100

    removes profile node actions

--- frontend/scripts/initial-state.js
+++ frontend/scripts/initial-state.js
@@ -4,27 +4,14 @@ export { default as widgets } from 'react-components/widgets/widgets.initial-sta
 export { default as toolLinks } from 'react-components/tool-links/tool-links.initial-state';
 export { default as toolLayers } from 'react-components/tool-layers/tool-layers.initial-state';
 export { default as team } from 'react-components/team/team.initial-state';
-export {
-  default as staticContent
-} from 'react-components/static-content/static-content.initial-state';
-export {
-  default as profileSelector
-} from 'react-components/shared/profile-selector/profile-selector.initial-state';
+export { default as staticContent } from 'react-components/static-content/static-content.initial-state';
+export { default as profileSelector } from 'react-components/shared/profile-selector/profile-selector.initial-state';
 export { default as newsletter } from 'react-components/shared/newsletter/newsletter.initial-state';
-export { default as profileNode } from 'react-components/profile-node/profile-node.initial-state';
 export { default as profileRoot } from 'react-components/profile-root/profile-root.initial-state';
 export { default as nodesPanel } from 'react-components/nodes-panel/nodes-panel.initial-state';
-export {
-  default as logisticsMap
-} from 'react-components/logistics-map/logistics-map.initial-state';
-export {
-  default as legacyExplore
-} from 'react-components/legacy-explore/legacy-explore.initial-state';
+export { default as logisticsMap } from 'react-components/logistics-map/logistics-map.initial-state';
+export { default as legacyExplore } from 'react-components/legacy-explore/legacy-explore.initial-state';
 export { default as explore } from 'react-components/explore/explore.initial-state';
 export { default as data } from 'react-components/data-portal/data-portal.initial-state';
-export {
-  default as dashboardRoot
-} from 'react-components/dashboard-root/dashboard-root.initial-state';
-export {
-  default as dashboardElement
-} from 'react-components/dashboard-element/dashboard-element.initial-state';
+export { default as dashboardRoot } from 'react-components/dashboard-root/dashboard-root.initial-state';
+export { default as dashboardElement } from 'react-components/dashboard-element/dashboard-element.initial-state';
