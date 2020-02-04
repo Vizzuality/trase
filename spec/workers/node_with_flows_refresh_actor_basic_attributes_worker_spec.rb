abafd6f6c5c73780aee0de9cf7846900027371db
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2020-02-04 08:46:35 +0100

    Simplified worker spec

--- spec/workers/node_with_flows_refresh_actor_basic_attributes_worker_spec.rb
+++ spec/workers/node_with_flows_refresh_actor_basic_attributes_worker_spec.rb
@@ -6,9 +6,6 @@ RSpec.describe NodeWithFlowsRefreshActorBasicAttributesWorker, type: :worker do
   include_context 'api v3 brazil flows'
   include_context 'api v3 brazil exporter actor profile'
 
-  let(:node) { api_v3_exporter1_node }
-  let(:year) { 2015 }
-
   before(:each) do
     Api::V3::Readonly::FlowNode.refresh(sync: true)
     Api::V3::Readonly::NodeWithFlowsPerYear.refresh(sync: true)
@@ -18,21 +15,16 @@ RSpec.describe NodeWithFlowsRefreshActorBasicAttributesWorker, type: :worker do
   end
 
   it 'update actor basic attributes column' do
-    valid_node_with_flows = Api::V3::Readonly::NodeWithFlows.all.select do |nodes|
-      nodes.send('can_update_actor_basic_attributes?')
-    end
-
-    valid_node_with_flows.each do |node_with_flows|
-      expect(node_with_flows.actor_basic_attributes).to be_nil
-    end
+    actor_with_flows = Api::V3::Readonly::NodeWithFlows.
+      where(context_id: api_v3_context.id, profile: 'actor').
+      find(api_v3_exporter1_node.id)
+    expect(actor_with_flows.actor_basic_attributes).to be_nil
 
     NodeWithFlowsRefreshActorBasicAttributesWorker.new.perform(
-      valid_node_with_flows.map(&:id)
+      [api_v3_exporter1_node.id]
     )
 
-    valid_node_with_flows.each do |node_with_flows|
-      node_with_flows.reload
-      expect(node_with_flows.actor_basic_attributes).not_to be_nil
-    end
+    actor_with_flows.reload
+    expect(actor_with_flows.actor_basic_attributes).not_to be_nil
   end
 end
