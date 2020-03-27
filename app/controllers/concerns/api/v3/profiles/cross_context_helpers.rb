dbae930a22b433e4b02c947ece806e26850d2309
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2020-03-27 09:34:21 +0100

    Simplified passing "primary context"

--- app/controllers/concerns/api/v3/profiles/cross_context_helpers.rb
+++ app/controllers/concerns/api/v3/profiles/cross_context_helpers.rb
@@ -6,47 +6,33 @@ module Api
 
         private
 
-        def load_contexts
-          if params[:context_id]
-            @context = Api::V3::Context.find(params[:context_id])
-            @contexts = [@context]
-          else
-            contexts = Api::V3::Context.all
-            if params[:commodity_id]
-              contexts = contexts.where(commodity_id: params[:commodity_id])
-            end
-            if params[:country_id]
-              contexts = contexts.where(country_id: params[:country_id])
-            end
-            if params[:year]
-              contexts = contexts.where('years && ARRAY[?]::INT[]', params[:year])
-            end
-            unless contexts.any?
-              raise ActiveRecord::RecordNotFound.new(
-                'No country commodity combinations found for given parameters'
-              )
-            end
-            @contexts = contexts
-          end
-        end
-
         def load_node
           ensure_required_param_present(:id)
-          nodes = Api::V3::Readonly::NodeWithFlows.all
+          nodes = Api::V3::Readonly::NodeWithFlows.where(
+            profile: profile_type, id: params[:id]
+          )
+          if (context_id = params[:context_id])
+            nodes = nodes.where(context_id: context_id)
+          else
+            if (commodity_id = params[:commodity_id])
+              nodes = nodes.where(commodity_id: commodity_id)
+            end
+            if (country_id = params[:country_id])
+              nodes = contexts.where(country_id: country_id)
+            end
+          end
           if (year = params[:year]&.to_i)
             nodes = nodes.where('years && ARRAY[?]::INT[]', year)
           end
-          @node = nodes.find_by(
-            id: params[:id],
-            context_id: @contexts.map(&:id),
-            profile: profile_type
-          )
+
+          @contexts = Api::V3::Context.where(id: nodes.select(:context_id))
+          @node = nodes.first
+
           unless @node.present?
             raise ActiveRecord::RecordNotFound.new(
               'Node not found for given parameters'
             )
           end
-          @context = @node.context
         end
 
         def set_year
