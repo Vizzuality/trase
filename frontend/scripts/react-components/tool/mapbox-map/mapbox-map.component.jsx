28fb1309581cb47b7fd4ff17dfbe5cd4297ac57d
Alvaro Leal <pursuance@gmail.com>
2020-04-13 20:42:10 +0200

    Add basemaps and biome contextual layer

--- frontend/scripts/react-components/tool/mapbox-map/mapbox-map.component.jsx
+++ frontend/scripts/react-components/tool/mapbox-map/mapbox-map.component.jsx
@@ -5,15 +5,17 @@ import { LayerManager, Layer } from 'layer-manager/dist/components';
 import { PluginMapboxGl } from 'layer-manager';
 import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import cx from 'classnames';
-import { TOOL_LAYOUT } from 'constants';
+import { TOOL_LAYOUT, BASEMAPS } from 'constants';
 import Basemaps from 'react-components/tool/basemaps';
 import Legend from 'react-components/tool/legend';
-import activeLayers from './test-layers';
+// import activeLayers from './test-layers';
+import developmentLayers from './vector-styles/development.json';
+import { providers } from './mapbox-map.config';
 
 import 'react-components/tool/mapbox-map/mapbox-map.scss';
 
 function MapBoxMap(props) {
-  const { defaultMapView, toolLayout } = props;
+  const { defaultMapView, toolLayout, basemapId, selectedMapContextualLayersData } = props;
   const [mapAttribution, setMapAttribution] = useState(null);
   const [viewport, setViewport] = useState({ ...defaultMapView });
   const [loaded, setLoaded] = useState(false);
@@ -31,6 +33,45 @@ function MapBoxMap(props) {
       }
     }
   }, [loaded]);
+
+  const getContextualLayers = () => {
+    let layers = [];
+    selectedMapContextualLayersData.forEach((layerData) => {
+      // TODO: implement multi-year support
+      const { cartoLayers, identifier } = layerData;
+      const cartoData = cartoLayers[0];
+
+      if (cartoData.rasterUrl) {
+        const url = `${cartoData.rasterUrl}{z}/{x}/{y}.mvt`;
+        layers.push({
+          id: identifier,
+          type: 'raster',
+          source: {
+            type: 'raster',
+            tiles: [url],
+            maxzoom: 11 // TODO: add this to layer configuration
+          }
+        });
+      } else {
+        const layerStyle = developmentLayers.find(l => l.name === identifier);
+        if (layerStyle) {
+          layers = layers.concat(layerStyle.layers);
+        }
+      }
+    });
+    return layers;
+  };
+  const baseLayerInfo = BASEMAPS[basemapId];
+  const baseLayer = {
+    id: baseLayerInfo.id,
+    type: 'raster',
+    source: {
+      type: 'raster',
+      tiles: [baseLayerInfo.url],
+      minzoom: 2,
+      maxzoom: 12
+    }
+  };
   return (
     <div className={cx('c-map', { '-fullscreen': fullscreen }, { '-minimized': minimized })}>
       <ReactMapGL
@@ -49,10 +90,17 @@ function MapBoxMap(props) {
       >
         <NavigationControl showCompass={false} className="navigation-control" />
         {loaded && mapRef.current && (
-          <LayerManager map={mapRef.current.getMap()} plugin={PluginMapboxGl}>
-            {activeLayers.map(l => (
-              <Layer key={l.id} {...l} />
-            ))}
+          <LayerManager
+            map={mapRef.current.getMap()}
+            plugin={PluginMapboxGl}
+            providers={providers}
+          >
+            {[]
+              .concat(getContextualLayers())
+              .concat(baseLayer)
+              .map(l => (
+                <Layer key={l.id} {...l} />
+              ))}
           </LayerManager>
         )}
       </ReactMapGL>
@@ -85,7 +133,9 @@ function MapBoxMap(props) {
 
 MapBoxMap.propTypes = {
   defaultMapView: PropTypes.object,
-  toolLayout: PropTypes.number
+  toolLayout: PropTypes.number,
+  basemapId: PropTypes.string,
+  selectedMapContextualLayersData: PropTypes.array
 };
 
 export default MapBoxMap;
