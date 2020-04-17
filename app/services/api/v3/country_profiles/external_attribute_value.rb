bff68467eb910cc739ee80f1bbde48d32949c954
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2020-04-17 19:34:10 +0200

    Fixed lookup for header attributes

--- app/services/api/v3/country_profiles/external_attribute_value.rb
+++ app/services/api/v3/country_profiles/external_attribute_value.rb
@@ -12,12 +12,12 @@ module Api
         # @option attribute_ref [String] :id
         # e.g. {source: :wb, id: 'WB SP.POP.TOTL'}
         def call(iso_code2, year, activity, attribute_ref)
-          # TODO: implement
+          # TODO: implement comtrade
           if attribute_ref[:source] == :wb
             return Api::V3::CountriesWbIndicator.find_by(
-              iso_code: iso_code2,
+              iso_code: ISO3166::Country.new(iso_code2).alpha3,
               year: year,
-              name: attribute_ref[:id]
+              name: Api::V3::CountriesWbIndicators::ApiService::INDICATORS.key(attribute_ref[:id])
             )&.value
           end
 
