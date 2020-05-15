87ec51b8d90fbb8c143e5e5d5a366741e475dcf6
Alvaro Leal <pursuance@gmail.com>
2020-05-15 16:42:39 +0200

    Load all unit layers when we enter the tool

--- frontend/scripts/react-components/tool-layers/tool-layers.actions.js
+++ frontend/scripts/react-components/tool-layers/tool-layers.actions.js
@@ -1,7 +1,11 @@
+import axios from 'axios';
+import pick from 'lodash/pick';
+
 export const TOOL_LAYERS__SET_LINKED_GEOIDS = 'TOOL_LAYERS__SET_LINKED_GEOIDS';
 export const TOOL_LAYERS__SET_MAP_DIMENSIONS = 'TOOL_LAYERS__SET_MAP_DIMENSIONS';
 export const TOOL_LAYERS__SAVE_MAP_VIEW = 'TOOL_LAYERS__SAVE_MAP_VIEW';
 export const TOOL_LAYERS__SET_ACTIVE_MODAL = 'TOOL_LAYERS__SET_ACTIVE_MODAL';
+export const TOOL_LAYERS__SET_UNIT_LAYERS = 'TOOL_LAYERS__SET_UNIT_LAYERS';
 
 export function setLinkedGeoIds(nodes) {
   return {
@@ -16,6 +20,29 @@ export function setMapDimensions(dimensions, dimensionGroups) {
     payload: { dimensions, dimensionGroups }
   };
 }
+
+export function loadUnitLayers() {
+  return async (dispatch, getState) => {
+    if (!getState().toolLayers.data.mapUnitLayers) {
+      const urlsPromise = await axios.get(`${UNIT_LAYERS_API_URL}/services`);
+      const urls = urlsPromise?.data.map(d => d.url);
+      if (urls && urls.length) {
+        const layersPromise = await Promise.all(urls.map(url => axios.get(url)));
+        const layers =
+          layersPromise &&
+          layersPromise.map(l =>
+            pick(l.data, ['bounds', 'center', 'id', 'maxzoom', 'minzoom', 'tiles', 'version'])
+          );
+        return dispatch({
+          type: TOOL_LAYERS__SET_UNIT_LAYERS,
+          payload: { layers }
+        });
+      }
+      return undefined;
+    }
+    return undefined;
+  }
+};
 
 export function saveMapView(latlng, zoom) {
   return {
