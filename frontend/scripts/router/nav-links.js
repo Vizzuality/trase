56646b3f02a27a82e091e4a69874148e220994e1
Alvaro Leal <pursuance@gmail.com>
2020-05-28 15:48:39 +0200

    Change finance link position

--- frontend/scripts/router/nav-links.js
+++ frontend/scripts/router/nav-links.js
@@ -48,6 +48,11 @@ let nav = [
     external: true
   },
   {
+    name: 'Finance',
+    page: 'https://trase.finance',
+    external: true
+  },
+  {
     name: 'Insights',
     page: 'https://insights.trase.earth/insights',
     external: true
@@ -59,11 +64,6 @@ let nav = [
   {
     name: 'About',
     page: 'about'
-  },
-  {
-    name: 'Finance',
-    page: 'https://trase.finance',
-    external: true
   }
 ];
 
