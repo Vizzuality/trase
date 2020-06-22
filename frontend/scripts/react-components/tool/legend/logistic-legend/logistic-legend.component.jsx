a3b7b6a900c19399c116b7a32ce1b2b8797d32d7
Alvaro Leal <pursuance@gmail.com>
2020-06-22 20:42:12 +0200

    Start logistic layers legend

---
+++ frontend/scripts/react-components/tool/legend/logistic-legend/logistic-legend.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './logistic-legend.styles.scss';
+
+function LogisticLegend({ layers }) {
+  return (
+    <div className='cartodb-legend custom'>
+      <ul className='bullets'>
+        {layers.map(l => (
+          <li className='bkg'>
+            <div className='bullet' style={{ backgroundColor: l.color }}/>
+            {l.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+LogisticLegend.propTypes = {
+  layers: PropTypes.array
+};
+
+export default LogisticLegend;
