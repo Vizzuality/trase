ad5a4b03b6b689cb94e099e68e0a04f0285dd94e
Alvaro Leal <pursuance@gmail.com>
2020-06-29 20:03:47 +0200

    Add title and more styling

--- frontend/scripts/react-components/tool/legend/logistic-legend/logistic-legend.component.jsx
+++ frontend/scripts/react-components/tool/legend/logistic-legend/logistic-legend.component.jsx
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 import './logistic-legend.styles.scss';
 
 function LogisticLegend({ layers }) {
+  if (!layers.length) return null;
   return (
     <div className="c-logistic-legend cartodb-legend custom">
+      <div className="title bullets">
+        {layers[0].categoryName}
+      </div>
+      <div className="title bullets">
+        Inspection level:
+      </div>
       <ul className="bullets">
         {layers.map(l => (
           <li className="bkg">
-            <div className="bullet" style={{ backgroundColor: l.color }}/>
+            <div className="bullet" style={{ backgroundColor: l.color }} />
             {l.name}
           </li>
         ))}
