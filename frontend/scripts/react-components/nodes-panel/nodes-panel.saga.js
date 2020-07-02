067c20b187b8b662ef0b631880f1cf2d08b4b23a
Alvaro Leal <pursuance@gmail.com>
2020-07-02 19:36:27 +0200

    Fix context change crash

--- frontend/scripts/react-components/nodes-panel/nodes-panel.saga.js
+++ frontend/scripts/react-components/nodes-panel/nodes-panel.saga.js
@@ -223,16 +223,20 @@ function* syncSearchedNodes() {
 }
 
 function* broadcastContextChange() {
-  const previousContext = { countryId: null, commodityId: null };
+  let previousContext = { countryId: null, commodityId: null };
 
   function* onPanelSave() {
-    const { contexts } = yield select(state => state.app);
+    const { contexts, selectedContextId } = yield select(state => state.app);
     const { countries, commodities } = yield select(state => state.nodesPanel);
     const context = contexts.find(
       ctx =>
         ctx.countryId === countries.draftSelectedNodeId &&
         ctx.commodityId === commodities.draftSelectedNodeId
     );
+    const stateContext = contexts.find(ctx => ctx.id === selectedContextId);
+    if (stateContext) {
+      previousContext = stateContext;
+    }
     if (
       countries.draftSelectedNodeId !== previousContext.countryId ||
       commodities.draftSelectedNodeId !== previousContext.commodityId
