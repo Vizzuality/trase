dc321fb6b3b6581c196b2ed780c241ee579de651
Tomas Eriksson <tomasmoosee@gmail.com>
2020-07-09 12:24:24 +0200

    Country profiles, add summary information

--- frontend/scripts/react-components/profile/profile-components/summary/country-summary.component.jsx
+++ frontend/scripts/react-components/profile/profile-components/summary/country-summary.component.jsx
@@ -5,18 +5,31 @@ import cx from 'classnames';
 
 import PropTypes from 'prop-types';
 import capitalize from 'lodash/capitalize';
+import HelpTooltip from 'react-components/shared/help-tooltip/help-tooltip.component';
 import TitleGroup from 'react-components/profile/profile-components/title-group';
 import SummaryTitle from 'react-components/profile/profile-components/summary/summary-title.component';
 import Map from 'react-components/profile/profile-components/map.component';
 import Text from 'react-components/shared/text';
+import formatValue from 'utils/formatValue';
+import Icon from 'react-components/shared/icon';
 
 function CountrySummary(props) {
   const {
     year,
     onChange,
+    printMode,
     openModal,
-    profileMetadata: { years, activity, commodities, activities, commodityId, name: countryName } = {}
+    profileMetadata: {
+      years,
+      activity,
+      commodities,
+      activities,
+      commodityId,
+      name: countryName
+    } = {},
+    data: { summary, headerAttributes } = {}
   } = props;
+
   const renderCountryMap = () => (
     <div className="c-overall-info page-break-inside-avoid">
       <div className="c-locator-map map-country-banner">
@@ -24,13 +37,67 @@ function CountrySummary(props) {
           topoJSONPath="./vector_layers/WORLD.topo.json"
           topoJSONRoot="world"
           getPolygonClassName={d =>
-            d.properties.name.toLowerCase() === countryName.toLowerCase()
-              ? '-isCurrent'
-              : ''
+            d.properties.name.toLowerCase() === countryName.toLowerCase() ? '-isCurrent' : ''
           }
           useRobinsonProjection
         />
       </div>
+    </div>
+  );
+
+  const renderForest500 = ({ name, tooltip, value }) => (
+    <div className="stat-item">
+      <div className="legend">
+        <Text transform="uppercase" variant="mono" as="span">
+          {name}
+        </Text>
+        <span id="forest-500-tooltip">
+          <HelpTooltip text={tooltip} />
+        </span>
+      </div>
+      <div className="value forest-500-score">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <Icon
+            key={`circle${index}`}
+            color="grey"
+            className="circle-icon"
+            icon={`icon-circle-${value > index ? 'filled' : 'empty'}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+
+  const renderZeroDeforestation = ({ name, tooltip, value }) => (
+    <div className="stat-item">
+      <Text transform="uppercase" variant="mono" as="div" className="legend">
+        {name}
+        <span>
+          <HelpTooltip text={tooltip} />
+        </span>
+      </Text>
+      {value.toLowerCase() !== 'none' ? (
+        <div className="value">
+          <Icon color="grey" icon="icon-check-circle" className="icon-check-circle" />
+          <Text
+            transform="uppercase"
+            variant="mono"
+            as="span"
+            weight="bold"
+            size="lg"
+            className="stats-text"
+          >
+            {value}
+          </Text>
+        </div>
+      ) : (
+        <div className="value">
+          <Icon color="grey" icon="icon-no-circle" className="icon-no" />
+          <Text transform="uppercase" variant="mono" as="span" weight="bold" className="stats-text">
+            NONE
+          </Text>
+        </div>
+      )}
     </div>
   );
 
@@ -69,6 +136,32 @@ function CountrySummary(props) {
     }
   ];
 
+  const renderIndicator = indicatorKey => {
+    if (indicatorKey === 'forest_500') {
+      return renderForest500(headerAttributes.forest_500);
+    }
+    if (indicatorKey === 'zero_deforestation') {
+      return renderZeroDeforestation(headerAttributes.zero_deforestation);
+    }
+    const { name, value, unit, tooltip } = headerAttributes[indicatorKey];
+    if (!value) return null;
+    return (
+      <div className="stat-item" key={`${indicatorKey}${name}`}>
+        <Text transform="uppercase" variant="mono" as="div" className="legend">
+          {name}
+          {tooltip && <HelpTooltip text={tooltip} />}
+        </Text>
+        <Text as="span" variant="mono" size="lg" weight="bold">
+          {formatValue(value, indicatorKey)}
+        </Text>
+        <Text as="span" variant="mono" size="lg" weight="bold">
+          {' '}
+          {unit === 'km2' ? 'km²' : unit}
+        </Text>
+      </div>
+    );
+  };
+
   return (
     <div
       id="overall-info"
@@ -97,6 +190,16 @@ function CountrySummary(props) {
                   titles={titles}
                   on={newYear => onChange('year', newYear)}
                 />
+                {status !== Sticky.STATUS_FIXED &&
+                  headerAttributes &&
+                  Object.keys(headerAttributes).length > 0 &&
+                  Object.keys(headerAttributes).some(k => headerAttributes[k].value !== null) && (
+                    <div className="small-12">
+                      {Object.keys(headerAttributes).map(indicatorKey =>
+                        renderIndicator(indicatorKey)
+                      )}
+                    </div>
+                  )}
               </div>
             )}
           </Sticky>
@@ -105,7 +208,9 @@ function CountrySummary(props) {
       </div>
 
       <div className="row">
-        <div className="small-12 columns">
+        <div
+          className={cx('small-12', 'columns', { 'large-12': printMode, 'large-10': !printMode })}
+        >
           <Text
             variant="serif"
             size="md"
@@ -113,7 +218,7 @@ function CountrySummary(props) {
             lineHeight="lg"
             color="grey"
             className="summary"
-            dangerouslySetInnerHTML={{ __html: 'summary' }}
+            dangerouslySetInnerHTML={{ __html: summary }}
           />
         </div>
       </div>
@@ -123,6 +228,8 @@ function CountrySummary(props) {
 
 CountrySummary.propTypes = {
   year: PropTypes.number,
+  data: PropTypes.object,
+  printMode: PropTypes.bool,
   onChange: PropTypes.func.isRequired,
   openModal: PropTypes.func.isRequired,
   profileMetadata: PropTypes.object.isRequired
