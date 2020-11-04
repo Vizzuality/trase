bbed0577180f16876574334f7c16f351e1b45fa1
Alvaro Leal <pursuance@gmail.com>
2020-11-04 14:13:15 +0100

    Add icons and style button

--- frontend/scripts/react-components/shared/full-screen-button/full-screen-button.component.jsx
+++ frontend/scripts/react-components/shared/full-screen-button/full-screen-button.component.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import Button from 'react-components/shared/button';
+import Icon from 'react-components/shared/icon';
 import 'react-components/shared/full-screen-button/full-screen-button.scss';
 
 function FullScreenButton({ setFullScreen, isFullScreen }) {
   return (
     <div className="c-full-screen-button">
       <Button
-        size="lg"
-        weight="bold"
-        color="gray-transparent"
+        size="xs"
+        variant="icon"
+        icon={`icon-full-screen-${isFullScreen ? 'expand' : 'exit'}`}
+        color="red"
         onClick={setFullScreen}
-        className="button"
-      >
-        {isFullScreen ? 'Exit' : 'Go to'} full screen
-      </Button>
+        className="button-full-screen"
+      />
     </div>
   );
 }
