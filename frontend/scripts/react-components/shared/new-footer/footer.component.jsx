c3c2f313200778aac75b983e74d0c6885e6eca97
Alvaro Leal <pursuance@gmail.com>
2021-01-12 16:38:36 +0100

    Update styles and images

--- frontend/scripts/react-components/shared/new-footer/footer.component.jsx
+++ frontend/scripts/react-components/shared/new-footer/footer.component.jsx
@@ -10,14 +10,14 @@ const partners = [
   {
     className: 'gcp-logo',
     href: 'http://globalcanopy.org/',
-    image: '/images/footer/partnership/g-c-p@2x.png',
+    image: '/images/footer/partnership/gc@2x.png',
     alt: 'Global Canopy Programme',
     imageClassName: 'gcp-img'
   },
   {
     className: 'sei-logo',
     href: 'https://sei-international.org/',
-    image: '/images/footer/partnership/s-e-i@2x.png',
+    image: '/images/footer/partnership/sei@2x.png',
     alt: 'Stockholm Environment Institute',
     imageClassName: null
   }
@@ -26,16 +26,15 @@ const partners = [
 const donors = [
   {
     className: 'ggp-logo',
-    href:
-      'https://www.gov.uk/government/organisations/department-for-international-development',
-    image: '/images/footer/donors/u-kaidsvg@2x.png',
+    href: 'https://goodgrowthpartnership.com/',
+    image: '/images/footer/donors/ggp@2x.png',
     alt: 'Good growth partnership',
     imageClassName: null
   },
   {
     className: 'moore-logo',
     href: 'https://www.moore.org/',
-    image: '/images/footer/donors/moore@2x.png',
+    image: '/images/footer/donors/moore-white@2x.png',
     alt: 'Gordon and Betty Moore Foundation',
     imageClassName: null
   },
@@ -43,7 +42,7 @@ const donors = [
     className: 'nicfi-logo',
     href:
       'https://www.regjeringen.no/en/topics/climate-and-environment/climate/climate-and-forest-initiative/id2000712/',
-    image: '/images/footer/donors/nicfi_logo.svg',
+    image: '/images/footer/donors/nicfi@2x.png',
     alt: "Norway's International Climate and Forest Initiative (NICFI)",
     imageClassName: null
   }
@@ -67,19 +66,37 @@ const Footer = () => (
             ))}
           </ul>
           <div className="partnership-text">
-            <p>
-              A partnership between Global Canopy and the Stockholm Environment Institute.
-            </p>
-            <p>
+            <div>A partnership between Global Canopy and the Stockholm Environment Institute.</div>
+            <div>
               In collaboration with
-              <a href="http://www.vizzuality.com/" target="_blank" alt="vizzuality" rel="noopener noreferrer">
+              <a
+                className="partnership-link"
+                href="http://www.vizzuality.com/"
+                target="_blank"
+                alt="vizzuality"
+                rel="noopener noreferrer"
+              >
                 Vizzuality
-              </a>,
-              <a href="http://www.efi.int/portal/" target="_blank" alt="vizzuality" rel="noopener noreferrer">
+              </a>
+              ,{' '}
+              <a
+                className="partnership-link"
+                href="http://www.efi.int/portal/"
+                target="_blank"
+                alt="vizzuality"
+                rel="noopener noreferrer"
+              >
                 EFI
               </a>
-              , and many other organizations and individuals. Find out more here.
-            </p>
+              , and many other organizations and individuals. Find out more{' '}
+              <Link
+                className="partnership-link"
+                to={{ type: 'about', payload: { section: 'partners' } }}
+              >
+                here
+              </Link>
+              .
+            </div>
           </div>
         </div>
         <div className="footer-menu">Menu</div>
@@ -87,67 +104,72 @@ const Footer = () => (
       </div>
     </div>
     <div className="section-separator" />
-    <div className="footer-low row column">
-      <div className="link-list-container row">
-        <ul className="social-links-list">
-          <li className="social-link-item">
-            <a className="social-link" href="http://www.vizzuality.com/">
-              <Img src="/images/logos/new-logo-trase.svg" alt="twitter link" />
-            </a>
-          </li>
-          <li className="social-link-item">
-            <a className="social-link" href="http://www.vizzuality.com/">
-              <Img src="/images/logos/new-logo-trase.svg" alt="linkedin link" />
-            </a>
-          </li>
-        </ul>
-        <ul className="links-list">
-        <li className="link-item">
-          <Link
-            className="title -mono-font"
-            to={{ type: 'about', payload: { section: 'terms-of-use' } }}
-          >
-            Terms of Use
-        </Link>
-        </li>
-        <>
-          <li className="separator"> · </li>
-          <li className="link-item">
-            <Link
-              className="title -mono-font"
-              to={{ type: 'about', payload: { section: 'privacy-policy' } }}
-            >
-              Privacy policy
-          </Link>
-          </li>
-          <li className="separator"> · </li>
-          <li className="link-item">
-            <Link
-              className="title -mono-font"
-              to={{ type: 'about', payload: { section: 'cookie-policy' } }}
-            >
-              Cookie policy
-          </Link>
-          </li>
-        </>
-        <li className="separator"> · </li>
-        <li className="link-item">
-          <a className="title -mono-font" href="mailto:info@trase.earth">
-            Contact us
-        </a>
-        </li>
-      </ul>
-      </div>
-      <div className="donors-logos-container row">
-        <ul className="donors-logos">
-          {donors.map(logo => (
-            <li key={logo.className} className={cx('logo-item', logo.className)}>
-              <a href={logo.href} target="_blank" rel="noopener noreferrer">
-                <Img src={logo.image} alt={logo.alt} />
+    <div className="footer-low-container row">
+      <div className="footer-low">
+        <div className="link-list-container">
+          <ul className="social-links-list">
+            <li className="social-link-item">
+              <a className="social-link" href="https://twitter.com/traseearth">
+                <Img src="/images/footer/social/twitter.svg" alt="twitter link" />
               </a>
             </li>
-          ))}
-        </ul>
+            <li className="social-link-item">
+              <a
+                className="social-link"
+                href="https://www.linkedin.com/company/trase-transparency-for-sustainable-economies"
+              >
+                <Img src="/images/footer/social/linkedin.svg" alt="linkedin link" />
+              </a>
+            </li>
+          </ul>
+          <ul className="links-list">
+            <li className="link-item">
+              <Link
+                className="privacy-menu-link"
+                to={{ type: 'about', payload: { section: 'terms-of-use' } }}
+              >
+                Terms of Use
+              </Link>
+            </li>
+            <>
+              <li className="separator"> · </li>
+              <li className="link-item">
+                <Link
+                  className="privacy-menu-link"
+                  to={{ type: 'about', payload: { section: 'privacy-policy' } }}
+                >
+                  Privacy policy
+                </Link>
+              </li>
+              <li className="separator"> · </li>
+              <li className="link-item">
+                <Link
+                  className="privacy-menu-link"
+                  to={{ type: 'about', payload: { section: 'cookie-policy' } }}
+                >
+                  Cookie policy
+                </Link>
+              </li>
+            </>
+            <li className="separator"> · </li>
+            <li className="link-item">
+              <a className="privacy-menu-link" href="mailto:info@trase.earth">
+                Contact us
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="donors-logos-container row">
+          <ul className="donors-logos">
+            {donors.map(logo => (
+              <li key={logo.className} className={cx('logo-item', logo.className)}>
+                <a href={logo.href} target="_blank" rel="noopener noreferrer">
+                  <Img src={logo.image} alt={logo.alt} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   </div>
