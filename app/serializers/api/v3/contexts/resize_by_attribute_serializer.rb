2b5001070b61ef8386cc7aa502d4e23506cffb1a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-01-18 22:03:01 +0100

    Override display name and tooltip for resize by / recolor by

--- app/serializers/api/v3/contexts/resize_by_attribute_serializer.rb
+++ app/serializers/api/v3/contexts/resize_by_attribute_serializer.rb
@@ -4,8 +4,23 @@ module Api
       class ResizeByAttributeSerializer < ActiveModel::Serializer
         attributes :is_default, :is_disabled, :group_number, :position, :name,
                    :unit, :years, :attribute_id
-        attribute :display_name, key: :label
-        attribute :tooltip_text, key: :description
+        attribute :display_name, key: :label do
+          name_and_tooltip.display_name
+        end
+
+        attribute :tooltip_text, key: :description do
+          name_and_tooltip.tooltip_text
+        end
+
+        def name_and_tooltip
+          return @name_and_tooltip if defined?(@name_and_tooltip)
+
+          @name_and_tooltip = Api::V3::AttributeNameAndTooltip.call(
+            attribute: object.readonly_attribute,
+            context: object.context,
+            defaults: Api::V3::AttributeNameAndTooltip::NameAndTooltip.new(object.display_name, object.tooltip_text)
+          )
+        end
       end
     end
   end
