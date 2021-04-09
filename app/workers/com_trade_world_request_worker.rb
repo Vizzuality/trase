3d6eef5167e9d26d8e91db46519fd0f91cac27b4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-04-09 19:10:42 +0200

    Retry 10 times

--- app/workers/com_trade_world_request_worker.rb
+++ app/workers/com_trade_world_request_worker.rb
@@ -1,8 +1,8 @@
 class ComTradeWorldRequestWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :high,
-                  retry: 5,
+  sidekiq_options queue: :default,
+                  retry: 10,
                   backtrace: true
 
   def perform(uri)
