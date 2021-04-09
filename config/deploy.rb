b11ad899bdcd30036f6deed676dfe36f582e340e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-04-09 17:21:52 +0200

    Updated Capistrano lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.14.0'
+lock '~> 3.16.0'
 
 set :application, 'trase'
 set :repo_url, 'git@github.com:Vizzuality/trase.git'
