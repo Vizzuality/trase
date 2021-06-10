7058ca3b15f3b212fac218e161186c162395de1a
Alvaro Leal <pursuance@gmail.com>
2021-06-10 15:25:03 +0200

    Remove tooltip data from map

--- frontend/scripts/react-components/tool/map/map.selectors.js
+++ frontend/scripts/react-components/tool/map/map.selectors.js
@@ -1,9 +1,11 @@
 import { createSelector } from 'reselect';
 import formatValue from 'utils/formatValue';
-import { getSelectedMapContextualLayersData } from 'react-components/tool-layers/tool-layers.selectors';
 import { getHighlightedNodesData } from 'react-components/tool/tool.selectors';
-import { getSelectedContext } from 'app/app.selectors';
-import { getSelectedResizeBy } from 'react-components/tool-links/tool-links.selectors';
+import { getSelectedContext, getSelectedYears } from 'app/app.selectors';
+import {
+  getSelectedMapDimensionsData,
+  getSelectedMapContextualLayersData
+} from 'react-components/tool-layers/tool-layers.selectors';
 import { getContextualLayersTemplates, getRasterLayerTemplate } from './layers/contextual-layers';
 import { getLogisticMapLayerTemplates } from './layers/logistic-layers';
 
@@ -16,32 +18,78 @@ export const getCountryName = createSelector(
   selectedContext => selectedContext?.countryName || null
 );
 
+const getNodeAttributes = state => state.toolLinks.data.nodeAttributes || null;
 const getHighlightedNodesCoordinates = state => state.toolLayers.highlightedNodeCoordinates;
 export const getNodeHeights = state => state.toolLinks.data.nodeHeights || null;
 
+const getValue = (unitLayerValues, selectedYears, dimension) => {
+  if (!selectedYears) {
+    return null;
+  }
+  const [firstYear, lastYear] = selectedYears;
+
+  let value = 0;
+  for (let i = firstYear; i <= lastYear; i++) {
+    if (unitLayerValues.years[i]) {
+      value += unitLayerValues.years[i];
+    }
+  }
+
+  // Non-temporal indicator values are stored in year 0
+  if (!value && unitLayerValues.years['0']) {
+    return unitLayerValues.years['0'];
+  }
+
+  if (dimension.aggregationMethod === 'AVG' || dimension.unit === '%' || dimension.type === 'ind') {
+    // Average of the year range
+    value /= lastYear - firstYear + 1;
+  }
+
+  return value;
+};
+
 export const getTooltipValues = createSelector(
   [
-    getNodeHeights,
+    getNodeAttributes,
+    getSelectedMapDimensionsData,
     getHighlightedNodesData,
     getHighlightedNodesCoordinates,
-    getSelectedResizeBy,
+    getSelectedYears,
     getUnitLayersData
   ],
-  (nodeHeights, nodesData, coordinates, selectedResizeBy, unitLayersData) => {
-    const values = [];
+  (
+    nodeAttributes,
+    selectedMapDimensions,
+    nodesData,
+    coordinates,
+    selectedYears,
+    unitLayersData
+  ) => {
+    let values = [];
     const node = nodesData && nodesData[0];
     if (!coordinates || !unitLayersData || !node) {
       return null;
     }
-    const nodeHeight = nodeHeights && nodeHeights[node.id];
 
-    // if node is visible in sankey, quant is available. E.g. Trade Volume
-    if (nodeHeight) {
-      values.push({
-        title: selectedResizeBy.label,
-        unit: selectedResizeBy.unit,
-        value: formatValue(nodeHeight.quant, selectedResizeBy.label)
-      });
+    if (nodeAttributes && selectedMapDimensions && selectedMapDimensions.length > 0) {
+      values = selectedMapDimensions
+        .map(dimension => {
+          const unitLayerValues = unitLayersData.find(
+            d => d.geo_id === node.geoId && d.attribute_id === dimension.attributeId
+            // && d.node_type_id === nodeTypeId - If we need to find the selected values for a specific nodeTypeId
+          );
+          if (!unitLayerValues) {
+            return null;
+          }
+
+          const indicatorValue = getValue(unitLayerValues, selectedYears, dimension);
+          return {
+            title: dimension.name,
+            unit: dimension.unit,
+            value: formatValue(indicatorValue, dimension.name)
+          };
+        })
+        .filter(Boolean);
     }
 
     return values;
