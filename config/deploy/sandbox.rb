6a1650f2621bffa3d00224cdb9016c5ab362f52d
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-09-17 23:16:33 +0200

    Use trase domain for deploy urls

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,5 +1,5 @@
-server 'ec2-34-216-27-79.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app db}, primary: true
-server 'ec2-44-225-147-82.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app}, primary: false
+server 'sandbox-data.trase.earth', user: 'ubuntu', roles: %w{web app db}, primary: true
+server 'sandbox-web.trase.earth', user: 'ubuntu', roles: %w{web app}, primary: false
 
 set :ssh_options, forward_agent: true
 
