0353325d1d16f37482ee2baa801e171ef3cad0da
Alvaro leal <pursuance@gmail.com>
2022-08-29 18:25:54 +0200

    Sort nodes if both are unknown

--- frontend/scripts/app/helpers/sortVisibleNodes.js
+++ frontend/scripts/app/helpers/sortVisibleNodes.js
@@ -6,6 +6,9 @@ export default function(visibleNodesByColumn, nodeHeights) {
   };
 
   const byHeightOthersLast = (nodeA, nodeB) => {
+    if (nodeA.name && nodeA.name.startsWith('UNKNOWN')) {
+      console.log(nodeA, nodeB);
+    }
     if (nodeA.isDomesticConsumption || nodeB.isDomesticConsumption) {
       if (nodeA.isDomesticConsumption) {
         return 1;
@@ -13,6 +16,11 @@ export default function(visibleNodesByColumn, nodeHeights) {
       return -1;
     }
     if ((nodeA.isAggregated && !nodeB.isUnknown) || nodeA.isUnknown) {
+      // If both are unknown the 'just' UNKNOWN node come first
+      if (nodeB.isUnknown) {
+        return nodeA.name === 'UNKNOWN' ? 1 : -1;
+      }
+
       return 1;
     }
     if ((nodeB.isAggregated && !nodeA.isUnknown) || nodeB.isUnknown) {
