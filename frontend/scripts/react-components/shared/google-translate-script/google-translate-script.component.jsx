2931b6594f6b34aa8d994c1fcd1eef31ea4fda22
Alvaro Leal <pursuance@gmail.com>
2022-09-05 17:46:12 +0200

    Limit the languages on google translate

--- frontend/scripts/react-components/shared/google-translate-script/google-translate-script.component.jsx
+++ frontend/scripts/react-components/shared/google-translate-script/google-translate-script.component.jsx
@@ -4,15 +4,6 @@ import Helmet from 'react-helmet';
 const GoogleTranslateScript = () => (
   <>
     <div id="google_translate_element" className="google-translate" />
-    <Helmet
-      script={[
-        {
-          type: 'text/javascript',
-          innerHTML: `function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');}`
-        }
-      ]}
-    />
     <Helmet>
       <script
         src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
