32ed6e136931591c734b30e06363dd1a3cbfd76b
Alvaro Leal <pursuance@gmail.com>
2022-10-28 11:26:37 +0200

    Fix warning

--- frontend/scripts/react-components/data-portal/data-portal-form/data-portal-form.js
+++ frontend/scripts/react-components/data-portal/data-portal-form/data-portal-form.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux'
-import Component from './data-portal-form.component';
 import { newsletterActions } from 'react-components/shared/newsletter/newsletter.register';
+import Component from './data-portal-form.component';
 
 export default connect(null, newsletterActions)(Component);
