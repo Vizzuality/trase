9f9fa12e6aaff9f5ec46cb02c32802031f55b046
Alvaro Leal <pursuance@gmail.com>
2022-11-24 10:10:50 +0100

    Update pop-up

--- frontend/scripts/react-components/popup/popup-component.jsx
+++ frontend/scripts/react-components/popup/popup-component.jsx
@@ -20,7 +20,8 @@ function PopUp({ handleOnRequestClose }) {
       <div className="c-popup">
         <div className="popup-content">
           <Heading as="h3" weight="bold" size="md">
-            The term Deforestation Risk changes to Deforestation exposure
+            The term {`"`}Deforestation Risk{`"`} has been replaced with {`"`}Deforestation exposure
+            {`"`}
           </Heading>
           <Text as="p" size="md" lineHeight="md" className="description">
             On 10 November 2022, the term{' '}
@@ -33,6 +34,10 @@ function PopUp({ handleOnRequestClose }) {
             </Text>{' '}
             as a measure of the exposure of supply chain actors to deforestation from commodity
             production based on sourcing patterns.
+          </Text>
+          <Text as="p" size="md" lineHeight="md" className="description">
+            Improvements in how commodity deforestation exposure is calculated were made on 7
+            December 2022. Numbers accessed before that date will differ from those accessed after.
           </Text>
           <Text as="p" size="md" lineHeight="md">
             For more information, see{' '}
