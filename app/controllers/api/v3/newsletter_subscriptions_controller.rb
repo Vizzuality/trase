ac12e63d658d01e2949db4b2e0660b6a97f45b60
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2023-04-03 14:05:06 +0200

    Merge tags updates

--- app/controllers/api/v3/newsletter_subscriptions_controller.rb
+++ app/controllers/api/v3/newsletter_subscriptions_controller.rb
@@ -3,6 +3,15 @@ module Api
     class NewsletterSubscriptionsController < ApiController
       skip_before_action :load_context
 
+      # Fields and their Mailchimp merge tags:
+      # Given name - *|FNAME|* (can just send name here)
+      # Surname - *|LNAME|*
+      # Email - *|EMAIL|*
+      # Country - *|COUNTRY|*
+      # Organisation name - *|MMERGE3|*
+      # Type of organisation - is this a dropdown or can people type anything they want? I've created the field as a text field for now - *|TRASETYPE|*
+      # Use of Trase data - *|TRASEUSE|*
+      # About your work - *|TRASEWORK|*
       def create
         mailchimp = Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
         response = mailchimp.lists.subscribe(
@@ -10,10 +19,15 @@ module Api
           { email: params[:email] },
           {
             'FNAME' => params[:firstname],
+            # TODO: this comes undefined
             'LNAME' => (params[:lastname] && params[:lastname] != "") ? params[:lastname] : "-",
+            'EMAIL' => params[:email],
+            'COUNTRY' => params[:country] || "-",
             'MMERGE3' => params[:organisation],
-            'MMERGE4' => params[:country] || "-",
-            'MMERGE5' => "-"
+            'TRASETYPE' => params[:trase_type],
+            'TRASEUSE' => params[:trase_use],
+            'TRASEWORK' => params[:trase_work],
+            'REFERRER' => request.referer
           }
         )
         p response
